ac9d0458ef9680ba8f1532100e161c7a6f88348b
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-06-21 00:33:15 +0000

    nueva exception

---
+++ SWB4/Client/OpenOffice/SWBOfficeCore/src/org/semanticwb/openoffice/NoHasLocationException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.semanticwb.openoffice;
+
+/**
+ *
+ * @author victor.lorenzana
+ */
+public class NoHasLocationException extends WBException
+{
+
+    public NoHasLocationException()
+    {
+        super("");
+    }
+}
