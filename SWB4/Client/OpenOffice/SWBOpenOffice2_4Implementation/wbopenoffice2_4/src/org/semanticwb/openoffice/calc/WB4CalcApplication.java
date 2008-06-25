96e080b3d3d6bc6bfdc4995833f2185af748e9a3
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-06-25 20:47:28 +0000

    Cambios por preparedhtml

--- SWB4/Client/OpenOffice/SWBOpenOffice2_4Implementation/wbopenoffice2_4/src/org/semanticwb/openoffice/calc/WB4CalcApplication.java
+++ SWB4/Client/OpenOffice/SWBOpenOffice2_4Implementation/wbopenoffice2_4/src/org/semanticwb/openoffice/calc/WB4CalcApplication.java
@@ -7,6 +7,7 @@ package org.semanticwb.openoffice.calc;
 import org.semanticwb.openoffice.*;
 import org.semanticwb.openoffice.calc.*;
 import com.sun.star.beans.PropertyValue;
+import com.sun.star.container.NoSuchElementException;
 import com.sun.star.container.XEnumeration;
 import com.sun.star.container.XEnumerationAccess;
 import com.sun.star.frame.XComponentLoader;
@@ -28,18 +29,26 @@ import org.semanticwb.openoffice.WBOfficeException;
  * 
  * @author victor.lorenzana
  */
-public class WB4Application extends OfficeApplication
+public class WB4CalcApplication extends OfficeApplication
 {
-    private static final
-    String SCHEMA_FILE = "file:///";
 
+    private static final String SCHEMA_FILE = "file:///";
     private final XComponentContext m_xContext;
-
-    public WB4Application(XComponentContext m_xContext)
+    /**
+     * Create a representation of a Calc Application as Office Application
+     * @param m_xContext XComponentContext that represent a Calc Application
+     * @see XComponentContext
+     */
+    public WB4CalcApplication(XComponentContext m_xContext)
     {
         this.m_xContext = m_xContext;
     }
 
+    /**
+     * Gets the Calc documents opened
+     * @return List of OfficeDocument that represent the documents
+     * @throws org.semanticwb.openoffice.WBException In case that the Desktop of Office can not be used
+     */
     public List<OfficeDocument> getDocuments() throws WBException
     {
         ArrayList<OfficeDocument> documents = new ArrayList<OfficeDocument>();
@@ -53,14 +62,21 @@ public class WB4Application extends OfficeApplication
             XEnumeration enumeration = access.createEnumeration();
             while (enumeration.hasMoreElements())
             {
-                Object nextElement = enumeration.nextElement();
-                XComponent document = (XComponent) UnoRuntime.queryInterface(XComponent.class, nextElement);
-                XServiceInfo xServiceInfo = (XServiceInfo) UnoRuntime.queryInterface(
-                        XServiceInfo.class, document);
-                if (xServiceInfo.supportsService("com.sun.star.sheet.SpreadsheetDocument"))
+                try
                 {
-                    WB4Calc calDocument = new WB4Calc(document);
-                    documents.add(calDocument);
+                    Object nextElement = enumeration.nextElement();
+                    XComponent document = (XComponent) UnoRuntime.queryInterface(XComponent.class, nextElement);
+                    XServiceInfo xServiceInfo = (XServiceInfo) UnoRuntime.queryInterface(
+                            XServiceInfo.class, document);
+                    if (xServiceInfo.supportsService("com.sun.star.sheet.SpreadsheetDocument"))
+                    {
+                        WB4Calc calDocument = new WB4Calc(document);
+                        documents.add(calDocument);
+                    }
+                }
+                catch (NoSuchElementException nse)
+                {
+                    ErrorLog.log(nse);
                 }
             }
         }
@@ -71,6 +87,12 @@ public class WB4Application extends OfficeApplication
         return documents;
     }
 
+    /**
+     * Open a Document in a certain path
+     * @param file A File path of a Open Office document to Open
+     * @return OfficeDocument opened
+     * @throws org.wb.WBException If the document can not be opened
+     */
     public final OfficeDocument open(File file) throws WBException
     {
         // Obtener la factoria de servicios de OpenOffice   
@@ -88,11 +110,7 @@ public class WB4Application extends OfficeApplication
             String url = SCHEMA_FILE + file.getPath().replace('\\', '/');
             xCompLoader.loadComponentFromURL(url, "_blank", 0, loadProps);
             return new WB4Calc(m_xContext);
-        }
-        catch (com.sun.star.io.IOException e)
-        {
-            throw new WBOfficeException("No se puede abrir el documento", e);
-        }
+        }        
         catch (com.sun.star.uno.Exception e)
         {
             throw new WBOfficeException("No se puede abrir el documento", e);
