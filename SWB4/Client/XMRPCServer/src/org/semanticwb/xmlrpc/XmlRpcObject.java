75af8bfe31c5d6c3382fd7bc541d4af82c496edc
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-07-08 23:29:25 +0000

    RPCServer for comunication

---
+++ SWB4/Client/XMRPCServer/src/org/semanticwb/xmlrpc/XmlRpcObject.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.xmlrpc;
+
+import java.util.List;
+
+/**
+ *
+ * @author victor.lorenzana
+ */
+public class XmlRpcObject {
+    protected List<Part> parts;
+    public void setParts(List<Part> parts)
+    {
+        this.parts=parts;
+    }
+}
