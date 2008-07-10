98190c214509aa1f9eacf1c5a6c3a065b2575cde
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-07-10 17:45:18 +0000

    Soporte a Float

--- SWB4/Client/XMRPCServer/src/org/semanticwb/xmlrpc/XMLRPCServlet.java
+++ SWB4/Client/XMRPCServer/src/org/semanticwb/xmlrpc/XMLRPCServlet.java
@@ -44,16 +44,16 @@ public class XMLRPCServlet extends HttpServlet
         {
             Document xmlrpcDocument;
             List<Part> parts = new ArrayList<Part>();
-            if (isMultipart(request))
+            if ( isMultipart(request) )
             {
                 WBFileUpload upload = new WBFileUpload();
                 upload.getFiles(request);
-                for (String name : upload.getFileNames())
+                for ( String name : upload.getFileNames() )
                 {
-                    if(!name.equals("xmlrpc"))
+                    if ( !name.equals("xmlrpc") )
                     {
                         byte[] content = upload.getFileData(name);
-                        Part part = new Part(content,name,upload.getFileName(name));
+                        Part part = new Part(content, name, upload.getFileName(name));
                         parts.add(part);
                     }
                 }
@@ -72,39 +72,39 @@ public class XMLRPCServlet extends HttpServlet
                 Document docResponse = this.serialize(objResponse);
                 sendResponse(response, docResponse);
             }
-            catch (Exception cne)
+            catch ( Exception cne )
             {
                 try
                 {
                     Document docResponse = this.getException(cne);
                     sendResponse(response, docResponse);
                 }
-                catch (IOException ioeSendResponse)
+                catch ( IOException ioeSendResponse )
                 {
 
                 }
             }
         }
-        catch (JDOMException jde)
+        catch ( JDOMException jde )
         {
             try
             {
                 Document docResponse = this.getException(jde);
                 sendResponse(response, docResponse);
             }
-            catch (Exception ex)
+            catch ( Exception ex )
             {
 
             }
         }
-        catch (IOException ioe)
+        catch ( IOException ioe )
         {
             try
             {
                 Document docResponse = this.getException(ioe);
                 sendResponse(response, docResponse);
             }
-            catch (Exception ioeSendResponse)
+            catch ( Exception ioeSendResponse )
             {
 
             }
@@ -116,7 +116,7 @@ public class XMLRPCServlet extends HttpServlet
     {
         ServletOutputStream out = response.getOutputStream();
         XMLOutputter xMLOutputter = new XMLOutputter();
-        xMLOutputter.output(docResponse,out);      
+        xMLOutputter.output(docResponse, out);
         out.flush();
         out.close();
     }
@@ -138,46 +138,39 @@ public class XMLRPCServlet extends HttpServlet
         Element value = new Element("value");
         String type = "string";
         String svalue = "";
-        if (obj instanceof Integer)
+        if ( obj instanceof Integer )
         {
             type = "i4";
             svalue = obj.toString();
         }
+        else if ( obj instanceof String )
+        {
+            type = "string";
+            svalue = obj.toString();
+        }
+        else if ( obj instanceof Boolean )
+        {
+            type = "boolean";
+            svalue = (( Boolean ) obj).booleanValue() ? "1" : "0";
+        }
+        else if ( obj instanceof Date )
+        {
+            type = "dateTime.iso8601";
+            svalue = iso8601dateFormat.format(( Date ) obj);
+        }
+        else if ( obj instanceof Float )
+        {
+            type = "float";
+            svalue = obj.toString();
+        }
+        else if ( obj instanceof Double )
+        {
+            type = "double";
+            svalue = obj.toString();
+        }
         else
         {
-            if (obj instanceof String)
-            {
-                type = "string";
-                svalue = obj.toString();
-            }
-            else
-            {
-                if (obj instanceof Boolean)
-                {
-                    type = "boolean";
-                    svalue = ((Boolean) obj).booleanValue() ? "1" : "0";
-                }
-                else
-                {
-                    if (obj instanceof Date)
-                    {
-                        type = "dateTime.iso8601";                        
-                        svalue = iso8601dateFormat.format((Date) obj);
-                    }
-                    else
-                    {
-                        if (obj instanceof Double || obj instanceof Float)
-                        {
-                            type = "double";
-                            svalue = obj.toString();
-                        }
-                        else
-                        {
-                            throw new XmlRpcException("This kind of convertion is not possible");
-                        }
-                    }
-                }
-            }
+            throw new XmlRpcException("This kind of convertion is not possible");
         }
         Element elementType = new Element(type);
         elementType.setText(svalue);
@@ -187,9 +180,9 @@ public class XMLRPCServlet extends HttpServlet
 
     public void addParameters(Element params, Object[] pParams) throws XmlRpcException
     {
-        for (Object obj : pParams)
+        for ( Object obj : pParams )
         {
-            if (obj != null)
+            if ( obj != null )
             {
                 Element param = new Element("param");
                 addParameter(param, obj);
@@ -202,17 +195,21 @@ public class XMLRPCServlet extends HttpServlet
     private Class[] getParameterTypes(Object[] parameters)
     {
         Class[] classToReturn = new Class[parameters.length];
-        for (int i = 0; i < parameters.length; i++)
+        for ( int i = 0; i < parameters.length; i++ )
         {
-            if(parameters[i].getClass()==Integer.class)
+            if ( parameters[i].getClass() == Integer.class )
             {
                 classToReturn[i] = int.class;
             }
-            else if(parameters[i].getClass()==Double.class)
+            else if ( parameters[i].getClass() == Double.class )
             {
                 classToReturn[i] = double.class;
             }
-            else if(parameters[i].getClass()==Boolean.class)
+            else if ( parameters[i].getClass() == Float.class )
+            {
+                classToReturn[i] = float.class;
+            }
+            else if ( parameters[i].getClass() == Boolean.class )
             {
                 classToReturn[i] = boolean.class;
             }
@@ -227,7 +224,7 @@ public class XMLRPCServlet extends HttpServlet
     private String getClassFullPath(String objectName) throws ClassNotFoundException
     {
         String classFullPath = System.getProperty("org.semanticwb.xmlrpc." + objectName, null);
-        if (classFullPath == null)
+        if ( classFullPath == null )
         {
             throw new ClassNotFoundException("The class for he Object Name: " + objectName + " was not found");
         }
@@ -237,7 +234,7 @@ public class XMLRPCServlet extends HttpServlet
     private Object execute(String pCallMethod, Object[] parameters, List<Part> parts) throws ClassNotFoundException, XmlRpcException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException
     {
         String[] values = pCallMethod.split("\\.");
-        if (values.length != 2)
+        if ( values.length != 2 )
         {
             throw new XmlRpcException("The callMethos is incorrect");
         }
@@ -245,58 +242,50 @@ public class XMLRPCServlet extends HttpServlet
         String methodName = values[1];
         String classFullPath = getClassFullPath(objectName);
         Class clazz = Class.forName(classFullPath);
-        Class[] parameterTypes=this.getParameterTypes(parameters);        
-        Method methodToFind = clazz.getMethod(methodName,parameterTypes);
+        Class[] parameterTypes = this.getParameterTypes(parameters);
+        Method methodToFind = clazz.getMethod(methodName, parameterTypes);
         Object objToExecute = clazz.newInstance();
-        if (objToExecute instanceof XmlRpcObject)
+        if ( objToExecute instanceof XmlRpcObject )
         {
-            XmlRpcObject xmlRpcObject = (XmlRpcObject) objToExecute;
+            XmlRpcObject xmlRpcObject = ( XmlRpcObject ) objToExecute;
             xmlRpcObject.setParts(parts);
-        }                
+        }
         return methodToFind.invoke(objToExecute, parameters);
-        
+
     }
 
     private Object getParameter(Element eType) throws ParseException
     {
         Object res = null;
         String name = eType.getName();
-        if (name.equalsIgnoreCase("i4") || name.equalsIgnoreCase("int"))
+        if ( name.equalsIgnoreCase("i4") || name.equalsIgnoreCase("int") )
         {
             res = Integer.parseInt(eType.getText());
         }
+        else if ( name.equalsIgnoreCase("boolean") )
+        {
+            res = eType.getText().equals("1") ? true : false;
+        }
+        else if ( name.equalsIgnoreCase("dateTime.iso8601") )
+        {
+            String dateTime = eType.getText();
+            res = iso8601dateFormat.parse(dateTime);
+        }
+        else if ( name.equalsIgnoreCase("float") )
+        {
+            res = Float.parseFloat(eType.getText());
+        }
+        else if ( name.equalsIgnoreCase("double") )
+        {
+            res = Double.parseDouble(eType.getText());
+        }
+        else if ( name.equalsIgnoreCase("string") )
+        {
+            res = eType.getText();
+        }
         else
         {
-            if (name.equalsIgnoreCase("boolean"))
-            {
-                res = eType.getText().equals("1") ? true : false;
-            }
-            else
-            {
-                if (name.equalsIgnoreCase("dateTime.iso8601"))
-                {                    
-                    String dateTime=eType.getText();                                        
-                    res = iso8601dateFormat.parse(dateTime);
-                }
-                else
-                {
-                    if (name.equalsIgnoreCase("double"))
-                    {
-                        res = Double.parseDouble(eType.getText());
-                    }
-                    else
-                    {
-                        if (name.equalsIgnoreCase("string"))
-                        {
-                            res = eType.getText();
-                        }
-                        else
-                        {
-                            res = null;
-                        }
-                    }
-                }
-            }
+            res = null;
         }
         return res;
 
@@ -304,14 +293,14 @@ public class XMLRPCServlet extends HttpServlet
 
     private Object[] getParameters(Document document) throws JDOMException, ParseException
     {
-        ArrayList<Object> parameters = new ArrayList<Object>();             
+        ArrayList<Object> parameters = new ArrayList<Object>();
         List values = XPath.selectNodes(document, "/methodCall/params/param/value");
-        for (Object oElement : values)
+        for ( Object oElement : values )
         {
-            Element eValue = (Element) oElement;
-            for (Object objElementType : eValue.getChildren())
+            Element eValue = ( Element ) oElement;
+            for ( Object objElementType : eValue.getChildren() )
             {
-                Element eType = (Element) objElementType;
+                Element eType = ( Element ) objElementType;
                 parameters.add(getParameter(eType));
             }
         }
@@ -320,27 +309,27 @@ public class XMLRPCServlet extends HttpServlet
 
     private String getCallMethod(Document document) throws XmlRpcException, JDOMException
     {
-        Element methodName = (Element) XPath.selectSingleNode(document.getRootElement(), "/methodCall/methodName");
-        if (methodName == null)
+        Element methodName = ( Element ) XPath.selectSingleNode(document.getRootElement(), "/methodCall/methodName");
+        if ( methodName == null )
         {
             throw new XmlRpcException("The methodName tag was not found");
         }
         return methodName.getText();
     }
 
-    private Document getException(Exception e) throws JDOMException,IOException
+    private Document getException(Exception e) throws JDOMException, IOException
     {
-        String xmlString="<?xml version=\"1.0\" encoding=\"utf-8\"?><methodResponse><fault><value><struct><member><name>faultCode</name>" +
-                "<value><int>"+ e.hashCode() +"</int></value></member><member><name>faultString</name>" +
+        String xmlString = "<?xml version=\"1.0\" encoding=\"utf-8\"?><methodResponse><fault><value><struct><member><name>faultCode</name>" +
+                "<value><int>" + e.hashCode() + "</int></value></member><member><name>faultString</name>" +
                 "<value><string>" + e.toString() + "</string></value></member></struct>" +
                 "</value></fault></methodResponse>";
-        Reader stringReader=new StringReader(xmlString);
-        SAXBuilder builder=new SAXBuilder();
+        Reader stringReader = new StringReader(xmlString);
+        SAXBuilder builder = new SAXBuilder();
         return builder.build(stringReader);
     }
 
     private Document getDocument(byte[] document) throws JDOMException, IOException
-    {        
+    {
         ByteArrayInputStream in = new ByteArrayInputStream(document);
         SAXBuilder builder = new SAXBuilder();
         return builder.build(in);
@@ -356,7 +345,7 @@ public class XMLRPCServlet extends HttpServlet
 
     private boolean isMultipart(HttpServletRequest request)
     {
-        if (request.getContentType().indexOf("multipart/form-data") != -1)
+        if ( request.getContentType().indexOf("multipart/form-data") != -1 )
         {
             return true;
         }
