668dedcb043a6c01fdb5920be1d6f26199e4b6c8
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-07-14 20:39:18 +0000

    Cambio para soporte a autenticación basica

--- SWB4/Client/XMLRPCClient/src/org/semanticwb/xmlrpc/XmlRpcClientConfig.java
+++ SWB4/Client/XMLRPCClient/src/org/semanticwb/xmlrpc/XmlRpcClientConfig.java
@@ -13,9 +13,65 @@ import java.net.URI;
  */
 public class XmlRpcClientConfig {
     private URI ServerURI;
-    public XmlRpcClientConfig()
+    private URI proxyServer;
+    private int proxyPort;
+    private String userName;
+    private String password;
+    public XmlRpcClientConfig(URI ServerURI)
     {
-        
+        this.ServerURI=ServerURI;
+    }
+    public XmlRpcClientConfig(URI ServerURI,URI proxyServer,int proxyPort)
+    {
+        this(ServerURI);
+        this.proxyServer=proxyServer;
+        this.proxyPort=proxyPort;
+    }
+    public XmlRpcClientConfig(URI ServerURI,URI proxyServer,int proxyPort,String user,String password)
+    {
+        this(ServerURI,proxyServer,proxyPort);
+        this.userName=user;
+        this.password=password;
+    }
+    public XmlRpcClientConfig(URI ServerURI,String user,String password)
+    {        
+        this(ServerURI);
+        this.userName=user;
+        this.password=password;
+    }
+    
+    
+    public boolean hasUserPassWord()
+    {
+        if(this.userName==null || password==null)
+        {
+            return false;
+        }
+        return true;
+    }
+    public String getUserName()
+    {
+        return userName;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+    public boolean usesProxyServer()
+    {
+        if(this.proxyServer()==null || proxyPort==0)
+        {
+            return false;
+        }
+        return true;
+    }
+    public URI proxyServer()
+    {
+        return proxyServer;    
+    }
+    public int getProxyPort()
+    {
+        return proxyPort;    
     }
     public URI getServerURI()
     {
