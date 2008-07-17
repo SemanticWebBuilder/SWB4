532822ff4bf4d72024c0546d9f060a3cd0173ac2
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-07-17 19:27:52 +0000

    Pruebas de generación de ontologia

---
+++ SWB4/swb/SWBPlatform/test/org/semanticwb/codegen/CodeGeneratorTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.semanticwb.codegen;
+
+import java.io.File;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.semanticwb.SWBContext;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author victor.lorenzana
+ */
+public class CodeGeneratorTest
+{
+
+    public CodeGeneratorTest()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+        SWBContext.createInstance(null);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void generateCode()
+    {
+        try
+        {
+            File dir = new File("C:\\temp\\WBObjectTest\\src");
+            String sPakage = "com.demo.infotec";
+            CodeGenerator codeGeneration = new CodeGenerator(dir, sPakage);
+            codeGeneration.generateCode();
+        }
+        catch ( CodeGeneratorException cge )
+        {
+            fail(cge.getMessage());
+        }
+    }
+}
