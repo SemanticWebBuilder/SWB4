d8021563e27ded00e5196efa64981f36f13c8e0b
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-07-22 23:07:40 +0000

    Soporte a Attachments sin probar

--- SWB4/Client/XMLRPCClient/src/org/semanticwb/xmlrpc/XmlRpcProxyFactory.java
+++ SWB4/Client/XMLRPCClient/src/org/semanticwb/xmlrpc/XmlRpcProxyFactory.java
@@ -16,9 +16,11 @@ import java.util.List;
  */
 public class XmlRpcProxyFactory implements java.lang.reflect.InvocationHandler, XmlProxy
 {
-    List<Attachment> attachments=new ArrayList<Attachment>();
+
+    List<Attachment> attachments = new ArrayList<Attachment>();
     private URI Uri;
-    private String user,password;
+    private String user,  password;
+
     public URI getUri()
     {
         return Uri;
@@ -28,32 +30,37 @@ public class XmlRpcProxyFactory implements java.lang.reflect.InvocationHandler, 
     {
         this.Uri = uri;
     }
-    
+
     public String getUser()
     {
         return user;
     }
+
     public void setUser(String user)
     {
-        this.user=user;
+        this.user = user;
     }
-    
+
     public String getPassword()
     {
         return this.password;
     }
+
     public void setPassword(String password)
     {
-        this.password=password;
+        this.password = password;
     }
+
     public void addAttachment(Attachment attachment)
     {
         this.attachments.add(attachment);
     }
+
     public void clearAttachments()
     {
         this.attachments.clear();
     }
+
     public static <T> T newInstance(java.lang.Class<T> clazz)
     {
         Class[] interfaces = {clazz};
@@ -69,25 +76,25 @@ public class XmlRpcProxyFactory implements java.lang.reflect.InvocationHandler, 
     public Object invoke(Object proxy, Method m, Object[] args)
             throws Throwable
     {
-        Object ObjectToreturn=null;
+        Object ObjectToreturn = null;
         if ( m.getName().equals("getUri") )
         {
-            ObjectToreturn=this.Uri;
+            ObjectToreturn = this.Uri;
         }
         else if ( m.getName().equals("setUri") )
         {
             URI uri = ( URI ) args[0];
-            this.setUri(uri);            
+            this.setUri(uri);
         }
         else if ( m.getName().equals("setUser") )
         {
             String user = ( String ) args[0];
-            this.setUser(user);            
+            this.setUser(user);
         }
         else if ( m.getName().equals("setPassword") )
         {
             String password = ( String ) args[0];
-            this.setPassword(password);            
+            this.setPassword(password);
         }
         else if ( m.getName().equals("getUser") )
         {
@@ -101,31 +108,30 @@ public class XmlRpcProxyFactory implements java.lang.reflect.InvocationHandler, 
         {
             Attachment attachment = ( Attachment ) args[0];
             this.addAttachment(attachment);
-            
+
         }
         else if ( m.getName().equals("clearAttachments") )
-        {            
-            this.clearAttachments();            
-        }        
+        {
+            this.clearAttachments();
+        }
         else
         {
-            String methodName = "" + m.getName();            
+            String methodName = m.getClass().getName() + "." + m.getName();
             AnnotatedClass annotatedClass = AnnotationManager.getAnnotatedClass(m.getDeclaringClass());
             if ( annotatedClass != null )
             {
                 AnnotatedMethod annotatedMethod = annotatedClass.getAnnotatedMethod(m);
                 if ( annotatedMethod != null && annotatedMethod.getAnnotation(XmlRpcMethod.class) != null )
                 {
-                    XmlRpcMethod xmlRpcMethod = ( XmlRpcMethod ) annotatedMethod.getAnnotation(XmlRpcMethod.class);                    
+                    XmlRpcMethod xmlRpcMethod = ( XmlRpcMethod ) annotatedMethod.getAnnotation(XmlRpcMethod.class);
                     methodName = xmlRpcMethod.methodName();
-                    
                 }
             }
             XmlRpcClientConfig config = new XmlRpcClientConfig(this.Uri);
             config.setPassword(password);
             config.setUserName(user);
             XmlRpcClient<Object> xmlclient = new XmlRpcClient<Object>(config);
-            ObjectToreturn = xmlclient.execute(methodName, args,this.attachments);
+            ObjectToreturn = xmlclient.execute(methodName, args, this.attachments);
         }
         return ObjectToreturn;
     }
