3a475781369755a51d59a7970d220748b4b67276
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-07-23 22:19:31 +0000

    Interfaces comunes entre cliente y servidor para objetos remotos

---
+++ SWB4/Client/OpenOffice/OfficeComunicationInterfaces/src/org/semanticwb/office/interfaces/IOfficeApplication.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.office.interfaces;
+
+/**
+ *
+ * @author victor.lorenzana
+ */
+public interface IOfficeApplication {
+    public static String version="1.0";
+    public boolean isValidVersion(String version);            
+}
