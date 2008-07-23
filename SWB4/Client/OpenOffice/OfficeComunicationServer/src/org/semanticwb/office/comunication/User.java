365a07594163e280b3227395090a179f761c948f
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-07-23 22:20:57 +0000

    Implementación de interfaces de objetos remotos para servidor, no hay versión para cliente ya que utiliza versión Interface

---
+++ SWB4/Client/OpenOffice/OfficeComunicationServer/src/org/semanticwb/office/comunication/User.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.office.comunication;
+
+import org.semanticwb.xmlrpc.XmlRpcObject;
+
+/**
+ *
+ * @author victor.lorenzana
+ */
+public class User extends XmlRpcObject
+{
+    public void changePassword(String newPassword)
+    {
+        
+    }
+}
---
+++ SWB4/Client/OpenOffice/OfficeComunicationServer/src/org/semanticwb/office/comunication/WebPage.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.office.comunication;
+
+import org.semanticwb.xmlrpc.XmlRpcObject;
+
+/**
+ *
+ * @author victor.lorenzana
+ */
+public class WebPage extends XmlRpcObject
+{
+    public void create(String title,String description,String path)
+    {
+        
+    }
+}
