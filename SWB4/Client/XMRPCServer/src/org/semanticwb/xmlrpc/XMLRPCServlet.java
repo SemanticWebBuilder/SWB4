3fc3940cf498a32e46fd3d0710481fccd9369bdd
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-07-23 22:11:36 +0000

    Mejora por PMD

--- SWB4/Client/XMRPCServer/src/org/semanticwb/xmlrpc/XMLRPCServlet.java
+++ SWB4/Client/XMRPCServer/src/org/semanticwb/xmlrpc/XMLRPCServlet.java
@@ -128,7 +128,10 @@ public abstract class XMLRPCServlet extends HttpServlet
             }
             catch ( Exception ex )
             {
-                 
+                // No se puede hacer nada, no puede seralizar la respuesta, debe guardar el error en el log
+                // TODO:
+                ex.printStackTrace(System.out);
+                
             }
         }        
 
