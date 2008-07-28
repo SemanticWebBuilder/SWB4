ab51a8d27709156c8330b8643a9c7f7b9e97c77c
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-07-28 22:31:27 +0000

    Interfaces de implementación XmlRpc

---
+++ SWB4/Client/OpenOffice/SWBOfficeCore/src/org/semanticwb/openoffice/interfaces/IOpenOfficeApplication.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.openoffice.interfaces;
+
+import org.semanticwb.office.interfaces.IOfficeApplication;
+import org.semanticwb.xmlrpc.XmlProxy;
+
+/**
+ *
+ * @author victor.lorenzana
+ */
+public interface IOpenOfficeApplication extends IOfficeApplication,XmlProxy{
+
+}
