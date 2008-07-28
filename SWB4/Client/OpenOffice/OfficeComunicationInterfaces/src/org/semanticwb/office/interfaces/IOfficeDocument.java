c3cfad2e1b086399da631e16631d8ba34064b054
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-07-28 23:58:42 +0000

    SE agregaron los xmlrpcmethods

--- SWB4/Client/OpenOffice/OfficeComunicationInterfaces/src/org/semanticwb/office/interfaces/IOfficeDocument.java
+++ SWB4/Client/OpenOffice/OfficeComunicationInterfaces/src/org/semanticwb/office/interfaces/IOfficeDocument.java
@@ -4,18 +4,28 @@
  */
 
 package org.semanticwb.office.interfaces;
+
+import org.semanticwb.xmlrpc.XmlRpcMethod;
+
 /**
  *
  * @author victor.lorenzana
  */
 public interface IOfficeDocument
 {
+    @XmlRpcMethod(methodName="OfficeDocument.publish")
     public int publish(String title,String description,String path);
+    @XmlRpcMethod(methodName="OfficeDocument.getPath")
     public String getPath(int contentID);
+    @XmlRpcMethod(methodName="OfficeDocument.setTitle")
     public void setTitle(int contentID,String title);
+    @XmlRpcMethod(methodName="OfficeDocument.setDescription")
     public void setDescription(int contentID,String description);
+    @XmlRpcMethod(methodName="OfficeDocument.setPath")
     public void setPath(int contentID,String path);
+    @XmlRpcMethod(methodName="OfficeDocument.setActive")
     public void setActive(int contentID,boolean active);
+    @XmlRpcMethod(methodName="OfficeDocument.setActive")
     public boolean getActive(int contentID);
     public void delete(int contentID);
     public boolean exists(int contentId);
