2c68db5155303acc1ddf408ff68569dbdce85caf
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-07-30 20:54:30 +0000

    Mejoras en mensajes de error

--- SWB4/Client/OpenOffice/SWBOfficeCore/test/org/semanticwb/openoffice/test/LoginTest.java
+++ SWB4/Client/OpenOffice/SWBOfficeCore/test/org/semanticwb/openoffice/test/LoginTest.java
@@ -4,15 +4,19 @@
  */
 package org.semanticwb.openoffice.test;
 
+import java.util.Locale;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.semanticwb.openoffice.Configuration;
 import org.semanticwb.openoffice.ConfigurationListURI;
 import org.semanticwb.openoffice.ErrorLog;
+import org.semanticwb.openoffice.ui.dialogs.DialogError;
 import org.semanticwb.openoffice.ui.dialogs.DialogLogin;
 import static org.junit.Assert.*;
 
@@ -30,6 +34,16 @@ public class LoginTest
     @BeforeClass
     public static void setUpClass() throws Exception
     {
+        Locale.setDefault(new Locale("es"));
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch ( Exception ue )
+        {
+            // No debe hacer nada
+            System.out.println(ue.getMessage());
+        }
         System.setProperty(ConfigurationListURI.CONFIGURATION, "c:\\temp\\list.xml");
         System.setProperty(Configuration.CONFIGURATION_PROPERTY_NAME, "c:\\temp\\config.xml");
         System.setProperty(ErrorLog.CONFIGURATION, "c:\\temp");
@@ -54,10 +68,27 @@ public class LoginTest
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
-    public void DialogTest()
+    @Ignore
+    public void DialogLoginTest()
     {
         ConfigurationListURI conf = new ConfigurationListURI();
         DialogLogin login = new DialogLogin(new JFrame(), true);
         login.setVisible(true);
+    }
+
+    @Test
+    public void DialogErrorTest()
+    {
+        try
+        {
+            throw new Exception("Mensaje");
+        }
+        catch ( Exception e )
+        {
+            DialogError error = new DialogError(new javax.swing.JFrame("Error"), true, e);            
+            error.setTitle("");
+            error.setLocationRelativeTo(null);
+            error.setVisible(true);
+        }
     }
 }
