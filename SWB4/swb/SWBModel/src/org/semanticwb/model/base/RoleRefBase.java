c2100f8db0cf89190f425aab37eb810b32d1414a
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-08-01 17:55:42 +0000

    Privera versión de objetos

---
+++ SWB4/swb/SWBModel/src/org/semanticwb/model/base/RoleRefBase.java
@@ -0,0 +1,50 @@
+package org.semanticwb.model.base;
+
+import java.util.Date;
+import org.semanticwb.platform.SemanticObject;
+import org.semanticwb.model.*;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import org.semanticwb.platform.SemanticIterator;
+
+public class RoleRefBase extends SemanticObject 
+{
+    SWBVocabulary vocabulary=SWBContext.getVocabulary();
+
+    public RoleRefBase(com.hp.hpl.jena.rdf.model.Resource res)
+    {
+        super(res);
+    }
+
+    public SemanticIterator<org.semanticwb.model.Role> listRole()
+    {
+        StmtIterator stit=getRDFResource().listProperties(vocabulary.hasRole.getRDFProperty());
+        return new SemanticIterator<org.semanticwb.model.Role>(org.semanticwb.model.Role.class, stit);
+    }
+
+    public void addRole(org.semanticwb.model.Role role)
+    {
+        addObjectProperty(vocabulary.hasRole, role);
+    }
+
+    public void removeAllRole()
+    {
+        getRDFResource().removeAll(vocabulary.hasRole.getRDFProperty());
+    }
+
+    public Role getRole()
+    {
+         StmtIterator stit=getRDFResource().listProperties(vocabulary.hasRole.getRDFProperty());
+         SemanticIterator<org.semanticwb.model.Role> it=new SemanticIterator<org.semanticwb.model.Role>(Role.class, stit);
+         return it.next();
+    }
+
+    public int getStatus()
+    {
+        return getIntProperty(vocabulary.status);
+    }
+
+    public void setStatus(int status)
+    {
+        setLongProperty(vocabulary.status, status);
+    }
+}
