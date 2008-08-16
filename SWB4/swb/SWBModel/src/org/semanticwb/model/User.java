690d819f673f36a1e94bc70cb0385bc2ecc665e7
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2008-08-16 02:31:11 +0000

    Soporte para JAAS, interface Principal

--- SWB4/swb/SWBModel/src/org/semanticwb/model/User.java
+++ SWB4/swb/SWBModel/src/org/semanticwb/model/User.java
@@ -1,9 +1,10 @@
 package org.semanticwb.model;
 
+import java.io.Serializable;
 import java.security.Principal;
 import org.semanticwb.model.base.*;
 
-public class User extends UserBase implements Principal
+public class User extends UserBase implements Principal, Serializable
 {
     public User(com.hp.hpl.jena.rdf.model.Resource res)
     {
