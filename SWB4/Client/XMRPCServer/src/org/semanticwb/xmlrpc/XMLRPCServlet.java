77940caab9e3cb5ad3d99f1b7701dace498123b4
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-08-18 22:26:29 +0000

    Mejoras para modulo comun de serialización y deserialización

--- SWB4/Client/XMRPCServer/src/org/semanticwb/xmlrpc/XMLRPCServlet.java
+++ SWB4/Client/XMRPCServer/src/org/semanticwb/xmlrpc/XMLRPCServlet.java
@@ -8,18 +8,11 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.servlet.ServletInputStream;
@@ -35,7 +28,7 @@ import org.jdom.input.SAXBuilder;
 import org.jdom.output.XMLOutputter;
 import org.jdom.xpath.XPath;
 import static org.semanticwb.xmlrpc.Base64.decode;
-
+import static org.semanticwb.xmlrpc.XmlRpcSerializer.*;
 /**
  *
  * @author victor.lorenzana
@@ -45,12 +38,11 @@ public abstract class XMLRPCServlet extends HttpServlet
     private static final
     String RETURN = "\r\n";
 
-    Hashtable<String, Object> cacheObjects = new Hashtable<String, Object>();
+    private static Hashtable<String, Object> cacheObjects = new Hashtable<String, Object>();
     private static final String PREFIX_PROPERTY_PATH = "org.semanticwb.xmlrpc.";
     private static final String XMLRPC_DOCUMENT = "xmlrpc";
     private static String realm = "Secure Area";
-    private static String prefixBasic = "Basic ";
-    private static SimpleDateFormat iso8601dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    private static String prefixBasic = "Basic ";    
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -112,12 +104,12 @@ public abstract class XMLRPCServlet extends HttpServlet
                 {
 
                     String methodName = getMethodName(xmlrpcDocument);
-                    ArrayList<Method> methods = getMethods(methodName);
-                    Object[] parameters = deserialize(xmlrpcDocument, methods);
+                    ArrayList<Method> methods = getMethods(methodName);                                        
+                    Object[] parameters = deserializeRequest(xmlrpcDocument, methods);
                     Method method = getMethod(methodName, parameters, methods);
                     String objectName = method.getDeclaringClass().getName();
                     Object objResponse = execute(objectName, method, parameters, parts, pUserName, pPassword);
-                    Document docResponse = this.serialize(objResponse);
+                    Document docResponse = serializeResponse(objResponse);
                     sendResponse(response, docResponse);
                 }
             }
@@ -152,7 +144,7 @@ public abstract class XMLRPCServlet extends HttpServlet
 
     }
 
-    protected void beforeExecute(Object objToExecute, Set<Part> parts, String user, String password) throws Exception
+    protected static void beforeExecute(Object objToExecute, Set<Part> parts, String user, String password) throws Exception
     {
         if ( objToExecute instanceof XmlRpcObject )
         {
@@ -161,7 +153,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         }
     }
 
-    protected void afterExecute(Object objToExecute)
+    protected static void afterExecute(Object objToExecute)
     {
         if ( objToExecute instanceof XmlRpcObject )
         {
@@ -170,7 +162,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         }
     }
 
-    private String getUserName(String userpassDecoded) throws IOException
+    private static String getUserName(String userpassDecoded) throws IOException
     {
         String userName = "";
         String[] values = userpassDecoded.split(":");
@@ -178,7 +170,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         return userName;
     }
 
-    public Method getMethod(String methodName, Object[] parameters, ArrayList<Method> methods) throws NoSuchMethodException
+    public static Method getMethod(String methodName, Object[] parameters, ArrayList<Method> methods) throws NoSuchMethodException
     {
         switch ( methods.size() )
         {
@@ -189,7 +181,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         throw new NoSuchMethodException("The method " + methodName + "was not found");
     }
 
-    private String getPassword(String userpassDecoded) throws IOException
+    private static String getPassword(String userpassDecoded) throws IOException
     {
         String password = "";
         String[] values = userpassDecoded.split(":");
@@ -205,7 +197,7 @@ public abstract class XMLRPCServlet extends HttpServlet
      */
     public abstract boolean isAuthenticate(String pUserName, String pPassword);
 
-    private void sendResponse(ServletResponse response, Document docResponse) throws IOException
+    private static void sendResponse(ServletResponse response, Document docResponse) throws IOException
     {
         response.setContentType("text/xml");
         ServletOutputStream out = response.getOutputStream();
@@ -215,131 +207,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         out.close();
     }
 
-    private Document serialize(Object value) throws XmlRpcException
-    {
-        Document doc = new Document();
-        Element methodResponse = new Element("methodResponse");
-        doc.setRootElement(methodResponse);
-        Element params = new Element("params");
-        methodResponse.addContent(params);
-        Object[] pParams = {value};
-        addParameters(params, pParams);
-        return doc;
-    }
-
-    public void addParameter(Element param, Object obj) throws XmlRpcException
-    {
-        Element value = new Element("value");
-        param.addContent(value);
-        String type = "string";
-        String svalue = "";
-        if ( obj instanceof Integer )
-        {
-            type = "i4";
-            svalue = obj.toString();
-        }
-        else if ( obj instanceof String )
-        {
-            type = "string";
-            svalue = obj.toString();
-        }
-        else if ( obj instanceof Boolean )
-        {
-            type = "boolean";
-            svalue = (( Boolean ) obj).booleanValue() ? "1" : "0";
-        }
-        else if ( obj instanceof Date )
-        {
-            type = "dateTime.iso8601";
-            svalue = iso8601dateFormat.format(( Date ) obj);
-        }
-        else if ( obj instanceof Float )
-        {
-            type = "float";
-            svalue = obj.toString();
-        }
-        else if ( obj instanceof Double )
-        {
-            type = "double";
-            svalue = obj.toString();
-        }
-        else if ( obj.getClass().isArray() )
-        {
-            type = "array";
-            svalue = null;
-            Element elementType = new Element(type);
-            value.setContent(elementType);
-            addArray(obj, elementType);
-        }
-        else
-        {
-            //throw new XmlRpcException("This kind of convertion is not possible");
-            type = "struct";
-            svalue = null;
-            Element elementType = new Element(type);
-            value.setContent(elementType);
-            addStruct(obj, elementType);
-        }
-        if ( svalue != null )
-        {
-            Element elementType = new Element(type);
-            value.setContent(elementType);
-            elementType.setText(svalue);
-
-        }
-    }
-
-    private void addStruct(Object obj, Element structElement) throws XmlRpcException
-    {
-        Class clazz = obj.getClass();
-        for ( Field field : clazz.getDeclaredFields() )
-        {
-            try
-            {
-                Element member = new Element("member");
-                Element name = new Element("name");
-                name.setText(field.getName());
-                member.addContent(name);
-                Element value = new Element("value");
-                addParameter(member, field.get(obj));
-                member.addContent(value);
-                structElement.addContent(member);
-            }
-            catch ( IllegalAccessException iae )
-            {
-                iae.printStackTrace(System.out);
-            }
-
-        }
-    }
-
-    private void addArray(Object obj, Element arrayElement) throws XmlRpcException
-    {
-        Element data = new Element("data");
-        arrayElement.addContent(data);
-        int len = Array.getLength(obj);
-        for ( int i = 0; i < len; i++ )
-        {
-            Object value = Array.get(obj, i);
-            addParameter(data, value);
-        }
-    }
-
-    public void addParameters(Element params, Object[] pParams) throws XmlRpcException
-    {
-        for ( Object obj : pParams )
-        {
-            if ( obj != null )
-            {
-                Element param = new Element("param");
-                addParameter(param, obj);
-                params.addContent(param);
-            }
-
-        }
-    }
-
-    private String getClassFullPath(String objectName) throws ClassNotFoundException
+    private static String getClassFullPath(String objectName) throws ClassNotFoundException
     {
         String classFullPath = System.getProperty(PREFIX_PROPERTY_PATH + objectName, null);
         if ( classFullPath == null )
@@ -349,7 +217,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         return classFullPath;
     }
 
-    private Object execute(String objectName, Method method, Object[] parameters, Set<Part> parts, String user, String password) throws ClassNotFoundException, XmlRpcException, InstantiationException, IllegalAccessException, NoSuchMethodException
+    private static Object execute(String objectName, Method method, Object[] parameters, Set<Part> parts, String user, String password) throws ClassNotFoundException, XmlRpcException, InstantiationException, IllegalAccessException, NoSuchMethodException
     {
         Class clazz = method.getDeclaringClass();
 
@@ -380,205 +248,8 @@ public abstract class XMLRPCServlet extends HttpServlet
                 throw new XmlRpcException(inte.getTargetException().getLocalizedMessage(), inte.getTargetException());
             }
         }
-
-
     }
-
-    private <T> T convertString(Class<T> clazz, String data) throws XmlRpcException
-    {
-        if(clazz==String.class)
-        {
-            return ( T ) data;
-        }
-        throw new XmlRpcException("The data are incopatibles, can not be converted String to "+clazz.getName());
-    }
-
-    private <T> T convertInteger(Class<T> clazz, int data) throws XmlRpcException
-    {
-        if(clazz==Integer.class || clazz==int.class)
-        {
-            return ( T ) new Integer(data);
-        }
-        else
-        {
-            throw new XmlRpcException("The data are incopatibles, can not be converted int to "+clazz.getName());
-        }
-    }
-
-    private <T> T convertBoolean(Class<T> clazz, boolean data) throws XmlRpcException
-    {
-        if(clazz==Boolean.class || clazz==boolean.class)
-        {
-            return ( T ) new Boolean(data);
-        }
-        else
-        {
-            throw new XmlRpcException("The data are incopatibles, can not be converted boolean to "+clazz.getName());
-        }
-    }
-
-    private <T> T convertFloat(Class<T> clazz, float data) throws XmlRpcException
-    {
-        if(clazz==Float.class || clazz==float.class)
-        {
-            return ( T ) new Float(data);
-        }
-        else
-        {
-            throw new XmlRpcException("The data are incopatibles, can not be converted float to "+clazz.getName());
-        }
-    }
-
-    private <T> T convertDouble(Class<T> clazz, double data) throws XmlRpcException
-    {
-        if(clazz==Double.class || clazz==double.class)
-        {
-            return ( T ) new Double(data);
-        }
-        else
-        {
-            throw new XmlRpcException("The data are incopatibles, can not be converted double to "+clazz.getName());
-        }
-    }
-
-    private <T> T convertDate(Class<T> clazz, Date data) throws XmlRpcException
-    {
-        if ( clazz==data.getClass() )
-        {
-            return ( T ) data;
-        }
-        throw new XmlRpcException("The data are incopatibles, can not be converted Date to "+clazz.getName());
-    }
-
-    private <T> T getParameter(Class<T> clazz, Element eType) throws ParseException, JDOMException, XmlRpcException
-    {
-        T res = null;
-        String name = eType.getName();
-        if ( name.equalsIgnoreCase("i4") || name.equalsIgnoreCase("int") )
-        {
-            res = convertInteger(clazz, Integer.parseInt(eType.getText()));
-        }
-        else if ( name.equalsIgnoreCase("boolean") )
-        {
-            res = convertBoolean(clazz, eType.getText().equals("1") ? true : false);
-        }
-        else if ( name.equalsIgnoreCase("dateTime.iso8601") )
-        {
-            String dateTime = eType.getText();
-            res = convertDate(clazz, iso8601dateFormat.parse(dateTime));
-        }
-        else if ( name.equalsIgnoreCase("float") )
-        {
-            res = convertFloat(clazz, Float.parseFloat(eType.getText()));
-        }
-        else if ( name.equalsIgnoreCase("double") )
-        {
-            res = convertDouble(clazz, Double.parseDouble(eType.getText()));
-        }
-        else if ( name.equalsIgnoreCase("string") )
-        {
-            res = convertString(clazz, eType.getText());
-        }
-        else if ( name.equalsIgnoreCase("array") )
-        {
-            res = deserializeArray(clazz, eType);
-        }
-        /*else if ( name.equalsIgnoreCase("struct") )
-        {
-            res = deserializeStruct(clazz, eType);
-        }*/
-        else
-        {
-            res = null;
-        }
-        return res;
-
-    }
-
-    /*private <T> T deserializeStruct(Class<T> clazz, Element struct) throws JDOMException, ParseException
-    {
-        List listValues = XPath.selectNodes(struct, "./member");
-        return null;
-    }*/
-
-    private <T> T deserializeArray(Class<T> clazz, Element array) throws JDOMException, ParseException, XmlRpcException
-    {
-        if ( clazz.isArray() )
-        {
-
-            List listValues = XPath.selectNodes(array, "./data/value");
-            Class componentType = clazz.getComponentType();
-            Object arrayToReturn = Array.newInstance(componentType, listValues.size());
-            int i = 0;
-            for ( Object objValue : listValues )
-            {
-                Element eValue = ( Element ) objValue;
-                Iterator itValues = eValue.getDescendants();
-                while (itValues.hasNext())
-                {
-                    Object child = itValues.next();
-                    if ( child instanceof Element )
-                    {
-                        Element eType = ( Element ) child;
-                        Object value = getParameter(componentType, eType);
-                        Array.set(arrayToReturn, i, value);
-                    }
-                }
-                i++;
-            }
-            return clazz.cast(arrayToReturn);
-        }
-        else
-        {
-            throw new XmlRpcException("The value of return must be an array");
-        }        
-    }
-
-    private ArrayList<Method> selectMethodsWithSameNumberOfParameters(ArrayList<Method> methods, int parameters)
-    {
-        ArrayList<Method> newmethods = new ArrayList<Method>();
-        for ( Method m : methods )
-        {
-            if ( m.getParameterTypes().length == parameters )
-            {
-                newmethods.add(m);
-            }
-        }
-        return newmethods;
-    }
-
-    private Object[] deserialize(Document document, ArrayList<Method> methods) throws JDOMException, ParseException, XmlRpcException
-    {
-        ArrayList<Method> newMethods = new ArrayList<Method>();
-        ArrayList<Object> parameters = new ArrayList<Object>();
-        for ( Method method : methods )
-        {
-            try
-            {
-                List values = XPath.selectNodes(document, "/methodCall/params/param/value");
-                methods = selectMethodsWithSameNumberOfParameters(methods, values.size());                
-                for ( Object oElement : values )
-                {
-                    Class expectedClass = method.getParameterTypes()[0];
-                    Element eValue = ( Element ) oElement;
-                    for ( Object objElementType : eValue.getChildren() )
-                    {
-                        Element eType = ( Element ) objElementType;
-                        parameters.add(getParameter(expectedClass, eType));
-                    }                    
-                }
-                newMethods.add(method);
-            }
-            catch ( Exception e )
-            {
-                parameters = new ArrayList<Object>();
-                break;
-            }
-        }
-        return parameters.toArray();
-    }
-
-    private String getMethodName(Document document) throws XmlRpcException, JDOMException, ClassNotFoundException
+    private static String getMethodName(Document document) throws XmlRpcException, JDOMException, ClassNotFoundException
     {
         Element methodNameElement = ( Element ) XPath.selectSingleNode(document.getRootElement(), "/methodCall/methodName");
         if ( methodNameElement == null )
@@ -588,7 +259,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         return methodNameElement.getText();
     }
 
-    private ArrayList<Method> getMethods(String pCallMethod) throws XmlRpcException, JDOMException, ClassNotFoundException
+    private static ArrayList<Method> getMethods(String pCallMethod) throws XmlRpcException, JDOMException, ClassNotFoundException
     {
         String[] values = pCallMethod.split("\\.");
         if ( values.length != 2 )
@@ -610,7 +281,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         return methods;
     }
 
-    private Document getException(Exception e) throws JDOMException, IOException
+    private static Document getException(Exception e) throws JDOMException, IOException
     {
         StringBuilder messageError = new StringBuilder(e.getLocalizedMessage() + RETURN);
         for ( StackTraceElement element : e.getStackTrace() )
@@ -636,14 +307,14 @@ public abstract class XMLRPCServlet extends HttpServlet
         return builder.build(stringReader);
     }
 
-    private Document getDocument(byte[] document) throws JDOMException, IOException
+    private static Document getDocument(byte[] document) throws JDOMException, IOException
     {
         ByteArrayInputStream in = new ByteArrayInputStream(document);
         SAXBuilder builder = new SAXBuilder();
         return builder.build(in);
     }
 
-    private Document getDocument(HttpServletRequest request) throws IOException, JDOMException
+    private static Document getDocument(HttpServletRequest request) throws IOException, JDOMException
     {
         ServletInputStream in = request.getInputStream();
         SAXBuilder builder = new SAXBuilder();
@@ -651,7 +322,7 @@ public abstract class XMLRPCServlet extends HttpServlet
         return builder.build(in);
     }
 
-    private boolean isMultipart(HttpServletRequest request)
+    private static boolean isMultipart(HttpServletRequest request)
     {
         boolean isMultipart = false;
         if ( request.getContentType().indexOf("multipart/form-data") != -1 )
