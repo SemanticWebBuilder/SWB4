f64ccabdef60b0be2f40e7e23237d62db6dccd42
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-08-18 21:41:15 +0000

    Mejoras por PMD

---
+++ SWB4/Client/OpenOffice/OfficeComunicationInterfaces/src/org/semanticwb/office/interfaces/CategoryInfo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.office.interfaces;
+
+/**
+ *
+ * @author victor.lorenzana
+ */
+public class CategoryInfo {    
+    public String UDDI;
+    public String title;
+    public String description;
+}
