1406da778bbd85d63d49b7e7c43c87d8a11f535b
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-08-20 16:39:11 +0000

    Cambios por seguridad en esta capa

--- SWB4/Client/OpenOffice/OfficeComunicationServer/src/org/semanticwb/office/comunication/OfficeServlet.java
+++ SWB4/Client/OpenOffice/OfficeComunicationServer/src/org/semanticwb/office/comunication/OfficeServlet.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.semanticwb.office.comunication;
 
 import java.io.*;
@@ -20,15 +19,19 @@ import org.semanticwb.xmlrpc.Part;
 import org.semanticwb.xmlrpc.XMLRPCServlet;
 import sun.net.www.MimeEntry;
 import sun.net.www.MimeTable;
+import static org.semanticwb.office.comunication.Base64.*;
 
 /**
  *
  * @author victor.lorenzana
  */
-public class OfficeServlet extends XMLRPCServlet {
-       
-    private Hashtable<String,Repository> repositories=new Hashtable<String, Repository>();
-    
+public class OfficeServlet extends XMLRPCServlet
+{
+
+    private Hashtable<String, Repository> repositories = new Hashtable<String, Repository>();
+    private static String realm = "Secure Area";
+    private static String prefixBasic = "Basic ";
+
     @Override
     public void init() throws ServletException
     {
@@ -39,9 +42,9 @@ public class OfficeServlet extends XMLRPCServlet {
             InputStream in = new FileInputStream(fileConfig);
             RepositoryConfig repositoryConfig = RepositoryConfig.create(in, "C:\\repositorio\\");
             Repository rep = RepositoryImpl.create(repositoryConfig);
-            repositories.put("wbrepository", rep);            
+            repositories.put("wbrepository", rep);
 
-        }        
+        }
         catch ( RepositoryException ce )
         {
             ce.printStackTrace(System.out);
@@ -51,41 +54,100 @@ public class OfficeServlet extends XMLRPCServlet {
             fnfe.printStackTrace(System.out);
         }
         addMappingType("OfficeDocument", OfficeDocument.class);
-        addMappingType("OfficeApplication", OfficeApplication.class);
-        MimeEntry me=new MimeEntry("application/msword");
+        addMappingType("OfficeApplication", OfficeApplication.class);        
+        MimeEntry me = new MimeEntry("application/msword");
         me.setExtensions(".doc,.docx");
         MimeTable.getDefaultTable().add(me);
 
     }
+   
+    
     @Override
     protected void finalize() throws Throwable
     {
-        for(Repository rep : repositories.values())
+        for ( Repository rep : repositories.values() )
         {
-            if ( rep != null && rep instanceof RepositoryImpl)
+            if ( rep != null && rep instanceof RepositoryImpl )
             {
                 (( RepositoryImpl ) rep).shutdown();
             }
         }
     }
+
+    private static String getPassword(String userpassDecoded) throws IOException
+    {
+        String password = "";
+        String[] values = userpassDecoded.split(":");
+        password = values[1];
+        return password;
+    }
+
+    private static String getUserName(String userpassDecoded) throws IOException
+    {
+        String userName = "";
+        String[] values = userpassDecoded.split(":");
+        userName = values[0];
+        return userName;
+    }
+
+    private void checkSecurity(HttpServletRequest request, HttpServletResponse response)
+    {
+        try
+        {
+            String pUserName = null;
+            String pPassword = null;
+            String authorization = request.getHeader("Authorization");
+            if ( authorization == null || authorization.equals("") )
+            {
+                response.setHeader("WWW-Authenticate", prefixBasic + "realm=\"" + realm + "\"");
+                response.setStatus(response.SC_UNAUTHORIZED);
+                return;
+            }
+            else
+            {
+                if ( authorization.startsWith(prefixBasic) )
+                {
+                    String userpassEncoded = authorization.substring(6);
+                    String userpassDecoded = new String(decode(userpassEncoded));
+                    pUserName = getUserName(userpassDecoded);
+                    pPassword = getPassword(userpassDecoded);
+                    if ( !this.isAuthenticate(pUserName, pPassword) )
+                    {
+                        response.sendError(response.SC_FORBIDDEN);
+                    }
+                }
+                else
+                {
+                    response.sendError(response.SC_FORBIDDEN);
+                }
+            }
+        }
+        catch ( IOException ioe )
+        {
+
+        }
+    }
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-    {           
+    {
+        checkSecurity(request, response);
         super.doPost(request, response);
     }
 
     public boolean isAuthenticate(String pUserName, String pPassword)
-    {       
+    {
         return true;
     }
+
     @Override
-    protected void beforeExecute(Object objToExecute, Set<Part> parts, String user, String password) throws Exception
+    protected void beforeExecute(Object objToExecute, Set<Part> parts) throws Exception
     {
-        super.beforeExecute(objToExecute, parts, user, password);
+        super.beforeExecute(objToExecute, parts);
         if ( objToExecute instanceof RepositorySupport )
         {
-            RepositorySupport obj = ( RepositorySupport ) objToExecute;                        
-            if(!obj.hasListOfRepositories())
+            RepositorySupport obj = ( RepositorySupport ) objToExecute;
+            if ( !obj.hasListOfRepositories() )
             {
                 obj.setRepositories(repositories);
             }
