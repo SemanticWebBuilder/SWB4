d41c10976e33d63316a50389b10fdddf3655ba9a
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2008-08-22 18:19:03 +0000

    test Principal

--- SWB4/swb/SWBModel/src/org/semanticwb/model/User.java
+++ SWB4/swb/SWBModel/src/org/semanticwb/model/User.java
@@ -1,11 +1,16 @@
 package org.semanticwb.model;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
+import java.util.Date;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBUtils;
 import org.semanticwb.model.base.*;
 import org.semanticwb.platform.SemanticObject;
 
 public class User extends UserBase implements Principal
 {
+    Logger log = SWBUtils.getLogger(User.class);
     public User(SemanticObject base)
     {
         super(base);
@@ -13,5 +18,18 @@ public class User extends UserBase implements Principal
 
     public String getName() {
         return getUsrLogin();
+    }
+    
+    @Override
+    public void setUsrPassword(String password){
+        String tmpPasswd=null;
+        try {
+            tmpPasswd = SWBUtils.CryptoWrapper.passwordDigest(password);
+            super.setUsrPassword(tmpPasswd);
+            setUsrPasswordChanged(new Date());
+        } catch (NoSuchAlgorithmException ex) {
+            log.error("User: Can't set a crypted Password", ex);
+        }
+        
     }
 }
