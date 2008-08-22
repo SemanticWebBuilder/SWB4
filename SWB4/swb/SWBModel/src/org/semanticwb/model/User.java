1f66ab86667839ae914ae20150d2c70dadc067f2
serch <serch@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-08-22 17:00:17 +0000

    Principal

--- SWB4/swb/SWBModel/src/org/semanticwb/model/User.java
+++ SWB4/swb/SWBModel/src/org/semanticwb/model/User.java
@@ -1,12 +1,17 @@
 package org.semanticwb.model;
 
+import java.security.Principal;
 import org.semanticwb.model.base.*;
 import org.semanticwb.platform.SemanticObject;
 
-public class User extends UserBase 
+public class User extends UserBase implements Principal
 {
     public User(SemanticObject base)
     {
         super(base);
+    }
+
+    public String getName() {
+        return getUsrLogin();
     }
 }
