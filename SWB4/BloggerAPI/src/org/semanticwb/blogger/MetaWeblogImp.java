5ef2515a8299549b4364420edd1db873a001b7ff
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-08-22 00:01:00 +0000

    Implementación en el repositorio

--- SWB4/BloggerAPI/src/org/semanticwb/blogger/MetaWeblogImp.java
+++ SWB4/BloggerAPI/src/org/semanticwb/blogger/MetaWeblogImp.java
@@ -5,16 +5,22 @@
 package org.semanticwb.blogger;
 
 import java.io.FileReader;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.Repository;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
+import javax.jcr.Value;
 import javax.jcr.Workspace;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryResult;
 import org.apache.jackrabbit.core.nodetype.InvalidNodeTypeDefException;
 import org.apache.jackrabbit.core.nodetype.NodeTypeDef;
 import org.apache.jackrabbit.core.nodetype.NodeTypeManagerImpl;
@@ -29,49 +35,32 @@ import org.semantic.blogger.interfaces.UserBlog;
  *
  * @author victor.lorenzana
  */
-public class MetaWeblogImp implements MetaWeblog,RepositorySupport
+public class MetaWeblogImp implements MetaWeblog, RepositorySupport
 {
+
     private static final String BLOG_MODEL_PATH = "C:\\repositorio\\blogmodel.cnd";
-    public static final String BLOG_URI = "http://www.semanticwb.org.mx/model/blog/1.0";
+    public static final String BLOG_MODEL_URI = "http://www.semanticwb.org.mx/model/blog/1.0";
     public static final String BLOG_MODEL_PREFIX = "blognode";
-    public static final String BLOG_MODEL_URI = "http://www.semanticwb.org.mx/blog/1.0/";
+    public static final String BLOG_URI = "http://www.semanticwb.org.mx/blog/1.0/";
     public static final String BLOG_PREFIX = "blog";
     private static Map<String, Repository> repositories;
-    public static Session openSession(String user,String password) throws Exception
+
+    public static Session openSession(String user, String password) throws Exception
     {
         if ( repositories.size() > 0 )
         {
             Set<String> keys = repositories.keySet();
-            Session session = repositories.get(keys.iterator().next()).login(new SimpleCredentials(user, password.toCharArray()));            
+            Session session = repositories.get(keys.iterator().next()).login(new SimpleCredentials(user, password.toCharArray()));
             RegisterCustomNodeTypes(session.getWorkspace(), BLOG_MODEL_PATH);
             return session;
         }
         throw new Exception("There are not repositories");
     }
+
     public static void RegisterCustomNodeTypes(Workspace ws, String cndFileName) throws Exception
     {
 
         boolean exists = false;
-        for ( String uri : ws.getNamespaceRegistry().getURIs() )
-        {
-            if ( uri.equals(BLOG_URI) )
-            {
-                exists = true;
-                break;
-            }
-        }
-        if ( !exists )
-        {
-            try
-            {
-                ws.getNamespaceRegistry().registerNamespace(BLOG_MODEL_PREFIX, BLOG_URI);
-            }
-            catch ( Exception e )
-            {
-                e.printStackTrace(System.out);
-            }
-        }
-        exists = false;
         for ( String uri : ws.getNamespaceRegistry().getURIs() )
         {
             if ( uri.equals(BLOG_MODEL_URI) )
@@ -84,7 +73,27 @@ public class MetaWeblogImp implements MetaWeblog,RepositorySupport
         {
             try
             {
-                ws.getNamespaceRegistry().registerNamespace(BLOG_PREFIX, BLOG_MODEL_URI);
+                ws.getNamespaceRegistry().registerNamespace(BLOG_MODEL_PREFIX, BLOG_MODEL_URI);
+            }
+            catch ( Exception e )
+            {
+                e.printStackTrace(System.out);
+            }
+        }
+        exists = false;
+        for ( String uri : ws.getNamespaceRegistry().getURIs() )
+        {
+            if ( uri.equals(BLOG_URI) )
+            {
+                exists = true;
+                break;
+            }
+        }
+        if ( !exists )
+        {
+            try
+            {
+                ws.getNamespaceRegistry().registerNamespace(BLOG_PREFIX, BLOG_URI);
             }
             catch ( Exception e )
             {
@@ -128,6 +137,7 @@ public class MetaWeblogImp implements MetaWeblog,RepositorySupport
             }
         }
     }
+
     public void setRepositories(Map<String, Repository> repositories)
     {
         if ( MetaWeblogImp.repositories != null )
@@ -136,31 +146,118 @@ public class MetaWeblogImp implements MetaWeblog,RepositorySupport
         }
         MetaWeblogImp.repositories = repositories;
     }
+
     public boolean hasListOfRepositories()
     {
-        boolean hasListOfRepositories=false;
-        if(repositories!=null)
+        boolean hasListOfRepositories = false;
+        if ( repositories != null )
         {
-            hasListOfRepositories=true;
+            hasListOfRepositories = true;
         }
         return hasListOfRepositories;
     }
 
-    public UserBlog[] getUsersBlogs(String appkey, String username, String password)
+    /**
+     * Returns tthe blogs that an user can publish posts
+     * @param appkey Is not used
+     * @param username User
+     * @param password //Password
+     * @return An array of blogs that the user can publish
+     * @see UserBlog
+     */
+    public UserBlog[] getUsersBlogs(String appkey, String username, String password) throws Exception
     {
         HashSet<UserBlog> blogs = new HashSet<UserBlog>();
-        UserBlog blog = new UserBlog();
-        blog.blogName = "a";
-        blog.blogid = "200";
-        blog.isAdmin = false;
-        blog.url = "http://localhost:8084/TestRPC/Blogger";
-        blogs.add(blog);
+        Session session = null;
+        try
+        {
+            session = openSession(username, password);
+            Query query = session.getWorkspace().getQueryManager().createQuery("//blog", Query.XPATH);
+            QueryResult result = query.execute();
+            NodeIterator nodeIterator = result.getNodes();
+            while (nodeIterator.hasNext())
+            {
+                UserBlog userblog = new UserBlog();
+                Node nodeBlog = nodeIterator.nextNode();
+                userblog.blogName = nodeBlog.getProperty("blog:name").getString();
+                userblog.blogid = nodeBlog.getUUID();
+                userblog.isAdmin = true;
+                userblog.url = "http://localhost:8084/TestRPC/Blogger";
+                blogs.add(userblog);
+            }
+        }
+        catch ( Exception ex )
+        {
+            throw ex;
+        }
+        finally
+        {
+            if ( session != null )
+            {
+                session.logout();
+            }
+        }
         return blogs.toArray(new UserBlog[blogs.size()]);
     }
-
-    public Post getRecentPosts(String blogid, String username, String password, int numberOfPosts) throws Exception
+    /**
+     * Retrieves a list of posts that were created recently. The results are returned in descending chronolocial order with the most recent post first in the list.
+     * @param blogid the Blog ID
+     * @param username The user name
+     * @param password The password
+     * @param numberOfPosts Number of máx posts to retrive
+     * @return The list of recently posts
+     * @throws java.lang.Exception
+     */
+    public Post[] getRecentPosts(String blogid, String username, String password, int numberOfPosts) throws Exception
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        ArrayList<Post> posts = new ArrayList<Post>();
+        Session session = null;
+        try
+        {
+            session = openSession(username, password);
+            Node blogNode = session.getNodeByUUID(blogid);
+            NodeIterator postNodes = blogNode.getNodes("post");
+            int numPost = 1;
+            while (postNodes.hasNext())
+            {
+                if ( numPost < numberOfPosts )
+                {
+                    Node postNode = postNodes.nextNode();
+                    Post value = new Post();
+                    ArrayList<String> categories=new ArrayList<String>();
+                    for(Value categoryValue : postNode.getProperty("blog:category").getValues())
+                    {
+                        categories.add(categoryValue.getString());
+                    }
+                    value.categories = categories.toArray(new String[categories.size()]);
+                    value.dateCreated = postNode.getProperty("blog:dateCreated").getDate().getTime();
+                    value.description = postNode.getProperty("blog:title").getString();
+                    value.postid = postNode.getUUID();
+                    value.title = postNode.getProperty("blog:description").getString();
+                    value.userid = postNode.getProperty("blog:userid").getString();
+                    posts.add(value);
+                    numPost++;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+        catch ( Exception ex )
+        {
+            throw ex;
+        }
+        finally
+        {
+            if ( session != null )
+            {
+                session.logout();
+            }
+        }
+        return posts.toArray(new Post[posts.size()]);
     }
 
     /**
@@ -175,7 +272,33 @@ public class MetaWeblogImp implements MetaWeblog,RepositorySupport
      */
     public boolean editPost(String postid, String userid, String password, Post post, boolean publish) throws Exception
     {
-        return true;
+        Session session = null;
+        try
+        {
+            session = openSession(userid, password);
+            Node postNode = session.getNodeByUUID(postid);
+            postNode.checkout();
+            postNode.setProperty("blog:title", post.title);
+            postNode.setProperty("blog:userid", post.userid);
+            postNode.setProperty("blog:description", post.description);
+            for ( String category : post.categories )
+            {
+                postNode.setProperty("blog:category", category);
+            }
+            postNode.checkin();
+            return true;
+        }
+        catch ( Exception ex )
+        {
+            throw ex;
+        }
+        finally
+        {
+            if ( session != null )
+            {
+                session.logout();
+            }
+        }
     }
 
     /**
@@ -188,19 +311,72 @@ public class MetaWeblogImp implements MetaWeblog,RepositorySupport
      */
     public Post getPost(String postid, String userid, String password) throws Exception
     {
-        Post value = new Post();
-        value.categories = new String[]{"Michegas", "Mind Bombs", "Rest & Relaxation"};
-        value.dateCreated = new Date();
-        value.description = "Blogger Ed Cone of Greensboro talks&nbsp; about the several intersections he overlooks. That is: junctions of the public and the personal (which every blogger faces) and more particularly the contrasting voices of a newspaper columnist and a blogger (he is both) and the opportunities for a local conversation in a global medium.";
-        value.postid = "1829";
-        value.title = "title";
-        value.userid = "1015";
-        return value;
+        Session session = null;
+        try
+        {
+            session = openSession(userid, password);
+            Node postNode = session.getNodeByUUID(postid);
+            Post value = new Post();
+            value.categories = new String[]{"Michegas", "Mind Bombs", "Rest & Relaxation"};
+            value.dateCreated = postNode.getProperty("blog:dateCreated").getDate().getTime();
+            value.description = postNode.getProperty("blog:title").getString();
+            value.postid = postid;
+            value.title = postNode.getProperty("blog:description").getString();
+            value.userid = postNode.getProperty("blog:userid").getString();
+            return value;
+        }
+        catch ( Exception ex )
+        {
+            throw ex;
+        }
+        finally
+        {
+            if ( session != null )
+            {
+                session.logout();
+            }
+        }
     }
-
+    /**
+     * Create a new post into a blog
+     * @param blogid The blog ID
+     * @param userid The userID
+     * @param password The password
+     * @param post The post information
+     * @param publish If the post must be published
+     * @return The Post ID
+     * @throws java.lang.Exception
+     * @see Post
+     */
     public String newPost(String blogid, String userid, String password, Post post, boolean publish) throws Exception
     {
-        return "200000"; // return the postId        
+        Session session = null;
+        try
+        {
+            session = openSession(userid, password);
+            Node blogNode = session.getNodeByUUID(blogid);
+            Node postNode = blogNode.addNode("post", "blognode:blogEntry");
+            postNode.setProperty("blog:title", post.title);
+            postNode.setProperty("blog:userid", post.userid);
+            postNode.setProperty("blog:description", post.description);
+            postNode.setProperty("blog:dateCreated", Calendar.getInstance());
+            for ( String category : post.categories )
+            {
+                postNode.setProperty("blog:category", category);
+            }
+            return postNode.getUUID();
+        }
+        catch ( Exception ex )
+        {
+            throw ex;
+        }
+        finally
+        {
+            if ( session != null )
+            {
+                session.logout();
+            }
+        }
     }
 
     public Post getCategories(String blogid, String username, String password) throws Exception
