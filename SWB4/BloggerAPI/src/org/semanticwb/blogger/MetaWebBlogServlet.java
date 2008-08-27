6cd0de8c1dae5a3f93d8898ed307a8ce62b993f1
vlorenzana <vlorenzana73@users.noreply.github.com>
2008-08-27 22:07:17 +0000

    Mejora por RepositoryManager

--- SWB4/BloggerAPI/src/org/semanticwb/blogger/MetaWebBlogServlet.java
+++ SWB4/BloggerAPI/src/org/semanticwb/blogger/MetaWebBlogServlet.java
@@ -8,56 +8,22 @@ package org.semanticwb.blogger;
 import org.semantic.blogger.interfaces.*;
 import java.io.*;
 import java.net.*;
-
-import java.util.Hashtable;
-import java.util.Set;
-import javax.jcr.Repository;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import org.semanticwb.blogger.JackRabbitRepositories;
-import org.semanticwb.blogger.MetaWeblogImp;
-import org.semanticwb.xmlrpc.Part;
 import org.semanticwb.xmlrpc.XMLRPCServlet;
 
 /**
  *
  * @author victor.lorenzana
  */
-public class MetaWebBlogServlet extends XMLRPCServlet {
+public class MetaWebBlogServlet extends XMLRPCServlet
+{   
     
-    private static Hashtable<String, Repository> repositories = new Hashtable<String, Repository>();    
-    private static JackRabbitRepositories rep;
-    static            
-    {
-        rep=new JackRabbitRepositories();
-    }
     @Override
     public void init() throws ServletException
     {          
         addMappingType("blogger", MetaWeblogImp.class);
         addMappingType("metaWeblog", MetaWeblogImp.class);        
-    }    
-    public static void addRepositories(Hashtable<String, Repository> repositories)
-    {
-        MetaWebBlogServlet.repositories=repositories;
-    }
-    
-    @Override
-    protected void beforeExecute(Object objToExecute, Set<Part> parts) throws Exception
-    {
-        super.beforeExecute(objToExecute, parts);
-        if ( objToExecute instanceof RepositorySupport )
-        {
-            RepositorySupport obj = ( RepositorySupport ) objToExecute;
-            //if ( !obj.hasListOfRepositories() )
-            //{
-                obj.setRepositories(repositories);
-            //}
-        }
-    }
-    @Override
-    protected void finalize() throws Throwable
-    {
-        rep.shutDown();
-    } 
+    }             
+     
 }
