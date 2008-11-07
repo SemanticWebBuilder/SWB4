33252925228c7e51cf56095da02786aeb035bf32
cramos <cramos@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-11-07 21:59:00 +0000

    Recurso adecuado a la nueva api

---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/StaticText.java
@@ -0,0 +1,118 @@
+/*
+ * INFOTEC WebBuilder es una herramienta para el desarrollo de portales de conocimiento, colaboraci�n e integraci�n para Internet,
+ * la cual, es una creaci�n original del Fondo de Informaci�n y Documentaci�n para la Industria INFOTEC, misma que se encuentra
+ * debidamente registrada ante el Registro P�blico del Derecho de Autor de los Estados Unidos Mexicanos con el
+ * No. 03-2002-052312015400-14, para la versi�n 1; No. 03-2003-012112473900 para la versi�n 2, y No. 03-2006-012012004000-01
+ * para la versi�n 3, respectivamente.
+ *
+ * INFOTEC pone a su disposici�n la herramienta INFOTEC WebBuilder a trav�s de su licenciamiento abierto al p�blico (�open source�),
+ * en virtud del cual, usted podr� usarlo en las mismas condiciones con que INFOTEC lo ha dise�ado y puesto a su disposici�n;
+ * aprender de �l; distribuirlo a terceros; acceder a su c�digo fuente y modificarlo, y combinarlo o enlazarlo con otro software,
+ * todo ello de conformidad con los t�rminos y condiciones de la LICENCIA ABIERTA AL P�BLICO que otorga INFOTEC para la utilizaci�n
+ * de INFOTEC WebBuilder 3.2.
+ *
+ * INFOTEC no otorga garant�a sobre INFOTEC WebBuilder, de ninguna especie y naturaleza, ni impl�cita ni expl�cita,
+ * siendo usted completamente responsable de la utilizaci�n que le d� y asumiendo la totalidad de los riesgos que puedan derivar
+ * de la misma.
+ *
+ * Si usted tiene cualquier duda o comentario sobre INFOTEC WebBuilder, INFOTEC pone a su disposici�n la siguiente
+ * direcci�n electr�nica:
+ *
+ *                                          http://www.webbuilder.org.mx
+ */
+
+
+package org.semanticwb.portal.resources;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.semanticwb.SWBPlatform;
+import org.semanticwb.SWBPortal;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.model.Portlet;
+import org.semanticwb.portal.api.GenericAdmResource;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+
+
+/** Objeto que se encarga de desplegar y administrar un texto est�tico, este texto
+ * se agrega en la administraci�n del recurso, acepta tags de html para cambiar su
+ * aspecto.
+ *
+ * Object that is in charge to unfold and to administer a static text, this text
+ * it is added in the administration of the resource, accepts tags of HTML to change his
+ * aspect.
+ * @author Javier Solis Gonzalez
+ */
+public class StaticText extends GenericAdmResource 
+{
+    /** 
+     * Creates a new instance of Static Text 
+     */
+    public StaticText() {
+    }
+
+    /** Obtiene la vista del recurso.
+     *
+     * @param request El servlet container crea un objeto HttpServletRequest y
+     *                      se pasa como argumento al m�todo del servlet.
+     * @param response El servlet container crea un objeto HttpServletResponse y
+     *                      se pasa como argumento al m�todo del servlet.
+     * @param paramsRequest Argumentos de la solicitud del recurso.
+     * @throws IOException
+     * @exception com.infotec.appfw.exception.AFException Si se origina cualquier error en el recurso al traer el html.
+     */
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        Portlet base=getResourceBase();
+        base.getAttribute("text");
+        String staticText = replaceTags(base.getAttribute("text"), request,paramRequest);
+
+        PrintWriter out = response.getWriter();
+        out.println(staticText);
+        out.println("<br><a href=\"" + paramRequest.getRenderUrl().setMode(paramRequest.Mode_ADMIN) + "\">admin static text</a>");
+    }
+
+    public String replaceTags(String str, HttpServletRequest request, SWBParamRequest paramRequest)
+    {
+        if(str==null || str.trim().length()==0)
+            return "";
+        
+        str=str.trim();
+        //TODO: codificar cualquier atributo o texto
+       
+        Iterator it=SWBUtils.TEXT.findInterStr(str, "{request.getParameter(\"", "\")}");
+        while(it.hasNext())
+        {
+            String s=(String)it.next();
+            str=SWBUtils.TEXT.replaceAll(str, "{request.getParameter(\""+s+"\")}", request.getParameter(replaceTags(s,request,paramRequest)));
+        }
+        
+        it=SWBUtils.TEXT.findInterStr(str, "{session.getAttribute(\"", "\")}");
+        while(it.hasNext())
+        {
+            String s=(String)it.next();
+            str=SWBUtils.TEXT.replaceAll(str, "{session.getAttribute(\""+s+"\")}", (String)request.getSession().getAttribute(replaceTags(s,request,paramRequest)));
+        }
+        
+        it=SWBUtils.TEXT.findInterStr(str, "{getEnv(\"", "\")}");
+        while(it.hasNext())
+        {
+            String s=(String)it.next();
+            str=SWBUtils.TEXT.replaceAll(str, "{getEnv(\""+s+"\")}", SWBPlatform.getEnv(replaceTags(s,request,paramRequest)));
+        }
+        
+        str=SWBUtils.TEXT.replaceAll(str, "{user.login}", paramRequest.getUser().getUsrLogin());
+        str=SWBUtils.TEXT.replaceAll(str, "{user.email}", paramRequest.getUser().getUsrEmail());
+        str=SWBUtils.TEXT.replaceAll(str, "{user.language}", paramRequest.getUser().getLanguage());
+        str=SWBUtils.TEXT.replaceAll(str, "{webpath}", SWBPlatform.getContextPath());
+        str=SWBUtils.TEXT.replaceAll(str, "{distpath}", SWBPortal.getDistributorPath());
+        str=SWBUtils.TEXT.replaceAll(str, "{webworkpath}", SWBPlatform.getWebWorkPath());
+        str=SWBUtils.TEXT.replaceAll(str, "{workpath}", SWBPlatform.getWorkPath());
+        return str;
+    }
+    
+}
