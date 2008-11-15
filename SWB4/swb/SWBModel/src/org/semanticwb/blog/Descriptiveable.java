81e694c30442c1521e8160d997241e7e2933f322
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2008-11-15 00:38:17 +0000

    Clases implementacion jcr170

---
+++ SWB4/swb/SWBModel/src/org/semanticwb/blog/Descriptiveable.java
@@ -0,0 +1,9 @@
+package org.semanticwb.blog;
+
+import org.semanticwb.model.base.*;
+import org.semanticwb.model.*;
+import org.semanticwb.model.GenericIterator;
+import java.util.Date;
+public interface Descriptiveable extends GenericObject
+{
+}
