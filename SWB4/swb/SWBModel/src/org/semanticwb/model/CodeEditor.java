30346ba89d7f19570f0a1e14402dc7173201a4b8
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2009-01-16 23:09:37 +0000

    Muestra los textos de los tooltips en el lenguaje configurado para el usuario. Se especifica el código de caracteres para el script que carga la configuración del editor.

--- SWB4/swb/SWBModel/src/org/semanticwb/model/CodeEditor.java
+++ SWB4/swb/SWBModel/src/org/semanticwb/model/CodeEditor.java
@@ -15,13 +15,14 @@ public class CodeEditor extends org.semanticwb.model.base.CodeEditorBase
     public String renderXHTML(SemanticObject obj, SemanticProperty prop, String type, String mode, String lang)
     {
         StringBuffer ret = new StringBuffer(250);
-        String id=obj.getURI()+"/"+prop.getName();
+        String id=obj.getURI()+"/"+prop.getName()+"_editArea";
         String name=prop.getName();
         String label=prop.getDisplayName(lang);
         SemanticObject sobj=prop.getDisplayProperty();
         boolean required=prop.isRequired();
         String pmsg=null;
         String imsg=null;
+        String language="es";
         if(sobj!=null)
         {
             DisplayProperty dobj=new DisplayProperty(sobj);
@@ -50,14 +51,18 @@ public class CodeEditor extends org.semanticwb.model.base.CodeEditorBase
             }
         }
 
+        if(lang.equals("en"))
+        {
+            language=lang;
+        }
         String value=obj.getProperty(prop);
         if(value==null)value="";
         if(mode.equals("edit") || mode.equals("create") )
         {
-            ret.append("<script language=\"javascript\" type=\"text/javascript\">\n");
+            ret.append("<script language=\"javascript\" type=\"text/javascript\" charset=\"UTF-8\">\n");
             ret.append("editAreaLoader.init({\n");
             ret.append("    id : \"" + id + "\"\n");
-            ret.append("    ,language: \"es\"\n");
+            ret.append("    ,language: \"" + language + "\"\n");
             ret.append("    ,syntax: \"" + getLanguage().toLowerCase() + "\"\n");
             ret.append("    ,start_highlight: true\n");
             ret.append("    ,toolbar: \"search, go_to_line,");
