4cb2c5e1c6c268a15f4d4b64e33cd6a57a535892
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2009-05-28 22:44:33 +0000

    problema de detectar a un usuario como signed si entraba desde el recurso login

---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/demo/ListaCommun.java
@@ -0,0 +1,200 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.portal.resources.demo;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.model.GenericIterator;
+import org.semanticwb.model.WebPage;
+import org.semanticwb.model.catalogs.LocationEntity;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBActionResponse;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import org.semanticwb.model.comm.MicroSite;
+
+
+/**
+ *
+ * @author serch
+ */
+public class ListaCommun extends GenericResource {
+    long refresh = 0;
+    String buffer = "Cargando...... ";
+    private static final String NL = System.getProperty("line.separator");
+
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
+    {
+        WebPage temas = paramRequest.getTopic().getWebSite().getWebPage("Temas");
+        Model model = paramRequest.getTopic().getSemanticObject().getModel().getRDFModel();
+        WebPage locate  = paramRequest.getTopic().getWebSite().getWebPage("Tlalpan");
+        PrintWriter out = response.getWriter();
+        out.println(doSearchComm(temas, (LocationEntity)locate, model));
+    }
+
+    @Override
+    public void processAction(HttpServletRequest request, SWBActionResponse response) throws SWBResourceException, IOException
+    {
+        super.processAction(request, response);
+    }
+
+    private String doSearch(WebPage temas, LocationEntity localidad, Model model){
+        String prolog = "PREFIX  swb:  <http://www.semanticwebbuilder.org/swb4/ontology#>"+NL+
+        "PREFIX  rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+NL+
+        "PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+NL+
+        "PREFIX swbcomm: <http://www.semanticwebbuilder.org/swb4/community#>"+NL+
+        "PREFIX swbc: <http://www.semanticwebbuilder.org/swbcatalogs#>"+NL;
+
+        prolog += "SELECT ?x WHERE {?x rdf:type swbcomm:TematicComm ";
+        prolog += ". ?x swbcomm:locatedIn <"+localidad.getURI()+"> ";
+        prolog += ". ?x swbcomm:about <"+temas.getURI()+"> ";
+        prolog += "}";
+
+        Query query = QueryFactory.create(prolog);
+
+        // System.out.println(getId());
+        // Print with line numbers
+        // query.serialize(new IndentedWriter(System.out,true)) ;
+        // System.out.println() ;
+
+        QueryExecution qexec = QueryExecutionFactory.create(query, model);
+        try
+        {
+            ResultSet rs = qexec.execSelect();
+            
+            for (; rs.hasNext();)
+            {
+                QuerySolution rb = rs.nextSolution();
+                String current = rb.get("x").toString();
+
+            }
+        } catch (Exception e) {}
+
+
+        return "";
+    }
+
+    private String doSearchComm(WebPage temas, LocationEntity localidad, Model model)
+    {
+        SortedSet<Contenedor> set = new TreeSet<Contenedor>();
+        Hashtable<String,Contenedor> tabla = new Hashtable<String, Contenedor>();
+        WebPage contenedor = temas.getWebSite().getWebPage("Comunidades");
+        GenericIterator<WebPage>git = contenedor.listChilds();
+        while (git.hasNext()){
+            MicroSite act = (MicroSite)git.next();
+            if (null==act.getAbout()) continue;
+            String name = act.getAbout().getDisplayName();
+            String url = act.getAbout().getRealUrl();
+            LocationEntity location = act.getLocatedIn();
+            if (localidad.equals(location)){
+                if (tabla.containsKey(name)){
+                Contenedor este = tabla.get(name);
+                este.incVal();
+                //tabla.put(name, este);
+                } else
+                {
+                    tabla.put(name, new Contenedor(name, url));
+                }
+            }
+        }
+        Enumeration<Contenedor> enucon = tabla.elements();
+        while (enucon.hasMoreElements()) set.add(enucon.nextElement());
+        Iterator<Contenedor> it = set.iterator();
+        String ret = "<ul>\n";
+        int cnt = 0;
+        while (it.hasNext() && cnt<12) {
+            cnt++;
+        Contenedor este = it.next();
+        ret += "<li><p><a href=\""+este.getUrl()+"\">"+este.getId()+" ("+este.getCount()+")</a></p></li>\n";
+        }
+        ret += "</ul>";
+        System.out.println(
+                set.toString());
+        return ret;
+    }
+
+    /**
+     *  <ul>
+          <li><p>Cine (24)</p></li>
+          <li><p>Danza (3)</p></li>
+          <li><p>Teatro (7)</p></li>
+          <li><p>Artes pl·sticas (55)</p></li>
+          <li><p>Literatura (46)</p></li>
+          <li><p>Automovilismo (24)</p></li>
+		  <li><p>Deportes (3)</p></li>
+		  <li><p>Campismo (7)</p></li>
+		  <li><p>M˙sica (55)</p></li>
+		  <li><p>Viajes (46)</p></li>
+        </ul>
+     */
+
+
+
+}
+
+
+
+    class Contenedor implements Comparable{
+    private String name=null;
+    private int cont = 0;
+    private String url = null;
+        public Contenedor(String name, String url){
+            this.name = name;
+            this.url = url;
+            cont=1;
+        }
+        public void incVal(){
+        cont++;
+        }
+        public String getId(){
+            return name;
+        }
+        public int getCount(){
+            return cont;
+        }
+
+        public String getUrl()
+        {
+            return url;
+        }
+
+        public int compareTo(Object o)
+        {
+            Contenedor other = (Contenedor)o;
+            if (other.getCount()>getCount()) return 1;
+            if (other.getCount()==getCount()) return 0;
+            return -1;
+        }
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            return name.equals(obj);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return name.hashCode();
+        }
+
+
+    }
---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/demo/Registro.java
@@ -0,0 +1,173 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.portal.resources.demo;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
+import java.util.Iterator;
+import java.util.Set;
+import javax.security.auth.Subject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBPortal;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.model.Role;
+import org.semanticwb.model.User;
+import org.semanticwb.model.UserRepository;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBActionResponse;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.portal.api.SWBResourceURL;
+
+/**
+ *
+ * @author serch
+ */
+public class Registro extends GenericResource{
+
+    static Logger log = SWBUtils.getLogger(Registro.class);
+
+    @Override
+    public void doEdit(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
+    {
+        User user = paramRequest.getUser();
+        if (!user.isSigned()) {
+            doView(request, response, paramRequest);
+        } else
+        {
+            request.setAttribute("paramRequest", paramRequest);
+            request.setAttribute("url", paramRequest.getActionUrl().setAction(SWBResourceURL.Action_EDIT));
+            String err = "";
+            if (request.getParameter("_errMsg")!=null) err = request.getParameter("_errMsg");
+            request.setAttribute("errMessage", err);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/swbadmin/jsp/demo/registroUpdate.jsp");
+            try
+            {
+                dispatcher.forward(request, response);
+            //dispatcher.include(request, response);
+            } catch (ServletException ex)
+            {
+                log.error("Can't include a jsp file /swbadmin/jsp/demo/registroUpdate.jsp" , ex);
+            }
+        }
+    }
+
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
+    {
+        User user = paramRequest.getUser();
+        if (user.isSigned()) {
+            doEdit(request, response, paramRequest);
+        } else
+        {
+            request.setAttribute("paramRequest", paramRequest);
+            request.setAttribute("url", paramRequest.getActionUrl().setAction(SWBResourceURL.Action_ADD));
+            String err = "";
+            if (request.getParameter("_errMsg")!=null) err = request.getParameter("_errMsg");
+            request.setAttribute("errMessage", err);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/swbadmin/jsp/demo/registroCreate.jsp");
+            try
+            {
+                dispatcher.forward(request, response);
+            //dispatcher.include(request, response);
+            } catch (ServletException ex)
+            {
+                log.error("Can't include a jsp file /swbadmin/jsp/demo/registroCreate.jsp" , ex);
+            }
+        }
+    }
+
+    @Override
+    public void processAction(HttpServletRequest request, SWBActionResponse response) throws SWBResourceException, IOException
+    {
+        if (response.getAction().equals(SWBResourceURL.Action_ADD))
+        {
+            if (response.getUser().isSigned()) {
+                response.setRenderParameter("errMessage", " ya se ha firmado, para crear un nuevo usuario se requiere que cierre su sessi&oacute;n");
+                return;
+            }
+            String login = request.getParameter("usrLogin");
+            if (null==login || "".equals(login))
+            {
+                response.setRenderParameter("errMessage", " Debe suministrar la clave de usuario");
+                return;
+            }
+            String pwd = request.getParameter("usrPwd");
+            String pwd2 = request.getParameter("usrPwd2");
+            if (!pwd.equals(pwd2))
+            {
+                response.setRenderParameter("errMessage", " las contrase&ntilde;as no corresponden");
+                return;
+            }
+            System.out.println("Topic: "+response.getTopic());
+            System.out.println("website: "+response.getTopic().getWebSite());
+            System.out.println("Repository: "+response.getTopic().getWebSite().getUserRepository());
+            UserRepository ur = response.getTopic().getWebSite().getUserRepository();
+            System.out.println("Rep: "+ur);
+            User u=ur.getUserByLogin(login);
+            if (null!=u)
+            {
+                response.setRenderParameter("errMessage", " la clave de usuario "+login+" ya est&aacute; en uso");
+                return;
+            }
+            u = ur.createUser();
+            u.setLogin(login);
+            u.setPassword(pwd);
+            u.setLanguage("es");
+            u.setActive(true);
+            try
+            {
+                u.checkCredential(pwd.toCharArray());
+            } catch (NoSuchAlgorithmException ex)
+            {
+                log.error("There are no crypto algorithms!!!! ");
+            }
+            String url = response.getTopic().getRealUrl();
+            Subject subject = SWBPortal.getUserMgr().getSubject(request);
+            Set<Principal> principals  = subject.getPrincipals();
+            principals.clear();
+            principals.add(u);
+            response.setMode(SWBResourceURL.Mode_EDIT);
+
+        }
+        else if (response.getAction().equals(SWBResourceURL.Action_EDIT))
+        {
+            User user = response.getUser();
+            String lName = null;
+            String mail = null;
+            String rolId = null;
+            Iterator<Role> lista = user.getUserRepository().listRoles();
+                while (lista.hasNext()){
+                    org.semanticwb.model.Role actRole = lista.next();
+                    user.removeRole(actRole);
+                }
+            if (null!=request.getParameter("usrFName")){
+                user.setFirstName(SWBUtils.XML.replaceXMLChars(request.getParameter("usrFName")));
+            }
+            if (null!=request.getParameter("usrLName")){
+                user.setLastName(SWBUtils.XML.replaceXMLChars(request.getParameter("usrLName")));
+            }
+            if (null!=request.getParameter("usrMail")){
+                user.setEmail(SWBUtils.XML.replaceXMLChars(request.getParameter("usrMail")));
+            }
+            if (null!=request.getParameter("usrRol")){
+                rolId = SWBUtils.XML.replaceXMLChars(request.getParameter("usrRol"));
+                Role role = user.getUserRepository().getRole(rolId);
+                if (null!=role){user.addRole(role);}
+            }
+            response.sendRedirect(response.getTopic().getWebSite().getHomePage().getUrl());
+
+
+
+        }
+    }
+
+}
