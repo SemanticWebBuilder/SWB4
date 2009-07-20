dff6ac8fd47653e10e0fbdd8cc76f73cfb8dc04f
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2009-07-20 23:56:35 +0000

    Primera versión del recurso, con internacionalización de la administración.

---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/PHPResource.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.portal.resources;
+
+
+/**
+ * Permite ejecutar un archivo php, dando la ruta del archivo a ejecutar dentro
+ * del sitio, pudiendose presentar como contenido o como un recurso.
+ *
+ * Allows to execute a php file, giving the file path to execute
+ * within the site, showing it as a content or as a resource.
+ *
+ * @author jose.jimenez
+ */
+public class PHPResource extends JSPResource {
+
+}
