ca384bfcac12c27468ff762ededa4e741b408b09
redentor <JRedentor@users.noreply.github.com>
2009-07-20 23:56:35 +0000

    Primera versión del recurso, con internacionalización de la administración.

---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/GroovyResource.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.portal.resources;
+
+
+/**
+ * Permite ejecutar un archivo con codigo en groovy, dando la ruta del archivo
+ * a ejecutar dentro del sitio, pudiendose presentar como contenido o como un
+ * recurso.
+ *
+ * Allows to execute a file with code in groovy, giving the file path to execute
+ * within the site, showing it as a content or as a resource.
+ *
+ * @author jose.jimenez
+ */
+public class GroovyResource extends JSPResource {
+
+}
