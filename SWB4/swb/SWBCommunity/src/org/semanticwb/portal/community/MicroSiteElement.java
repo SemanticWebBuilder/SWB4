f9e4ba4b6ecf8a511e40b988af38542f9f7ba15c
redentor <JRedentor@users.noreply.github.com>
2009-08-14 03:44:50 +0000

    Con la adición de presentación de elementos generales para los recursos.

--- SWB4/swb/SWBCommunity/src/org/semanticwb/portal/community/MicroSiteElement.java
+++ SWB4/swb/SWBCommunity/src/org/semanticwb/portal/community/MicroSiteElement.java
@@ -1,6 +1,14 @@
 package org.semanticwb.portal.community;
 
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import javax.servlet.http.HttpServletRequest;
 import org.semanticwb.SWBPlatform;
+import org.semanticwb.model.GenericIterator;
+import org.semanticwb.platform.SemanticObject;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.portal.api.SWBResourceURL;
 
 
 public class MicroSiteElement extends org.semanticwb.portal.community.base.MicroSiteElementBase 
@@ -14,11 +22,15 @@ public class MicroSiteElement extends org.semanticwb.portal.community.base.Micro
     private long timer;                     //valores de sincronizacion de views, hits
     private static long time;               //tiempo en milisegundos por cada actualizacion
     private boolean viewed = false;
+    private final String fullStarPath = "/swbadmin/resources/ranking/fullstar.png";
+    private final String halfStarPath = "/swbadmin/resources/ranking/halfstar.png";
+    private final String emptyStarPath = "/swbadmin/resources/ranking/emptystar.png";
 
     static
     {
         time = 1000L * Long.parseLong((String) SWBPlatform.getEnv("swb/accessLogTime","600"));
     }
+
 
 
     public MicroSiteElement(org.semanticwb.platform.SemanticObject base)
@@ -126,6 +138,171 @@ public class MicroSiteElement extends org.semanticwb.portal.community.base.Micro
             if(views>0)setViews(views);
             viewed = false;
         }
+    }
+
+    public void renderGenericElements(HttpServletRequest request,
+            PrintWriter out, MicroSiteElement mse, SWBParamRequest paramRequest)
+            throws SWBResourceException {
+
+        String suri = request.getParameter("uri");
+        StringBuilder sb = new StringBuilder(500);
+        String tmpUrl = "";
+        String abusedDesc = mse.isAbused() ? "Inapropiado" : "Apropiado";
+        int rank = 0;
+
+        if (suri == null && mse != null) {
+            suri = mse.getURI();
+        }
+        tmpUrl = "uri=\"+escape('" + suri + "')";
+        rank = (int) Math.round(Math.floor(mse.getRank() * 10));
+        SWBResourceURL url = paramRequest.getActionUrl();
+        url.setAction("vote");
+        url.setMode(paramRequest.getMode());
+        url.setCallMethod(SWBResourceURL.Call_DIRECT);
+        out.println("\n<script language=\"javascript\" type=\"text/javascript\">");
+        out.println("\nvar request = false;");
+        out.println("\ntry {");
+        out.println("\n  request = new XMLHttpRequest();");
+        out.println("\n} " +
+                "catch (trymicrosoft) {");
+        out.println("\n  try {");
+        out.println("\n    request = new ActiveXObject(\"Msxml2.XMLHTTP\");");
+        out.println("\n  } catch (othermicrosoft) {");
+        out.println("\n    try {");
+        out.println("\n      request = new ActiveXObject(\"Microsoft.XMLHTTP\");");
+        out.println("\n    } catch (failed) {");
+        out.println("\n      request = false;");
+        out.println("\n    }");
+        out.println("\n  }");
+        out.println("\n}");
+        out.println("\nif (!request)");
+        out.println("\n  alert(\"Error al inicializar XMLHttpRequest!\");");
+        out.println("\n\nfunction vote(val) {");
+        out.println("\n    if (!invoke) return;");
+        out.println("\n    var url = \"" + url + "?value=\"+escape(val)+\"&" + tmpUrl + ";\n" +
+                "    request.open(\"GET\", url, true);");
+        out.println("\n    request.onreadystatechange = votedPage;");
+        out.println("\n    request.send(null);");
+        out.println("\n}");
+        out.println("\n\nfunction votedPage() {");
+        out.println("\n    var response = request.responseText;");
+        out.println("\n    if ('OK'==response)");
+        out.println("\n        alert('Voto contabilizado, muchas gracias por tu opinión!');");
+        out.println("\n    invoke = false;");
+        out.println("\n}");
+        out.println("\nvar invoke = true;");
+        url.setAction("abuseReport");
+        out.println("\nvar invokeAbused = true;");
+
+        out.println("\n\nfunction changeAbusedState() {");
+        out.println("\n    if (!invoke) return;");
+        out.println("\n    var url = \"" + url + "?" + tmpUrl + ";\n" +
+                "    request.open(\"GET\", url, true);");
+        out.println("\n    request.onreadystatechange = abuseStateChanged;");
+        out.println("\n    request.send(null);");
+        out.println("\n}");
+        out.println("\n\nfunction abuseStateChanged() {");
+        out.println("\n    var response = request.responseText;");
+        out.println("\n    if ('OK' == response) {");
+        out.println("\n      var etiqueta = document.getElementById(\"abused\").innerHTML;");
+        out.println("\n      if (etiqueta == 'Apropiado') {");
+        out.println("\n        document.getElementById(\"abused\").innerHTML == 'Inapropiado'");
+        out.println("\n      } else {");
+        out.println("\n        document.getElementById(\"abused\").innerHTML == 'Apropiado'");
+        out.println("\n      } ");
+        out.println("\n      invoke = false;");
+        out.println("\n    }");
+        out.println("\n}");
+        out.println("\n\nfunction addComment() {");
+/*        out.println("\n    if (document.getElementById(\"addComment\").style.visibility==\"visible\") {");
+        out.println("\n      document.getElementById(\"addComment\").style.visibility=\"hidden\";");
+        out.println("\n    } else {");*/
+        out.println("\n      document.getElementById(\"addComment\").style.display=\"inline\";");
+/*        out.println("\n    }");*/
+        out.println("\n}");
+        out.println("\n</script>\n");
+
+        //TODO: Cambiar los out.println de arriba por sb.append
+
+        sb.append("\n<div>");
+        sb.append("\n  <span>Calificar");
+        sb.append("\n    <table boder=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr>");
+        for (int i = 1; i <= 5; i++) {
+            sb.append(printStar(i, rank));
+        }
+        sb.append("\n      </tr>\n    </table>");
+        sb.append("\n  </span>");
+        sb.append("\n  <div>" + mse.getReviews() + " calificaciones</div>");
+        sb.append("\n  <span><a href=\"javascript:changeAbusedState();\">P&uacute;blicamente</a> <span id=\"abused\">"
+                + abusedDesc + "</span></span>");
+        sb.append("\n</div>");
+        sb.append("\n<div>");
+        sb.append("\n  <span>Comentarios</span>");
+        sb.append("\n  <div>&nbsp;</div>");
+        url.setAction("addComment");
+        sb.append("\n  <span><a href=\"javascript:addComment();\">Publicar comentario</a></span>");
+        sb.append("\n</div>");
+        sb.append("\n<div id=\"addComment\" style=\"display:none\">");
+        sb.append("\n  <br/>Comentario");
+        sb.append("\n  <form name=\"addCommentForm\" action=\"" + url + "\">");
+        sb.append("\n    <input type=\"hidden\" name=\"uri\" value=\"" + suri + "\">");
+        sb.append("\n    <textarea name=\"comentario\" cols=\"40\" rows=\"\"></textarea>");
+        sb.append("\n    <input type=\"submit\" value=\"Publicar comentario\">");
+        sb.append("\n  </form>");
+        sb.append("\n</div>");
+        sb.append(renderListComments(mse));
+
+        out.println(sb.toString());
+    }
+
+    /**
+     * Genera listado de comentarios asociados al MicroSiteElement
+     * @param paramRequest
+     * @param uri
+     * @return
+     * @throws org.semanticwb.portal.api.SWBResourceException
+     */
+    private String renderListComments(MicroSiteElement mse) {
+
+        StringBuilder ret = new StringBuilder(200);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy | HH:mm");
+        int ordinal = 1;
+
+        GenericIterator<Comment> iterator = mse.listComments();
+        ret.append("<div><table>\n");
+        while (iterator.hasNext()) {
+            Comment comment = iterator.next();
+            ret.append("<tr>\n");
+            ret.append("  <td>" + (ordinal++) + ". <strong>"
+                    + (comment.getCreator().getLogin().equalsIgnoreCase("")
+                       ? "Desconocido"
+                       : comment.getCreator().getLogin())
+                    + "</strong> (" + sdf.format(comment.getCreated())
+                    + ")<br/><div class\"cmnttxt\">" + comment.getDescription()
+                    + "</div></td>\n");
+            ret.append("</tr>\n");
+        }
+        ret.append("</table><div>\n");
+        return ret.toString();
+    }
+
+    private String printStar(int current, int rank) {
+
+        String url = "javascript:vote(" + current + ");";
+        int midl = (current * 10) - 7;
+        int midt = (current * 10) - 2;
+        String imgRank = emptyStarPath;
+        if (rank >= midl && rank <= midt) {
+            imgRank = halfStarPath;
+        }
+        if (rank > midt) {
+            imgRank = fullStarPath;
+        }
+        return ("<td><a href=\"" + url + "\" title=\"Asigna " + current
+                + " estrella" + (current > 1 ? "s" : "") + "\"><img border=\"0\" src=\""
+                + SWBPlatform.getContextPath() + imgRank + "\" alt=\"has "
+                + ((0.0f + rank) / 10.0f) + " star(s)\"/></a></td>");
+
     }
 
 }
