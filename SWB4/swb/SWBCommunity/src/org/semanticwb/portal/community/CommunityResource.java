f865d2da1b2c7d4ff9d0a5a57d519ccb298a41a2
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2009-08-14 03:45:09 +0000

    Con la adición de presentación de elementos generales para los recursos.

--- SWB4/swb/SWBCommunity/src/org/semanticwb/portal/community/CommunityResource.java
+++ SWB4/swb/SWBCommunity/src/org/semanticwb/portal/community/CommunityResource.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.http.*;
 import org.semanticwb.model.WebPage;
+import org.semanticwb.platform.SemanticObject;
 import org.semanticwb.portal.api.*;
 
 public class CommunityResource extends org.semanticwb.portal.community.base.CommunityResourceBase 
@@ -41,6 +42,83 @@ public class CommunityResource extends org.semanticwb.portal.community.base.Comm
         }else if("unsubscribe".equals(action))
         {
             if(page instanceof MicroSiteWebPageUtil)((MicroSiteWebPageUtil)page).unSubscribeFromElement(mem);
+        } else if ("vote".equals(action)) {
+            vote(request, response);
+        } else if ("abuseReport".equals(action)) {
+            abusedStateChange(request, response);
+        } else if ("addComment".equals(action)) {
+            addComment(request, response, mem);
+        }
+    }
+
+    private void vote(HttpServletRequest request, SWBActionResponse response) {
+
+        String suri = request.getParameter("uri");
+        SemanticObject so = null;
+        if (null != suri) {
+            so = SemanticObject.createSemanticObject(suri);
+        }
+        if (so.getGenericInstance() instanceof MicroSiteElement) {
+            MicroSiteElement mse = (MicroSiteElement) so.getGenericInstance();
+            int vote = 0;
+            try {
+                vote = Integer.parseInt(request.getParameter("value"));
+            } catch (Exception ne) {}
+            double rank = mse.getRank();
+            long rev = mse.getReviews();
+            response.setRenderParameter("uri", suri);
+
+            rank = rank * rev;
+            rev++;
+            rank = rank + vote;
+            rank = rank / rev;
+
+            mse.setRank(rank);
+            mse.setReviews(rev);
+        }
+    }
+
+    private void abusedStateChange(HttpServletRequest request,
+            SWBActionResponse response) {
+
+        String suri = request.getParameter("uri");
+        SemanticObject so = null;
+        if (null != suri) {
+            so = SemanticObject.createSemanticObject(suri);
+        }
+        if (so.getGenericInstance() instanceof MicroSiteElement) {
+            MicroSiteElement mse = (MicroSiteElement) so.getGenericInstance();
+            if (mse.isAbused()) {
+                mse.setAbused(false);
+            } else {
+                mse.setAbused(true);
+            }
+        }
+    }
+
+    private void addComment(HttpServletRequest request,
+            SWBActionResponse response, Member mem) {
+
+        String suri = request.getParameter("uri");
+        String desc = request.getParameter("comentario");
+        SemanticObject so = null;
+        if (null != suri) {
+            so = SemanticObject.createSemanticObject(suri);
+        }
+        if (desc != null) {
+            desc = "";
+        }
+        if (so.getGenericInstance() instanceof MicroSiteElement) {
+            MicroSiteElement mse = (MicroSiteElement) so.getGenericInstance();
+            if (mse.canComment(mem) && desc.length() > 0) {
+                SemanticObject semObj = MicroSiteElement.swbcomm_Comment.newInstance(MicroSiteElement.swbcomm_Comment.getURI());
+                if (semObj.getGenericInstance() instanceof Comment) {
+                    Comment comment = (Comment) semObj.getGenericInstance();
+                    comment.setDescription(desc);
+                    mse.addComment(comment);
+                }
+                response.setRenderParameter("uri", suri);
+            }
         }
     }
 
