c40e5867043e2a51242d7c59ca413aa507da6c37
jafernandez <jafernandez@60c509d2-c84e-0410-9847-e8532c4ce32d>
2009-08-17 18:07:53 +0000

    Se agregó método estático para la revisión y creación de la tabla swb_commlog.

---
+++ SWB4/swb/SWBCommunity/src/org/semanticwb/portal/community/UserActivitiesResource.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.portal.community;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.model.Resource;
+import org.semanticwb.model.User;
+import org.semanticwb.model.WebPage;
+import org.semanticwb.portal.api.GenericAdmResource;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.portal.community.utilresources.CommunityActivity;
+import org.semanticwb.portal.community.utilresources.CommunityActivityUtil;
+
+/**
+ *
+ * @author juan.fernandez
+ */
+public class UserActivitiesResource extends GenericAdmResource {
+
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy hh:mm");
+        Resource base = getResourceBase();
+        int numrec = Integer.parseInt(base.getAttribute("numrec","10"));
+        PrintWriter out = response.getWriter();
+        User user = paramRequest.getUser();
+
+        CommunityActivityUtil cau = new CommunityActivityUtil();
+        Iterator<CommunityActivity> itca = cau.getMemberActivities(user);
+        out.println("<div id=\"UserActivitiesResource\">");
+        out.println("<p>");
+        CommunityActivity ca = null;
+        MicroSiteElement mse = null;
+        MicroSite ms = null;
+        if (itca.hasNext()) {
+            int num = 0;
+            while (itca.hasNext()) {
+                num++;
+                if (num <= numrec) {
+                    ca = itca.next();
+                    user = ca.getUser();
+                    mse = ca.getElement();
+                    ms = ca.getCommunity();
+                    out.println("El elemento <strong>" + mse.getDisplayTitle(null) + "</strong>");
+                    out.println(" de la comunidad ");
+                    out.println("<strong>" + ms.getDisplayTitle(null) + "</strong>");
+                    out.println(" fue modificado el día <strong>" + sdf.format(mse.getUpdated()) + "</strong>");
+                    if (itca.hasNext() && num <= numrec) {
+                        out.println("<br/>");
+                    }
+                }
+            }
+        } else {
+            out.println("No hay actividades que reportar.");
+        }
+        out.println("</p>");
+        out.println("</div>");
+        
+
+    }
+
+}
---
+++ SWB4/swb/SWBCommunity/src/org/semanticwb/portal/community/utilresources/CommunityActivityUtil.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.portal.community.utilresources;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.model.User;
+import org.semanticwb.portal.community.MicroSite;
+
+/**
+ *
+ * @author juan.fernandez
+ */
+public class CommunityActivityUtil {
+    private Logger log = SWBUtils.getLogger(CommunityActivityUtil.class);
+
+    public CommunityActivityUtil(){}
+    
+    public Iterator<CommunityActivity> getComminityActivities(MicroSite community)
+    {
+        ArrayList al = new ArrayList();
+        Connection conn = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+
+            conn = SWBUtils.DB.getDefaultConnection("MicroSiteWebPageUtil.getComminityActivities");
+            pst = conn.prepareStatement("select commuri,usruri,elementuri,modified from swb_commlog where commuri = ? order by modified desc");
+            pst.setString(1,community.getURI());
+            rs = pst.executeQuery();
+            while(rs.next())
+            {
+                String commURI = rs.getString("commuri");
+                String usrURI = rs.getString("usruri");
+                String elementURI = rs.getString("elementuri");
+                Date modified = rs.getDate("modified");
+                al.add(new CommunityActivity(commURI, usrURI, elementURI,modified)); 
+            }
+            rs.close();
+            pst.close();
+            conn.close();
+        } catch (Exception e) {
+            log.error("Error al traer las actividades de la comunidad.",e);
+        } finally
+        {
+            try { rs.close(); } catch (Exception e) { }
+            try { pst.close(); } catch (Exception e) { }
+            try { conn.close(); } catch (Exception e) { }
+        }
+        return al.iterator();
+    }
+
+    public Iterator<CommunityActivity> getMemberActivities(User user)
+    {
+        ArrayList al = new ArrayList();
+        Connection conn = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        if(user!=null)
+        {
+            try {
+            conn = SWBUtils.DB.getDefaultConnection("MicroSiteWebPageUtil.getMemberActivities");
+            pst = conn.prepareStatement("select commuri,usruri,elementuri,modified from swb_commlog where usruri = ? order by modified desc");
+            pst.setString(1,user.getURI());
+            rs=pst.executeQuery();
+            while(rs.next())
+            {
+                String commURI = rs.getString("commuri");
+                String usrURI = rs.getString("usruri");
+                String elementURI = rs.getString("elementuri");
+                Date modified = rs.getDate("modified");
+                al.add(new CommunityActivity(commURI, usrURI, elementURI,modified));
+            }
+            rs.close();
+            pst.close();
+            conn.close();
+            } catch (Exception e) {
+                log.error("Error al traer las actividades del miembro en la comunidad.",e);
+            } finally {
+                try { rs.close(); } catch (Exception e) { }
+                try { pst.close(); } catch (Exception e) { }
+                try { conn.close(); } catch (Exception e) { }
+            }
+        }
+        return al.iterator();
+
+    }
+
+}
