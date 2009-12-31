e8713846231beb0ce698e7d5d677090d6d4f8bfb
redentor <JRedentor@users.noreply.github.com>
2009-12-31 01:51:52 +0000

    Con los comentarios de documentación completos hasta esta versión.

--- SWB4/swb/SWBBase/src/org/semanticwb/Logger.java
+++ SWB4/swb/SWBBase/src/org/semanticwb/Logger.java
@@ -25,148 +25,202 @@
 package org.semanticwb;
 
 /**
- *
+ * Registers data or messages in the application log file with different purposes.
+ * <p>Registra informaci&oacute;n o mensajes en el archivo de bit&aacute;cora de
+ * la aplicaci&oacute;n con diferentes prop&oacute;sitos.</p>
  * @author Javier Solis 
  */
 public interface Logger 
 {
     /*
-     * Usado para debugueos mas profundos
+     * Los 3 siguientes metodos son usados para debugueos mas profundos
      */
     /**
-     * 
-     * @param txt
-     * @param t
+     * Registers the elements of the execution stack at the moment it is called 
+     * along with a message, to provide information about an exceptional condition
+     * in order to trace the origin of that exceptional condition.
+     * <p>Registra los elementos de la pila de ejecuci&oacute;n del hilo al momento
+     * en que es llamado, junto con un mensaje para proveer informaci&oacute;n
+     * sobre una condici&oacute;n excepcional a fin de rastrear su origen.</p>
+     * @param txt a string representing the message to include in the app's log
+     * @param t a throwable object indicating an exceptional condition
      */
     public void trace(String txt, Throwable t);
+    
     /**
-     * 
-     * @param txt
+     * Registers a message to provide information about an exceptional condition
+     * in order to trace the origin of that exceptional condition.
+     * <p>Registra un mensaje para proveer informaci&oacute;n sobre una condici&oacute;n
+     * excepcional a fin de rastrear su origen.</p>
+     * @param txt a string representing the message to include in the app's log
      */
     public void trace(String txt);
+    
     /**
-     * 
-     * @param t
+     * Registers the elements of the execution stack at the moment it is called
+     * to provide information about an exceptional condition in order to trace the
+     * origin of that exceptional condition.
+     * <p>Registra los elementos de la pila de ejecuci&oacute;n del hilo al momento
+     * en que es llamado para proveer informaci&oacute;n sobre una condici&oacute;n
+     * excepcional a fin de rastrear su origen.</p>
+     * @param t a throwable object indicating an exceptional condition
      */
     public void trace(Throwable t);
 
     /*
-     * Usado para debugueos
+     * Los 3 siguientes metodos son usados para debugueos sencillos
      */
     /**
-     * 
-     * @param txt
-     * @param t
+     * Registers an exceptional condition along with a message, to provide
+     * information about that exceptional condition.
+     * <p>Registra los elementos de la pila de ejecuci&oacute;n del hilo al momento
+     * en que es llamado, junto con un mensaje, para proveer informaci&oacute;n
+     * sobre una condici&oacute;n excepcional.</p>
+     * @param txt a string representing the message to include in the app's log
+     * @param t a throwable object indicating an exceptional condition
      */
     public void debug(String txt, Throwable t);
+    
     /**
-     * 
-     * @param txt
+     * Registers a message to provide information about an exceptional condition.
+     * <p>Registra un mensaje para proveer informaci&oacute;n sobre una
+     * condici&oacute;n excepcional.</p>
+     * @param txt a string representing the message to include in the app's log
      */
     public void debug(String txt);
+
     /**
-     * 
-     * @param t
+     * Registers an exceptional condition in the execution of the current thread.
+     * <p>Registra una condici&oacute;n excepcional en la ejecuci&oacute;n del
+     * hilo actual.</p>
+     * @param t a throwable object indicating an exceptional condition
      */
     public void debug(Throwable t);
 
     /*
-     * Usado para Informacion del Sistema 
+     * Los 3 siguientes metodos son usados para Informacion del Sistema
      */
     /**
-     * 
-     * @param txt
-     * @param t
+     * Registers the data needed to provide detail on an exceptional condition.
+     * @param txt a string representing the message to include in the app's log
+     * @param t a throwable object indicating an exceptional condition
      */
     public void info(String txt, Throwable t);
+
     /**
-     * 
-     * @param txt
+     * Registers a message to provide detail on an exceptional condition.
+     * @param txt a string representing the message to include in the app's log
      */
     public void info(String txt);
+
     /**
-     * 
-     * @param t
+     * Registers an exceptional condition in an informative manner.
+     * @param t a throwable object indicating an exceptional condition
      */
     public void info(Throwable t);
 
     /*
-     * Usado para Warnings
+     * Los 3 siguientes metodos son usados para registrar advertencias
      */
     /**
-     * 
-     * @param txt
-     * @param t
+     * Registers data as a warning on an exceptional condition generated.
+     * <p>Registra informaci&oacute;n a manera de advertencia sobre una condici&oacute;n
+     * excepcional generada.</p>
+     * @param txt a string representing the message to include in the app's log
+     * @param t a throwable object indicating an exceptional condition
      */
     public void warn(String txt, Throwable t);
+    
     /**
-     * 
-     * @param txt
+     * Registers a message as a warning on an exceptional condition generated.
+     * <p>Registra un mensaje a manera de advertencia sobre una condici&oacute;n
+     * excepcional generada.</p>
+     * @param txt a string representing the message to include in the app's log
      */
     public void warn(String txt);
+
     /**
-     * 
-     * @param t
+     * Registers data as a warning on an exceptional condition generated.
+     * <p>Registra informaci&oacute;n a manera de advertencia sobre una condici&oacute;n
+     * excepcional generada.</p>
+     * @param t a throwable object indicating an exceptional condition
      */
     public void warn(Throwable t);
 
     /*
-     * Usado para Errores
+     * Los 3 siguientes metodos son usados para registrar errores
      */
     /**
-     * 
-     * @param txt
-     * @param t
+     * Registers data to report an error in the application.
+     * <p>Registra informaci&oacute;n para reportar un error en la aplicaci&oacute;n.</p>
+     * @param txt a string representing the message to include in the app's log
+     * @param t a throwable object indicating an exceptional condition
      */
     public void error(String txt, Throwable t);
+
     /**
-     * 
-     * @param txt
+     * Registers a message to report an error in the application.
+     * <p>Registra un mensaje para reportar un error en la aplicaci&oacute;n.</p>
+     * @param txt a string representing the message to include in the app's log
      */
     public void error(String txt);
+
     /**
-     * 
-     * @param t
+     * Registers data to report an error in the application.
+     * <p>Registra informaci&oacute;n para reportar un error en la aplicaci&oacute;n.</p>
+     * @param t a throwable object indicating an exceptional condition
      */
     public void error(Throwable t);
 
     /*
-     * Usado para Errores RunTime Exceptions
+     * Los 3 siguientes metodos son usados para registrar excepciones en tiempo de ejecuci&oacute;n
      */
     /**
-     * 
-     * @param txt
-     * @param t
+     * Registers data of an exceptional condition generated at runtime.
+     * <p>Registra informaci&oacute;n sobre una condici&oacute;n excepcional generada
+     * en tiempo de ejecuci&oacute;n.</p>
+     * @param txt a string representing the message to include in the app's log
+     * @param t a throwable object indicating an exceptional condition
      */
     public void fatal(String txt, Throwable t);
+    
     /**
-     * 
-     * @param txt
+     * Registers a message about an exceptional condition generated at runtime.
+     * <p>Registra un mensaje acerca de una condici&oacute;n excepcional generada
+     * en tiempo de ejecuci&oacute;n.</p>
+     * @param txt a string representing the message to include in the app's log
      */
     public void fatal(String txt);
     /**
-     * 
-     * @param t
+     * Registers data of an exceptional condition generated at runtime.
+     * <p>Registra informaci&oacute;n sobre una condici&oacute;n excepcional generada
+     * en tiempo de ejecuci&oacute;n.</p>
+     * @param t a throwable object indicating an exceptional condition
      */
     public void fatal(Throwable t);
     
     /*
-     * Usado para notificar eventos
+     * Los 3 siguientes metodos son usados para notificar eventos
      */
     /**
-     * 
-     * @param txt
-     * @param t
+     * Registers data when certain event occurs.
+     * <p>Registra informaci&oacute;n cuando cierto evento ocurre.</p>
+     * @param txt a string representing the message to include in the app's log
+     * @param t a throwable object indicating an exceptional condition
      */
     public void event(String txt, Throwable t);
+
     /**
-     * 
-     * @param txt
+     * Registers a message when certain event occurs.
+     * <p>Registra un mensaje cuando cierto evento ocurre.</p>
+     * @param txt a string representing the message to include in the app's log
      */
     public void event(String txt);
+
     /**
-     * 
-     * @param t
+     * Registers data when certain event occurs.
+     * <p>Registra informaci&oacute;n cuando cierto evento ocurre.</p>
+     * @param t a throwable object indicating an exceptional condition
      */
     public void event(Throwable t);
     
