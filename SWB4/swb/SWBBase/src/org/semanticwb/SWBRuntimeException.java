fc94ad26804620bd0e1cb67a8da82dc9de7f1f66
redentor <JRedentor@users.noreply.github.com>
2010-01-05 00:35:53 +0000

    Con los comentarios de documentación completos hasta esta versión.

--- SWB4/swb/SWBBase/src/org/semanticwb/SWBRuntimeException.java
+++ SWB4/swb/SWBBase/src/org/semanticwb/SWBRuntimeException.java
@@ -21,32 +21,48 @@
 *  http://www.semanticwebbuilder.org
 **/ 
  
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.semanticwb;
 
+
 /**
- *
- * @author Jei
+ * A specialization of {@code RuntimeException} for the SemanticWebBuilder API.
+ * <p>Una especializaci&oacute;n de {@code RuntimeException} para el API de SemanticWebBuilder.</p>
+ * @author Javier Solis Gonzalez
  */
 public class SWBRuntimeException extends RuntimeException
 {
+
+
     public SWBRuntimeException()
     {
         super();
     }
     
+    /**
+     * Generates an instance of this exception with a specified detail message.
+     * <p>Genera una instancia de esta excepci&oacute;n con un mensaje de detalle
+     * especificado.</p>
+     * @param msg the detail message (which is saved for later retrieval by the
+     *            {@link Throwable.getMessage() Throwable.getMessage()} method)
+     */
     public SWBRuntimeException(String msg)
     {
         super(msg);
     }
 
+    /**
+     * Generates an instance of this exception with the specified detail message and cause.
+     * <p>Genera una instancia de esta excepci&oacute;n con el mensaje de detalle
+     * y causa especificados.</p>
+     * @param msg the detail message (which is saved for later retrieval by the
+     *            Throwable.getMessage() method)
+     * @param e the cause (which is saved for later retrieval by the {@link Throwable.getCause() Throwable.getCause()}
+     *          method). (A {@code null} value is permitted, and indicates that
+     *          the cause is nonexistent or unknown.)
+     */
     public SWBRuntimeException(String msg, Exception e)
     {
-        super(msg,e);
+        super(msg, e);
     }    
 
 }
