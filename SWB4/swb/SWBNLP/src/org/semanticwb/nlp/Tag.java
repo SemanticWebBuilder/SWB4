b7c75f032680b9f04dd375f4eecac0558e448c7d
Hasdai Pacheco <haxdai@gmail.com>
2010-01-15 18:12:56 +0000

    Creado

--- SWB4/swb/SWBNLP/src/org/semanticwb/nlp/Tag.java
+++ SWB4/swb/SWBNLP/src/org/semanticwb/nlp/Tag.java
@@ -11,6 +11,8 @@ package org.semanticwb.nlp;
  */
 public class Tag {
     private String label;
+    private String URI;
+    private String clsId;
 
     public Tag () {
         this("");
@@ -22,10 +24,34 @@ public class Tag {
 
     public void setTag (String t) {
         label = t;
-    }
+    }    
 
     public String getTag() {
         return label;
     }
 
+    public String getId() {
+        return clsId;
+    }
+
+    public void setId(String classId) {
+        clsId = classId;
+    }
+
+    public String getURI() {
+        return URI;
+    }
+
+    public void setURI(String uri) {
+        URI = uri;
+    }
+
+    public boolean equals(Tag tag) {
+        boolean ret = false;
+        if (tag.getTag().equalsIgnoreCase(label) &&
+                tag.getURI().equals(URI)) {
+            ret = true;
+        }
+        return ret;
+    }
 }
