f59196113b34b7d2e38dc19a2a2241497bc28d98
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2010-01-18 20:16:39 +0000

    Creado

---
+++ SWB4/swb/SWBNLP/src/org/semanticwb/nlp/analysis/EntityRecognizer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.nlp.analysis;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ *
+ * @author Hasdai Pacheco {haxdai@gmail.com}
+ */
+public class EntityRecognizer {
+
+    public EntityRecognizer() throws IOException {
+        NGramAnalyzer an = new NGramAnalyzer(2, 5);
+        String test = "nombre(s),  correo electrónico  de usuario";
+        StringReader reader = new StringReader(test);
+
+        NgramWordTokenizer ngwt = new NgramWordTokenizer(reader, 2, 5);
+
+        Token tk = new Token();
+        while((tk = ngwt.next(tk)) != null) {
+            String text = tk.getText();
+            System.out.println("===> " + text);
+        }        
+    }
+}
