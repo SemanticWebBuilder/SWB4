ff3b63abb24613aead601cc9dcc0de154dc10dfc
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2010-02-03 18:52:14 +0000

    Monitor Beta y GC

--- SWB4/swb/SWBPortal/src/org/semanticwb/portal/monitor/SWBMemoryPoolStat.java
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/monitor/SWBMemoryPoolStat.java
@@ -1,82 +1,87 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.semanticwb.portal.monitor;
 
 import java.lang.management.MemoryUsage;
+
 /**
  *
  * @author serch
  */
-public class SWBMemoryPoolStat {
+public class SWBMemoryPoolStat
+{
 
-    private String      poolName; 
-    private long        usageThreshold; 
-    private MemoryUsage usage; 
-    private long        lastGcId;
-    private long        lastGcStartTime;
-    private long        lastGcEndTime;
-    private long        collectThreshold; 
-    private MemoryUsage beforeGcUsage; 
+    private String poolName;
+    private long usageThreshold;
+    private MemoryUsage usage;
+    private long lastGcId;
+    private long lastGcStartTime;
+    private long lastGcEndTime;
+    private long collectThreshold;
+    private MemoryUsage beforeGcUsage;
     private MemoryUsage afterGcUsage;
-    
-    SWBMemoryPoolStat(String name, 
-                   long usageThreshold, 
-                   MemoryUsage usage, 
-                   long lastGcId,
-                   long lastGcStartTime,
-                   long lastGcEndTime,
-                   long collectThreshold, 
-                   MemoryUsage beforeGcUsage, 
-                   MemoryUsage afterGcUsage) {
-        this.poolName = name; 
-        this.usageThreshold = usageThreshold; 
-        this.usage = usage; 
+
+    SWBMemoryPoolStat(String name,
+            long usageThreshold,
+            MemoryUsage usage,
+            long lastGcId,
+            long lastGcStartTime,
+            long lastGcEndTime,
+            long collectThreshold,
+            MemoryUsage beforeGcUsage,
+            MemoryUsage afterGcUsage)
+    {
+        this.poolName = name;
+        this.usageThreshold = usageThreshold;
+        this.usage = usage;
         this.lastGcId = lastGcId;
         this.lastGcStartTime = lastGcStartTime;
         this.lastGcEndTime = lastGcEndTime;
-        this.collectThreshold = collectThreshold; 
-        this.beforeGcUsage = beforeGcUsage; 
+        this.collectThreshold = collectThreshold;
+        this.beforeGcUsage = beforeGcUsage;
         this.afterGcUsage = afterGcUsage;
     }
 
-       public String getPoolName() {
+    public String getPoolName()
+    {
         return poolName;
     }
 
-       public MemoryUsage getUsage() {
+    public MemoryUsage getUsage()
+    {
         return usage;
     }
 
-    public long getUsageThreshold() {
+    public long getUsageThreshold()
+    {
         return usageThreshold;
     }
 
-    public long getCollectionUsageThreshold() {
+    public long getCollectionUsageThreshold()
+    {
         return collectThreshold;
     }
 
-    public long getLastGcId() {
+    public long getLastGcId()
+    {
         return lastGcId;
     }
 
-
-    public long getLastGcStartTime() {
+    public long getLastGcStartTime()
+    {
         return lastGcStartTime;
     }
 
-    public long getLastGcEndTime() {
+    public long getLastGcEndTime()
+    {
         return lastGcEndTime;
     }
 
-    public MemoryUsage getBeforeGcUsage() {
-        return beforeGcUsage; 
+    public MemoryUsage getBeforeGcUsage()
+    {
+        return beforeGcUsage;
     }
 
-    public MemoryUsage getAfterGcUsage() {
-        return beforeGcUsage; 
+    public MemoryUsage getAfterGcUsage()
+    {
+        return beforeGcUsage;
     }
-
 }
--- SWB4/swb/SWBPortal/src/org/semanticwb/portal/monitor/SWBSummaryData.java
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/monitor/SWBSummaryData.java
@@ -1,7 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+* SemanticWebBuilder es una plataforma para el desarrollo de portales y aplicaciones de integración,
+* colaboración y conocimiento, que gracias al uso de tecnología semántica puede generar contextos de
+* información alrededor de algún tema de interés o bien integrar información y aplicaciones de diferentes
+* fuentes, donde a la información se le asigna un significado, de forma que pueda ser interpretada y
+* procesada por personas y/o sistemas, es una creación original del Fondo de Información y Documentación
+* para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite.
+*
+* INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público (‘open source’),
+* en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC lo ha diseñado y puesto a su disposición;
+* aprender de él; distribuirlo a terceros; acceder a su código fuente y modificarlo, y combinarlo o enlazarlo con otro software,
+* todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización
+* del SemanticWebBuilder 4.0.
+*
+* INFOTEC no otorga garantía sobre SemanticWebBuilder, de ninguna especie y naturaleza, ni implícita ni explícita,
+* siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los riesgos que puedan derivar
+* de la misma.
+*
+* Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposición la siguiente
+* dirección electrónica:
+*  http://www.semanticwebbuilder.org
+**/
+
 
 package org.semanticwb.portal.monitor;
 
@@ -50,7 +69,7 @@ public class SWBSummaryData implements Serializable {
             ret.append("<ul>\n<li>Sistema Operatvo: "+osName+"</li>\n");
             ret.append("<li>Arquitectura: "+vmArch+"</li>\n");
             ret.append("<li>Procesadores: "+nCPUs+"</li>\n");
-            ret.append("<li>Memoria Virutal en Uso: "+formatLong(commitedVirtualMem/1024)+" kbytes</li>\n");
+            ret.append("<li>Memoria Virutal Accesible: "+formatLong(commitedVirtualMem/1024)+" kbytes</li>\n");
             ret.append("<li>Total Memoria F&iacute;sica: "+formatLong(totalPhysicalMem/1024)+" kbytes</li>\n");
             ret.append("<li>Memoria f&iacute;sica Libre: "+formatLong(freePhysicalMem/1024)+" kbytes</li>\n");
             ret.append("<li>Total Memoria SWAP: "+formatLong(totalSwapMem/1024)+" kbytes</li>\n");
