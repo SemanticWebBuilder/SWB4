42a51def970680e176e8a50b256da719e0b80e1e
serch <serch@60c509d2-c84e-0410-9847-e8532c4ce32d>
2010-02-03 18:52:14 +0000

    Monitor Beta y GC

--- SWB4/swb/SWBPortal/src/org/semanticwb/portal/monitor/SWBLocalMemoryPool.java
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/monitor/SWBLocalMemoryPool.java
@@ -1,10 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * SemanticWebBuilder es una plataforma para el desarrollo de portales y aplicaciones de integración,
+ * colaboración y conocimiento, que gracias al uso de tecnología semántica puede generar contextos de
+ * información alrededor de algún tema de interés o bien integrar información y aplicaciones de diferentes
+ * fuentes, donde a la información se le asigna un significado, de forma que pueda ser interpretada y
+ * procesada por personas y/o sistemas, es una creación original del Fondo de Información y Documentación
+ * para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite.
+ *
+ * INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público (‘open source’),
+ * en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC lo ha diseñado y puesto a su disposición;
+ * aprender de él; distribuirlo a terceros; acceder a su código fuente y modificarlo, y combinarlo o enlazarlo con otro software,
+ * todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización
+ * del SemanticWebBuilder 4.0.
+ *
+ * INFOTEC no otorga garantía sobre SemanticWebBuilder, de ninguna especie y naturaleza, ni implícita ni explícita,
+ * siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los riesgos que puedan derivar
+ * de la misma.
+ *
+ * Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposición la siguiente
+ * dirección electrónica:
+ *  http://www.semanticwebbuilder.org
+ **/
+
 
 package org.semanticwb.portal.monitor;
-
 
 import static java.lang.management.ManagementFactory.*;
 import com.sun.management.GarbageCollectorMXBean;
@@ -14,63 +32,72 @@ import java.lang.management.MemoryUsage;
 import java.util.*;
 import javax.management.ObjectName;
 
-
 /**
  *
  * @author serch
  */
-public class SWBLocalMemoryPool {
+public class SWBLocalMemoryPool
+{
+
     private String poolName;
-    private ObjectName  objName;
+   // private ObjectName objName;
     private MemoryPoolMXBean pool;
-    private Map<ObjectName,Long> gcMBeans;
+    private Map<ObjectName, Long> gcMBeans;
     private GcInfo lastGcInfo;
 
-    public SWBLocalMemoryPool(ObjectName poolName) throws java.io.IOException {
-        this.objName = objName;
+    public SWBLocalMemoryPool(ObjectName poolName) throws java.io.IOException
+    {
+        //this.objName = objName;
         this.pool = newPlatformMXBeanProxy(getPlatformMBeanServer(),
-                                      poolName.toString(),
-                                      MemoryPoolMXBean.class);
+                poolName.toString(),
+                MemoryPoolMXBean.class);
         this.poolName = this.pool.getName();
-        this.gcMBeans = new HashMap<ObjectName,Long>();
+        this.gcMBeans = new HashMap<ObjectName, Long>();
         this.lastGcInfo = null;
 
         String[] mgrNames = pool.getMemoryManagerNames();
-        for (String name : mgrNames) {
-            try {
-                ObjectName mbeanName = new ObjectName(GARBAGE_COLLECTOR_MXBEAN_DOMAIN_TYPE +
-                                                      ",name=" + name);
-                if (getPlatformMBeanServer().isRegistered(mbeanName)) {
+        for (String name : mgrNames)
+        {
+            try
+            {
+                ObjectName mbeanName = new ObjectName(GARBAGE_COLLECTOR_MXBEAN_DOMAIN_TYPE + ",name=" + name);
+                if (getPlatformMBeanServer().isRegistered(mbeanName))
+                {
                     gcMBeans.put(mbeanName, new Long(0));
                 }
-            } catch (Exception e) {
+            } catch (Exception e)
+            {
                 assert false;
             }
 
         }
     }
 
-    public boolean isCollectedMemoryPool() {
+    public boolean isCollectedMemoryPool()
+    {
         return (gcMBeans.size() != 0);
     }
 
-    public ObjectName getObjectName() {
-        return objName;
-    }
+//    public ObjectName getObjectName()
+//    {
+//        return objName;
+//    }
 
-    public SWBMemoryPoolStat getStat() throws java.io.IOException {
+    public SWBMemoryPoolStat getStat() throws java.io.IOException
+    {
         long usageThreshold = (pool.isUsageThresholdSupported()
-                                  ? pool.getUsageThreshold()
-                                  : -1);
+                ? pool.getUsageThreshold()
+                : -1);
         long collectThreshold = (pool.isCollectionUsageThresholdSupported()
-                                  ? pool.getCollectionUsageThreshold()
-                                  : -1);
+                ? pool.getCollectionUsageThreshold()
+                : -1);
         long lastGcStartTime = 0;
         long lastGcEndTime = 0;
         MemoryUsage beforeGcUsage = null;
         MemoryUsage afterGcUsage = null;
         long gcId = 0;
-        if (lastGcInfo != null) {
+        if (lastGcInfo != null)
+        {
             gcId = lastGcInfo.getId();
             lastGcStartTime = lastGcInfo.getStartTime();
             lastGcEndTime = lastGcInfo.getEndTime();
@@ -78,39 +105,41 @@ public class SWBLocalMemoryPool {
             afterGcUsage = lastGcInfo.getMemoryUsageAfterGc().get(poolName);
         }
 
-        Set<Map.Entry<ObjectName,Long>> set = gcMBeans.entrySet();
-        for (Map.Entry<ObjectName,Long> e : set) {
+        Set<Map.Entry<ObjectName, Long>> set = gcMBeans.entrySet();
+        for (Map.Entry<ObjectName, Long> e : set)
+        {
             GarbageCollectorMXBean gc =
-                newPlatformMXBeanProxy(getPlatformMBeanServer(),
-                                      e.getKey().getCanonicalName(),
-                                      com.sun.management.GarbageCollectorMXBean.class);
+                    newPlatformMXBeanProxy(getPlatformMBeanServer(),
+                    e.getKey().getCanonicalName(),
+                    com.sun.management.GarbageCollectorMXBean.class);
             Long gcCount = e.getValue();
             Long newCount = gc.getCollectionCount();
-            if (newCount > gcCount) {
+            if (newCount > gcCount)
+            {
                 gcMBeans.put(e.getKey(), new Long(newCount));
                 lastGcInfo = gc.getLastGcInfo();
-                if (lastGcInfo.getEndTime() > lastGcEndTime) {
+                if (lastGcInfo.getEndTime() > lastGcEndTime)
+                {
                     gcId = lastGcInfo.getId();
                     lastGcStartTime = lastGcInfo.getStartTime();
                     lastGcEndTime = lastGcInfo.getEndTime();
                     beforeGcUsage = lastGcInfo.getMemoryUsageBeforeGc().get(poolName);
                     afterGcUsage = lastGcInfo.getMemoryUsageAfterGc().get(poolName);
-                    assert(beforeGcUsage != null);
-                    assert(afterGcUsage != null);
+                    assert (beforeGcUsage != null);
+                    assert (afterGcUsage != null);
                 }
             }
         }
 
         MemoryUsage usage = pool.getUsage();
         return new SWBMemoryPoolStat(poolName,
-                                  usageThreshold,
-                                  usage,
-                                  gcId,
-                                  lastGcStartTime,
-                                  lastGcEndTime,
-                                  collectThreshold,
-                                  beforeGcUsage,
-                                  afterGcUsage);
+                usageThreshold,
+                usage,
+                gcId,
+                lastGcStartTime,
+                lastGcEndTime,
+                collectThreshold,
+                beforeGcUsage,
+                afterGcUsage);
     }
-
 }
---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/monitor/SWBMonitorBeans.java
@@ -0,0 +1,87 @@
+/**
+ * SemanticWebBuilder es una plataforma para el desarrollo de portales y aplicaciones de integración,
+ * colaboración y conocimiento, que gracias al uso de tecnología semántica puede generar contextos de
+ * información alrededor de algún tema de interés o bien integrar información y aplicaciones de diferentes
+ * fuentes, donde a la información se le asigna un significado, de forma que pueda ser interpretada y
+ * procesada por personas y/o sistemas, es una creación original del Fondo de Información y Documentación
+ * para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite.
+ *
+ * INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público (‘open source’),
+ * en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC lo ha diseñado y puesto a su disposición;
+ * aprender de él; distribuirlo a terceros; acceder a su código fuente y modificarlo, y combinarlo o enlazarlo con otro software,
+ * todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización
+ * del SemanticWebBuilder 4.0.
+ *
+ * INFOTEC no otorga garantía sobre SemanticWebBuilder, de ninguna especie y naturaleza, ni implícita ni explícita,
+ * siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los riesgos que puedan derivar
+ * de la misma.
+ *
+ * Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposición la siguiente
+ * dirección electrónica:
+ *  http://www.semanticwebbuilder.org
+ **/
+package org.semanticwb.portal.monitor;
+
+import java.io.IOException;
+import static java.lang.management.ManagementFactory.*;
+import java.lang.management.ClassLoadingMXBean;
+import java.lang.management.CompilationMXBean;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
+import java.lang.management.ThreadMXBean;
+
+/**
+ *
+ * @author serch
+ */
+public class SWBMonitorBeans
+{
+
+    ClassLoadingMXBean classLoadingMBean = null;
+    CompilationMXBean compilationMBean = null;
+    MemoryMXBean memoryMBean = null;
+    OperatingSystemMXBean operatingSystemMBean = null;
+    RuntimeMXBean runtimeMBean = null;
+    ThreadMXBean threadMBean = null;
+    com.sun.management.OperatingSystemMXBean sunOperatingSystemMXBean = null;
+    long prevUpTime = -1l, prevProcessCpuTime = 0l;
+
+    public float updateCPUInfo(SWBMonitorData data)
+    {
+        float cpuUsage = Float.MIN_VALUE;
+        if (prevUpTime > 0L && data.upTime > prevUpTime)
+        {
+            // elapsedCpu is in ns and elapsedTime is in ms.
+            long elapsedCpu = data.processCpuTime - prevProcessCpuTime;
+            long elapsedTime = data.upTime - prevUpTime;
+            // cpuUsage could go higher than 100% because elapsedTime
+            // and elapsedCpu are not fetched simultaneously. Limit to
+            // 99% to avoid Plotter showing a scale from 0% to 200%.
+            cpuUsage =
+                    Math.min(99.99F,
+                    elapsedCpu / (elapsedTime * 10000F * data.nCPUs));
+        }
+        this.prevUpTime = data.upTime;
+        this.prevProcessCpuTime = data.processCpuTime;
+        return cpuUsage;
+    }
+
+    public SWBMonitorBeans()
+    {
+        classLoadingMBean = getClassLoadingMXBean();
+        compilationMBean = getCompilationMXBean();
+        memoryMBean = getMemoryMXBean();
+        operatingSystemMBean = getOperatingSystemMXBean();
+        runtimeMBean = getRuntimeMXBean();
+        threadMBean = getThreadMXBean();
+
+        try
+        {
+            sunOperatingSystemMXBean = newPlatformMXBeanProxy(getPlatformMBeanServer(), OPERATING_SYSTEM_MXBEAN_NAME, com.sun.management.OperatingSystemMXBean.class);
+        } catch (IOException ex)
+        {
+            assert (false);
+        }
+    }
+}
