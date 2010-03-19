fe37db641dd0f76c777e305b3fce12077dc41da8
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2010-03-19 02:08:04 +0000

    User Patch

--- SWB4/swb/SWBPortal/src/org/semanticwb/security/auth/FBLoginModule.java
+++ SWB4/swb/SWBPortal/src/org/semanticwb/security/auth/FBLoginModule.java
@@ -23,6 +23,7 @@ import javax.security.auth.spi.LoginModule;
 import org.semanticwb.Logger;
 import org.semanticwb.SWBUtils;
 import org.semanticwb.model.SWBContext;
+import org.semanticwb.model.SWBSessionUser;
 import org.semanticwb.model.User;
 import org.semanticwb.model.UserRepository;
 import org.semanticwb.model.WebSite;
@@ -206,14 +207,23 @@ public class FBLoginModule implements LoginModule
         }
         Iterator it = subject.getPrincipals().iterator();
         User tmp = null;
-        if (it.hasNext())
+        while(it.hasNext())
         {
-            tmp = (User) it.next();
+            tmp=(User)it.next();
+            if(principal.getUserRepository().equals(tmp.getUserRepository()))
+            {
+                break;
+            }
         }
         if (null != tmp)
         {
-            tmp.getSemanticObject().setRDFResource(principal.getSemanticObject().getRDFResource());
+            ((SWBSessionUser)tmp).updateUser(principal);
+            //tmp.getSemanticObject().setRDFResource(principal.getSemanticObject().getRDFResource());
             flag = true;
+            //TODO: Pendiente
+            //subject.getPrincipals().clear();
+            //principal.setDefaultData(tmp);
+            //subject.getPrincipals().add(principal);
         } else
         {
             subject.getPrincipals().add(principal);
