257a6efb1ce9837b1619ee6c824c22f964efa8d8
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2010-07-23 20:35:54 +0000

    Agregue metodo nodeToString

---
+++ SWB4/swb/SWBFormsProcesor/src/org/semanticwb/process/forms/util/SWBFormsUitls.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.process.forms.util;
+
+
+
+/**
+ *
+ * @author jorge.jimenez
+ */
+public class SWBFormsUitls {
+
+}
