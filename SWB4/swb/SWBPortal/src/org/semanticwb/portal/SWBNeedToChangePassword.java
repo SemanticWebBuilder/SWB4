caeab6bd09ec4fa73186fb6d751800ef149f46cc
serch <serch@60c509d2-c84e-0410-9847-e8532c4ce32d>
2010-12-30 17:36:23 +0000

    Force password change

---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/SWBNeedToChangePassword.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.portal;
+
+/**
+ *
+ * @author serch
+ */
+public class SWBNeedToChangePassword extends RuntimeException{
+
+    public SWBNeedToChangePassword(String message)
+    {
+        super(message);
+    }
+
+}
