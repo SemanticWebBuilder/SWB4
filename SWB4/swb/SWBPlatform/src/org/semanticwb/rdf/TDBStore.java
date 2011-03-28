afc2302b057e2a9064f7b9d681b37006007292e0
Javier Solis <softjei@gmail.com>
2011-03-28 17:27:32 +0000

    Cache error

---
+++ SWB4/swb/SWBPlatform/src/org/semanticwb/rdf/TDBStore.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.rdf;
+
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.tdb.TDB;
+import com.hp.hpl.jena.tdb.TDBFactory;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBPlatform;
+import org.semanticwb.SWBUtils;
+
+/**
+ *
+ * @author jei
+ */
+public class TDBStore implements AbstractStore
+{
+    private static Logger log=SWBUtils.getLogger(TDBStore.class);
+
+    /** The Dataset */
+    private Dataset set;
+    /** The timer. */
+    private Timer timer;                        //Commiter
+
+
+    public void init()
+    {
+        log.info("TDB Detected...," + SWBPlatform.createInstance().getPlatformWorkPath() + "/data");
+        TDB.getContext().set(TDB.symUnionDefaultGraph,true);
+        set = TDBFactory.createDataset(SWBPlatform.createInstance().getPlatformWorkPath() + "/data");
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                commit();
+            }
+        }, 60000, 30000);
+    }
+
+    public void removeModel(String name)
+    {
+        Model model=loadModel(name);
+        if(model!=null)model.removeAll();
+    }
+
+    public Model loadModel(String name)
+    {
+        return set.getNamedModel(name);
+        //return SDBFactory.connectNamedModel(store, name);
+    }
+
+    public Iterator<String> listModelNames()
+    {
+        return set.listNames();
+    }
+
+    /**
+     * Commit all models.
+     */
+    private void commit()
+    {
+        log.trace("ServerMgr.Commit()");
+        try {
+            Iterator<String> it = listModelNames();
+            while (it.hasNext()) {
+                Model model = loadModel(it.next());
+                model.commit();
+            }
+        } catch (ConcurrentModificationException noe) {
+        } catch (Exception e) {
+            log.error(e);
+        }
+    }
+
+    public void close()
+    {
+        //System.out.println("ServerMgr.Close()");
+        timer.cancel();
+        timer=null;
+        commit();
+        set.close();
+    }
+
+    public Dataset getDataset()
+    {
+        return set;
+    }
+    
+}
