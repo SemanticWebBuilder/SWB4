3653896561b8d29b30110b2e9c94b8e4122497d9
martha <martha@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-07-05 02:41:38 +0000

    Recurso semántico: Calendario de eventos

---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/sem/calendar/CalendarSubscription.java
@@ -0,0 +1,13 @@
+package org.semanticwb.portal.resources.sem.calendar;
+
+
+   /**
+   * Almacena la Subscripción de un usuario a un calendario 
+   */
+public class CalendarSubscription extends org.semanticwb.portal.resources.sem.calendar.base.CalendarSubscriptionBase 
+{
+    public CalendarSubscription(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/sem/calendar/Event.java
@@ -0,0 +1,13 @@
+package org.semanticwb.portal.resources.sem.calendar;
+
+
+   /**
+   * Almacena una serie de Eventos asociados a una instancia de tipo Calendario 
+   */
+public class Event extends org.semanticwb.portal.resources.sem.calendar.base.EventBase 
+{
+    public Event(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
