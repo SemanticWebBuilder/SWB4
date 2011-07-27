1d0af607e6a3ddf622f29013b5eebb0ecd18c589
martha <martha@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-07-27 21:18:16 +0000

    Calendario genérico

---
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/sem/genericCalendar/EventType.java
@@ -0,0 +1,13 @@
+package org.semanticwb.portal.resources.sem.genericCalendar;
+
+
+   /**
+   * Catálogo de Tipos de Eventos 
+   */
+public class EventType extends org.semanticwb.portal.resources.sem.genericCalendar.base.EventTypeBase 
+{
+    public EventType(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
