d4aa9dc13c9f35ecd691d2a41320c8a6a51370d0
jafernandez <jafdeza@users.noreply.github.com>
2011-08-31 16:27:47 +0000

    Se agregó selección en la administración para Grupo de usuarios.

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/ProcessFileRepository.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/ProcessFileRepository.java
@@ -656,7 +656,7 @@ public class ProcessFileRepository extends GenericResource {
         User user = paramRequest.getUser();
 
         WebPage wpage = paramRequest.getWebPage();
-        WebSite wsite = wpage.getWebSite();
+        WebSite wsite = getResourceBase().getWebSite(); //wpage.getWebSite();
 
         out.println("<div class=\"swbform\">");
 
@@ -735,7 +735,7 @@ public class ProcessFileRepository extends GenericResource {
             Iterator<UserGroup> iugroups = wsite.getUserRepository().listUserGroups();
             while (iugroups.hasNext()) {
                 UserGroup oUG = iugroups.next();
-                strRules.append("\n<option value=\"" + oUG.getURI() + "\">" + oUG.getDisplayTitle(user.getLanguage()) + "</option>");
+                strRules.append("\n<option value=\"" + oUG.getURI() + "\" " + (selectedItem.equals(oUG.getURI()) ? "selected" : "") + ">" + oUG.getDisplayTitle(user.getLanguage()) + "</option>");
             }
             strRules.append("\n</optgroup>");
             if (strRules.toString().length() > 0) {
