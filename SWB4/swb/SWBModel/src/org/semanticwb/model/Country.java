37f38705329c3024f5ab92a32c3bef7904781303
cramos <cramos@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-10-07 21:17:46 +0000

    Se sobreescribieron los métodos hashCode y equals

--- SWB4/swb/SWBModel/src/org/semanticwb/model/Country.java
+++ SWB4/swb/SWBModel/src/org/semanticwb/model/Country.java
@@ -36,5 +36,20 @@ public class Country extends org.semanticwb.model.base.CountryBase {
     public Country(org.semanticwb.platform.SemanticObject base) {
         super(base);
     }
+    
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if(obj==null)
+            return false;
+        if(obj instanceof Country) {
+            return hashCode()==obj.hashCode();
+        }
+        return false;
+    }
 }
 
