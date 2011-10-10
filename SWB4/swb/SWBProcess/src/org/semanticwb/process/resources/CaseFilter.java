6c92296245e61ccc38e7a73f44e9bebb45f8bd39
Hasdai Pacheco <haxdai@gmail.com>
2011-10-10 16:22:15 +0000

    Agregado encabezado de licencia

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/CaseFilter.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/CaseFilter.java
@@ -1,25 +1,29 @@
 /**
- * SemanticWebBuilder es una plataforma para el desarrollo de portales y aplicaciones de integraciÃ³n,
- * colaboraciÃ³n y conocimiento, que gracias al uso de tecnologÃ­a semÃ¡ntica puede generar contextos de
- * informaciÃ³n alrededor de algÃºn tema de interÃ©s o bien integrar informaciÃ³n y aplicaciones de diferentes
- * fuentes, donde a la informaciÃ³n se le asigna un significado, de forma que pueda ser interpretada y
- * procesada por personas y/o sistemas, es una creaciÃ³n original del Fondo de InformaciÃ³n y DocumentaciÃ³n
- * para la Industria INFOTEC, cuyo registro se encuentra actualmente en trÃ¡mite.
- *
- * INFOTEC pone a su disposiciÃ³n la herramienta SemanticWebBuilder a travÃ©s de su licenciamiento abierto al pÃºblico (â€˜open sourceâ€™),
- * en virtud del cual, usted podrÃ¡ usarlo en las mismas condiciones con que INFOTEC lo ha diseÃ±ado y puesto a su disposiciÃ³n;
- * aprender de Ã©l; distribuirlo a terceros; acceder a su cÃ³digo fuente y modificarlo, y combinarlo o enlazarlo con otro software,
- * todo ello de conformidad con los tÃ©rminos y condiciones de la LICENCIA ABIERTA AL PÃšBLICO que otorga INFOTEC para la utilizaciÃ³n
- * del SemanticWebBuilder 4.0.
- *
- * INFOTEC no otorga garantÃ­a sobre SemanticWebBuilder, de ninguna especie y naturaleza, ni implÃ­cita ni explÃ­cita,
- * siendo usted completamente responsable de la utilizaciÃ³n que le dÃ© y asumiendo la totalidad de los riesgos que puedan derivar
- * de la misma.
- *
- * Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposiciÃ³n la siguiente
- * direcciÃ³n electrÃ³nica:
- *  http://www.semanticwebbuilder.org
- **/
+* SemanticWebBuilder Process (SWBP) es una plataforma para la gestión de procesos de negocio mediante el uso de 
+* tecnología semántica, que permite el modelado, configuración, ejecución y monitoreo de los procesos de negocio
+* de una organización, así como el desarrollo de componentes y aplicaciones orientadas a la gestión de procesos.
+* 
+* Mediante el uso de tecnología semántica, SemanticWebBuilder Process puede generar contextos de información
+* alrededor de algún tema de interés o bien integrar información y aplicaciones de diferentes fuentes asociadas a
+* un proceso de negocio, donde a la información se le asigna un significado, de forma que pueda ser interpretada
+* y procesada por personas y/o sistemas. SemanticWebBuilder Process es una creación original del Fondo de 
+* Información y Documentación para la Industria INFOTEC.
+* 
+* INFOTEC pone a su disposición la herramienta SemanticWebBuilder Process a través de su licenciamiento abierto 
+* al público (‘open source’), en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC 
+* lo ha diseñado y puesto a su disposición; aprender de él; distribuirlo a terceros; acceder a su código fuente,
+* modificarlo y combinarlo (o enlazarlo) con otro software. Todo lo anterior de conformidad con los términos y 
+* condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización de SemanticWebBuilder Process. 
+* 
+* INFOTEC no otorga garantía sobre SemanticWebBuilder Process, de ninguna especie y naturaleza, ni implícita ni 
+* explícita, siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los 
+* riesgos que puedan derivar de la misma. 
+* 
+* Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder Process, INFOTEC pone a su disposición la
+* siguiente dirección electrónica: 
+*  http://www.semanticwebbuilder.org.mx
+**/
+
 package org.semanticwb.process.resources;
 
 import java.util.HashMap;
