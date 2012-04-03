df92b954384bfc149e40a792288c6a90126274dc
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-04-03 16:03:40 +0000

    SWBSocial

--- SWB4/swb/SWBSocial/src/org/semanticwb/social/networks/SWBSFlickerImp.java
+++ SWB4/swb/SWBSocial/src/org/semanticwb/social/networks/SWBSFlickerImp.java
@@ -7,6 +7,7 @@ package org.semanticwb.social.networks;
 
 import org.semanticwb.Logger;
 import org.semanticwb.SWBUtils;
+import org.semanticwb.model.SWBModel;
 import org.swb.social.lib.Exception.SWBSocialNetworkException;
 import org.swb.social.lib.SWBSocialNetWork;
 
@@ -18,18 +19,31 @@ public class SWBSFlickerImp extends SWBSocialNetWork
 {
     private static Logger log = SWBUtils.getLogger(SWBSFlickerImp.class);
 
-    public void connect() throws SWBSocialNetworkException
+    SWBModel model=null;
+
+    public SWBSFlickerImp(SWBModel model)
     {
-        System.out.println("Se conecta a la red social");
+        super(model);
+        this.model=model;
+        init(this.getClass().getName());
+    }
+
+
+    public boolean connect() throws SWBSocialNetworkException
+    {
+        return true;
     }
 
     public void sendPost() throws SWBSocialNetworkException
     {
-        System.out.println("Enviando Mensaje a publicar en SWBSFacebookImp:"+msg);
+        connect();
+        System.out.println("Enviando Mensaje a publicar en SWBSFlickerImp:"+msg);
+        disConnect();
     }
 
-    public void disConnect() throws SWBSocialNetworkException
+    public boolean disConnect() throws SWBSocialNetworkException
     {
          System.out.println("Se desconecta de la red social");
+         return true;
     }
 }
--- SWB4/swb/SWBSocial/src/swbsocial/test/testSWBSocialMgr.java
+++ SWB4/swb/SWBSocial/src/swbsocial/test/testSWBSocialMgr.java
@@ -6,10 +6,10 @@
 package swbsocial.test;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import org.swb.social.lib.Exception.SWBSocialException;
-import org.swb.social.lib.SWBSocialMgr;
-import org.swb.social.lib.SWBSocialNetWork;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.social.networks.SWBSFacebookImp;
+import org.semanticwb.social.networks.SWBSTwitterImp;
 
 /**
  *
@@ -17,51 +17,60 @@ import org.swb.social.lib.SWBSocialNetWork;
  */
 public class testSWBSocialMgr {
 
+    private static Logger log = SWBUtils.getLogger(testSWBSocialMgr.class);
+
     static String twitterFQN="org.semanticwb.social.networks.SWBSTwitterImp";
     static String facebookFQN="org.semanticwb.social.networks.SWBSFacebookImp";
 
     public static void main(String[] args)
     {
-
         ArrayList aClasses=new ArrayList();
         aClasses.add(twitterFQN);
         aClasses.add(facebookFQN);
 
+        String msg="Este es el mensaje de paz...";
+        // Para el uso con un wrapper
+        /*
         Iterator <String> itClasses=aClasses.iterator();
-
         while(itClasses.hasNext())
         {
-            String sClass=itClasses.next();
-            Object obj=null;
-            try{
-                Class classInstance=Class.forName(sClass);
-                obj=classInstance.newInstance();
-                send2Network(obj);
-            }catch(Exception e)
-            {
-                e.printStackTrace();
+           try{
+                SWBSocialMgr swbSocialNetWork=new SWBSocialMgr(itClasses.next());
+                swbSocialNetWork.connect();
+                swbSocialNetWork.setMsg(msg);
+                swbSocialNetWork.sendPost();
+                swbSocialNetWork.disConnect();
+            }catch (Exception e){
+                log.error(e);
             }
         }
-        
-    }
+        */
+        /////////.......................................................///////////////
 
-
-    private static void send2Network(Object obj)
-    {
-        try{
-            //Se configura una red social
-            SWBSocialNetWork swbSocialObj=(SWBSocialNetWork) obj;
-            swbSocialObj.connect();
-            swbSocialObj.setMsg("Hola Mundo este es un mensaje de pazz..");
-            //Se le pasa la red social al manager
-            SWBSocialMgr socialMgr=new SWBSocialMgr(swbSocialObj);
-            socialMgr.sendPost();
-            //Se desconecta la red social
-            swbSocialObj.disConnect();
-        }catch(SWBSocialException e)
+        //Para el uso de una determinada red social directamente
+        //WebSite wsite=WebSite.ClassMgr.getWebSite("visit");
+        SWBSTwitterImp swbTwitter=new SWBSTwitterImp(null);
+        try
         {
-            e.printStackTrace();
+            swbTwitter.setMsg(msg);
+            swbTwitter.sendPost();
+        }catch(Exception e)
+        {
+         log.error(e);
         }
+
+        SWBSFacebookImp swbFacebook=new SWBSFacebookImp(null);
+        try
+        {
+            swbFacebook.connect();
+            swbFacebook.setMsg(msg);
+            swbFacebook.sendPost();
+            swbFacebook.disConnect();
+        }catch(Exception e)
+        {
+         log.error(e);
+        }
+        
     }
 
 }
