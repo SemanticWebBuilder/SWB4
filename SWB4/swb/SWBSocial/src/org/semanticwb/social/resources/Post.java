2c55de7a7b92589382bd0b35ba4f6523ee929ff4
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-04-12 22:57:32 +0000

    SWBSocial

---
+++ SWB4/swb/SWBSocial/src/org/semanticwb/social/resources/Post.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.social.resources;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.model.WebSite;
+import org.semanticwb.platform.SemanticObject;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBActionResponse;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.social.Messageable;
+import org.semanticwb.social.SocialNetWork;
+import org.semanticwb.social.Videoable;
+import org.semanticwb.social.YouTube;
+
+/**
+ *
+ * @author jorge.jimenez
+ */
+public class Post extends GenericResource {
+
+    /** The log. */
+    private static Logger log = SWBUtils.getLogger(YouTubeResource.class);
+
+
+    /* (non-Javadoc)
+     * @see org.semanticwb.portal.api.GenericResource#doView(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, org.semanticwb.portal.api.SWBParamRequest)
+     */
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        RequestDispatcher dis = request.getRequestDispatcher("/swbadmin/jsp/social/post.jsp");
+        try {
+            request.setAttribute("paramRequest", paramRequest);
+            dis.include(request, response);
+        } catch (Exception e) {
+            log.error(e);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.semanticwb.portal.api.GenericResource#processAction(javax.servlet.http.HttpServletRequest, org.semanticwb.portal.api.SWBActionResponse)
+     */
+    @Override
+    public void processAction(HttpServletRequest request, SWBActionResponse response) throws SWBResourceException, IOException {
+        String socialUri=request.getParameter("socialUri");
+        String toPost=request.getParameter("toPost");
+        try{
+            String[] socialUris=socialUri.split("\\|");  //Dividir valores
+            for(int i=0;i<socialUris.length;i++)
+            {
+                String tmp_socialUri=socialUris[i];
+                SemanticObject semObject = SemanticObject.createSemanticObject(tmp_socialUri);
+                System.out.println(semObject instanceof Videoable);
+                SocialNetWork socialNet=(SocialNetWork)semObject.createGenericInstance();
+                if(toPost.equals("msg") && socialNet instanceof Messageable)
+                {
+                    //TODO: YO CREO QUE LO QUE TENGO QUE HACER AQUI, ES UN THREAD POR CADA UNA DE LAS REDES SOCIALES A LAS QUE SE ENVÍE UN POST
+                    Messageable messageable=(Messageable) socialNet;
+                    messageable.postMsg(null, request, response);
+                }else if(toPost.equals("video") && socialNet instanceof Videoable)
+                {
+                    //TODO: YO CREO QUE LO QUE TENGO QUE HACER AQUI, ES UN THREAD POR CADA UNA DE LAS REDES SOCIALES A LAS QUE SE ENVÍE UN POST
+                    Videoable videoable=(Videoable) socialNet;
+                    videoable.postVideo(null, request, response);
+                }
+            }
+
+        }catch(Exception e){
+            log.error(e);
+        }
+    }
+
+}
