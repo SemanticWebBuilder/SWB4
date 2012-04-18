3229cc78a4d059853ad96c8249d49237bbaa7574
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-04-18 22:26:02 +0000

    SWBSocial

--- SWB4/swbsocial/swbSocialResources/src/org/semanticwb/social/resources/Post.java
+++ SWB4/swbsocial/swbSocialResources/src/org/semanticwb/social/resources/Post.java
@@ -11,14 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.semanticwb.Logger;
 import org.semanticwb.SWBUtils;
+import org.semanticwb.model.WebSite;
 import org.semanticwb.platform.SemanticObject;
+import org.semanticwb.portal.SWBFormMgr;
 import org.semanticwb.portal.api.GenericResource;
 import org.semanticwb.portal.api.SWBActionResponse;
 import org.semanticwb.portal.api.SWBParamRequest;
 import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.social.Message;
 import org.semanticwb.social.Messageable;
+import org.semanticwb.social.Photo;
 import org.semanticwb.social.Photoable;
 import org.semanticwb.social.SocialNetwork;
+import org.semanticwb.social.Video;
 import org.semanticwb.social.Videoable;
 
 /**
@@ -52,32 +57,51 @@ public class Post extends GenericResource {
      */
     @Override
     public void processAction(HttpServletRequest request, SWBActionResponse response) throws SWBResourceException, IOException {
+        WebSite wsite=response.getWebPage().getWebSite();
         String socialUri=request.getParameter("socialUri");
         String toPost=request.getParameter("toPost");
         try{
             if(socialUri!=null)
             {
-                String[] socialUris=socialUri.split("\\|");  //Dividir valores
-                for(int i=0;i<socialUris.length;i++)
+                SWBFormMgr mgr=null;
+                if(toPost.equals("msg"))
                 {
-                    String tmp_socialUri=socialUris[i];
-                    SemanticObject semObject = SemanticObject.createSemanticObject(tmp_socialUri);
-                    SocialNetwork socialNet=(SocialNetwork)semObject.createGenericInstance();
-                    if(toPost.equals("msg") && socialNet instanceof Messageable)
+                    mgr = new SWBFormMgr(Message.sclass, wsite.getSemanticObject(), null);
+                }else if(toPost.equals("photo"))
+                {
+                    mgr = new SWBFormMgr(Photo.sclass, wsite.getSemanticObject(), null);
+                }else if(toPost.equals("video"))
+                {
+                    mgr = new SWBFormMgr(Video.sclass, wsite.getSemanticObject(), null);
+                }
+                if(mgr!=null)
+                {
+                    mgr.setFilterRequired(false);
+                    SemanticObject sobj = mgr.processForm(request);
+                    org.semanticwb.social.Post post=(org.semanticwb.social.Post)sobj.createGenericInstance();
+                    //Revisa las redes sociales a las cuales se tiene que enviar el Post
+                    String[] socialUris=socialUri.split("\\|");  //Dividir valores
+                    for(int i=0;i<socialUris.length;i++)
                     {
-                        //TODO: YO CREO QUE LO QUE TENGO QUE HACER AQUI, ES UN THREAD POR CADA UNA DE LAS REDES SOCIALES A LAS QUE SE ENVÍE UN POST
-                        Messageable messageable=(Messageable) socialNet;
-                        messageable.postMsg(null, request, response);
-                    } if(toPost.equals("photo") && socialNet instanceof Photoable)
-                    {
-                        //TODO: YO CREO QUE LO QUE TENGO QUE HACER AQUI, ES UN THREAD POR CADA UNA DE LAS REDES SOCIALES A LAS QUE SE ENVÍE UN POST
-                        Photoable photoable=(Photoable) socialNet;
-                        photoable.postPhoto(null, request, response);
-                    }else if(toPost.equals("video") && socialNet instanceof Videoable)
-                    {
-                        //TODO: YO CREO QUE LO QUE TENGO QUE HACER AQUI, ES UN THREAD POR CADA UNA DE LAS REDES SOCIALES A LAS QUE SE ENVÍE UN POST
-                        Videoable videoable=(Videoable) socialNet;
-                        videoable.postVideo(null, request, response);
+                        String tmp_socialUri=socialUris[i];
+                        SemanticObject semObject = SemanticObject.createSemanticObject(tmp_socialUri);
+                        SocialNetwork socialNet=(SocialNetwork)semObject.createGenericInstance();
+                        if(toPost.equals("msg") && socialNet instanceof Messageable)
+                        {
+                            //TODO: YO CREO QUE LO QUE TENGO QUE HACER AQUI, ES UN THREAD POR CADA UNA DE LAS REDES SOCIALES A LAS QUE SE ENVÍE UN POST
+                            Messageable messageable=(Messageable) socialNet;
+                            messageable.postMsg((Message)post, request, response);
+                        } if(toPost.equals("photo") && socialNet instanceof Photoable)
+                        {
+                            //TODO: YO CREO QUE LO QUE TENGO QUE HACER AQUI, ES UN THREAD POR CADA UNA DE LAS REDES SOCIALES A LAS QUE SE ENVÍE UN POST
+                            Photoable photoable=(Photoable) socialNet;
+                            photoable.postPhoto((Photo)post, request, response);
+                        }else if(toPost.equals("video") && socialNet instanceof Videoable)
+                        {
+                            //TODO: YO CREO QUE LO QUE TENGO QUE HACER AQUI, ES UN THREAD POR CADA UNA DE LAS REDES SOCIALES A LAS QUE SE ENVÍE UN POST
+                            Videoable videoable=(Videoable) socialNet;
+                            videoable.postVideo((Video)post, request, response);
+                        }
                     }
                 }
             }
