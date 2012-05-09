99a695c3356e30e021ef48e80c1b4a9d3080e9e9
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2012-05-09 21:03:41 +0000

    Alert Object

---
+++ SWB4/swb/SWBModel/src/org/semanticwb/model/AdminAlert.java
@@ -0,0 +1,13 @@
+package org.semanticwb.model;
+
+
+   /**
+   * AntiDDoS Parameters, this class holds the thresholds limits to recognize an attack 
+   */
+public class AdminAlert extends org.semanticwb.model.base.AdminAlertBase 
+{
+    public AdminAlert(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
