822f46439bab90f4be3fbca9507800c0b694843f
george24 <jajs1975@users.noreply.github.com>
2012-05-31 00:50:53 +0000

    SWBSocial

--- SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/SentimentWords.java
+++ SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/SentimentWords.java
@@ -1,5 +1,9 @@
 package org.semanticwb.social;
 
+import java.util.Iterator;
+import org.semanticwb.model.SWBModel;
+import org.semanticwb.platform.SemanticObject;
+
 
    /**
    * Conjunto de Palabras a buscar dentro de los Post que llegan por el Listener y con las cuales se podra determinar el sentimiento y la intensidad de los mismos(Post). 
@@ -9,5 +13,16 @@ public class SentimentWords extends org.semanticwb.social.base.SentimentWordsBas
     public SentimentWords(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
+    }
+
+    public static SentimentWords getSentimentalWordByWord(SWBModel model, String word)
+    {
+        Iterator<SemanticObject> it=model.getSemanticModel().listSubjects(social_sentimentalWord, word);
+        if(it.hasNext())
+        {
+            SemanticObject obj=it.next();
+            return (SentimentWords)obj.createGenericInstance();
+        }
+        return null;
     }
 }
