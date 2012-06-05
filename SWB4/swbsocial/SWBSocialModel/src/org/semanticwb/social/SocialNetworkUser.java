72b7418fae731862be7ff8c545b395330f805318
george24 <jajs1975@users.noreply.github.com>
2012-06-05 00:39:55 +0000

    SWBSocial

--- SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/SocialNetworkUser.java
+++ SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/SocialNetworkUser.java
@@ -1,5 +1,9 @@
 package org.semanticwb.social;
 
+import java.util.Iterator;
+import org.semanticwb.model.SWBModel;
+import org.semanticwb.platform.SemanticObject;
+
 
    /**
    * Clase en la cual se almacenan los usuarios que escriben los PostIn que llegan. El identificador de c/intancia de esta clase es el identificador de un usuarios en una red social. 
@@ -9,5 +13,20 @@ public class SocialNetworkUser extends org.semanticwb.social.base.SocialNetworkU
     public SocialNetworkUser(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
+    }
+
+    public static SocialNetworkUser getSocialNetworkUserbyIDAndSocialNet(String userId, SocialNetwork socialNetwork, SWBModel model)
+    {
+        Iterator<SemanticObject> it=model.getSemanticModel().listSubjects(social_snu_id, userId); //No encuentra
+        while(it.hasNext())
+        {
+            SemanticObject obj=it.next();
+            SocialNetworkUser socialNetUser=(SocialNetworkUser)obj.createGenericInstance();
+            if(socialNetUser.getSnu_SocialNetwork()!=null && socialNetUser.getSnu_SocialNetwork().getId().equals(socialNetwork.getId()));
+            {
+                  return socialNetUser;
+            }
+        }
+        return null;
     }
 }
--- SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/listener/twitter/SWBSocialStatusListener.java
+++ SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/listener/twitter/SWBSocialStatusListener.java
@@ -5,9 +5,11 @@
 
 package org.semanticwb.social.listener.twitter;
 
+import java.util.Date;
 import org.semanticwb.model.SWBModel;
 import org.semanticwb.social.MessageIn;
 import org.semanticwb.social.SocialNetwork;
+import org.semanticwb.social.SocialNetworkUser;
 import org.semanticwb.social.listener.Classifier;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
@@ -32,18 +34,41 @@ public class SWBSocialStatusListener implements twitter4j.StatusListener {
         //if (status.getGeoLocation() != null)
         //if(status.getText().indexOf("android")>-1 || status.getText().indexOf("ipad")>-1 || status.getText().indexOf("iphone")>-1 || status.getText().indexOf("tarea")>-1)
         {
-            /*
+            
             System.out.println();
             System.out.println("------------");
             System.out.println(status.getUser().getName() + " : " + status.getText() + " : " + status.getGeoLocation());
             System.out.println(status.getCreatedAt());
-             * 
-             */
-
+            
             //Persistencia del mensaje
             MessageIn mesagge=MessageIn.ClassMgr.createMessageIn(String.valueOf(status.getId()), model);
             mesagge.setMsg_Text(status.getText());
             mesagge.setPostInSocialNetwork(socialNetwork);
+            
+            if(status.getUser()!=null)
+            {
+                long userId=status.getUser().getId();
+                String name=status.getUser().getName();
+                Date userNetworkCreatedDate=status.getUser().getCreatedAt();
+                int followers=status.getUser().getFollowersCount();
+                int friends=status.getUser().getFriendsCount();
+                SocialNetworkUser socialNetUser=SocialNetworkUser.getSocialNetworkUserbyIDAndSocialNet(""+userId, socialNetwork, model);
+                if(socialNetUser==null)//
+                {
+                    //Si no existe el id del usuario para esa red social, lo crea.
+                    socialNetUser=SocialNetworkUser.ClassMgr.createSocialNetworkUser(model);
+                    socialNetUser.setSnu_id(""+userId);
+                    socialNetUser.setSnu_SocialNetwork(socialNetwork);
+                    System.out.println("SocialNetworkUser Creado:"+socialNetUser.getSnu_id()+", name:"+socialNetUser.getSnu_name());
+                }else{
+                    System.out.println("SocialNetworkUser Actualizado:"+socialNetUser.getSnu_id()+", name:"+socialNetUser.getSnu_name());
+                }
+                socialNetUser.setFollowers(followers);
+                socialNetUser.setFriends(friends);
+                //int listedCount=status.getUser().getListedCount();
+                System.out.println("userId:"+userId+", date:"+userNetworkCreatedDate+", followers:"+socialNetUser.getFollowers()+", friends:"+socialNetUser.getFriends());
+            }
+
             socialNetwork.addReceivedPost(mesagge, String.valueOf(status.getId()), socialNetwork);
             new Classifier(mesagge, model);
         }
