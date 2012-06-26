bda7f5ef585e7394eb4324c217b8d5d1190ea1e4
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2012-06-26 22:55:19 +0000

    BUG: Bad redirecting on logout, bad redirecting on context

--- SWB4/swb/SWBPortal/src/org/semanticwb/servlet/internal/Login.java
+++ SWB4/swb/SWBPortal/src/org/semanticwb/servlet/internal/Login.java
@@ -233,7 +233,7 @@ public class Login implements InternalServlet
                 if ((url == null))
                 {
                    // url = path + "/" + SWBPlatform.getEnv("swb/distributor") + "/" + dparams.getWebPage().getWebSiteId() + "/" + dparams.getWebPage().getId() + "/_lang/" + dparams.getUser().getLanguage();
-                     url = path + "/" + SWBPlatform.getEnv("swb/distributor") + dparams.getWebPage().getUrl(dparams.getUser().getLanguage());
+                     url = dparams.getWebPage().getUrl(dparams.getUser().getLanguage());
                 }
                 log.debug("LOGOUT (Path, uri, url): " + path + "   |   " + uri + "    |  " + url);
                 sendRedirect(response, url);
@@ -359,7 +359,7 @@ public class Login implements InternalServlet
         if ((url == null || url.equals("/")))
         {
             log.debug("PATHs: Path:" + path + " - " + dparams.getWebPage().getWebSiteId() + " - " + dparams.getWebPage().getId());
-            url =   SWBPlatform.getContextPath() + dparams.getWebPage().getUrl(dparams.getUser().getLanguage());
+            url =   /*SWBPlatform.getContextPath() + */ dparams.getWebPage().getUrl(dparams.getUser().getLanguage());
                     //SWBPlatform.getContextPath() + "/" + SWBPlatform.getEnv("swb/distributor") + "/" + dparams.getWebPage().getWebSiteId() + "/" + dparams.getWebPage().getId() + "/_lang/" + dparams.getUser().getLanguage();
         } 
         if (null!=request.getQueryString() && request.getQueryString().indexOf("wb_logout")==-1) url = url+"?"+request.getQueryString();
