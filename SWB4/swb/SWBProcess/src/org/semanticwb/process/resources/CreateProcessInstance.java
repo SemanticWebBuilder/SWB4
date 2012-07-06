30d63263d90634d5338bcac1c4d0c59f3417336e
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-07-06 19:13:16 +0000

    Modificado para permitir configuración de vista tipo menú.

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/CreateProcessInstance.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/CreateProcessInstance.java
@@ -28,7 +28,10 @@ package org.semanticwb.process.resources;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.semanticwb.model.*;
@@ -40,45 +43,137 @@ import org.semanticwb.process.model.*;
  *
  * @author juan.fernandez
  */
-public class CreateProcessInstance extends GenericResource {
+public class CreateProcessInstance extends GenericAdmResource{ 
 
+    public void doShowMenu(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        User user = paramRequest.getUser();
+        PrintWriter out = response.getWriter();
+        Map<String, ArrayList<Process>> groups = new TreeMap<String, ArrayList<Process>>();
+        ArrayList<Process> processes = new ArrayList<Process>();
+        boolean showGroups = false;
+        
+        String lang = "es";
+        if (paramRequest.getUser() != null && paramRequest.getUser().getLanguage()!=null) lang = paramRequest.getUser().getLanguage();
+        
+        if (!getResourceBase().getAttribute("showGroups", "").equals("")) {
+            showGroups = true;
+        }
+        
+        ArrayList<Process> pccs = null;
+        //Obtener los eventos de inicio
+        Iterator<StartEvent> startEvents = StartEvent.ClassMgr.listStartEvents(paramRequest.getWebPage().getWebSite());            
+        while(startEvents.hasNext()) {
+            StartEvent sevt = startEvents.next();
+            //Si el usuario tiene permisos en el evento
+            if (user.haveAccess(sevt)) {
+                Process itp = sevt.getProcess();
+                //Si el proceso al que pertenece el evento y es válido
+                if (itp != null && itp.isValid()) {
+                    if (showGroups) {
+                        if(itp.getProcessGroup() != null) {
+                            String pg = itp.getProcessGroup().getDisplayTitle(lang);
+
+                            //Si ya existe el grupo de procesos en el treemap
+                            if(groups.get(pg) != null) {
+                                pccs = groups.get(pg);
+                                if (!pccs.contains(itp)) {
+                                    pccs.add(itp);
+                                }
+                                groups.put(pg, pccs);
+                            } else { //Si no existe el grupo de procesos en el treemap
+                                pccs = new ArrayList<Process>();
+                                pccs.add(itp);
+                                groups.put(pg, pccs);
+                            }
+                        } 
+                    } else {
+                        if (!processes.contains(itp)) {
+                            processes.add(itp);
+                        }
+                    }
+                }
+            }
+        }
+
+        SWBResourceURL createUrl = paramRequest.getActionUrl().setAction("CREATE");
+        
+        if (showGroups) {
+            Iterator<String> keys = groups.keySet().iterator();
+            if (keys.hasNext()) {
+                out.println("<ul id=\"processMenu\">");
+                while(keys.hasNext()) {                    
+                    String key = keys.next();
+                    out.println("  <li id=\"processGroup\">"+key);
+                    out.println("    <ul>");
+                    Iterator<Process> it_pccs = SWBComparator.sortByDisplayName(groups.get(key).iterator(), lang);
+                    while(it_pccs.hasNext()) {
+                        Process pcc = it_pccs.next();
+                        createUrl.setParameter("prid", pcc.getId());
+                        out.println(      "<li><a href=\""+createUrl+"\">"+pcc.getTitle()+"</a></li>");
+                    }
+                    out.println("    </ul>");
+                    out.println("  </li>");                    
+                }
+                out.println("</ul>");
+            }
+        } else {
+            if (!processes.isEmpty()) {
+                out.println("  <li id=\"processMenu\">");
+                out.println("    <ul>");
+                Iterator<Process> it_pccs = SWBComparator.sortByDisplayName(processes.iterator(), lang);
+                while(it_pccs.hasNext()) {
+                    Process pcc = it_pccs.next();
+                    createUrl.setParameter("prid", pcc.getId());
+                    out.println(      "<li><a href=\""+createUrl+"\">"+pcc.getTitle()+"</a></li>");
+                }
+                out.println("    </ul>");
+                out.println("  </li>");
+            }
+        }
+    }
+    
     @Override
     public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
     {
         PrintWriter out = response.getWriter();
         SWBResourceURL url =  paramRequest.getActionUrl();
         Process process = null;
-        String label = paramRequest.getLocaleString("lblCreateInstance");
-        String pid = "";
+        String label = getResourceBase().getAttribute("linkLabel", "");
+        String pid = getResourceBase().getAttribute("processId", "");
         User user = paramRequest.getUser();
         
-        if (paramRequest.getArguments().containsKey("label")) {
-            label = (String)paramRequest.getArguments().get("label");
-        }
-        if (paramRequest.getArguments().containsKey("process")) {
-            pid = (String)paramRequest.getArguments().get("process");
-        }
-        
-        if (paramRequest.getWebPage() instanceof WrapperProcessWebPage) {
-            process = ((WrapperProcessWebPage)paramRequest.getWebPage()).getProcess();
-        } else if (!pid.trim().equals("")) {
-            process = Process.ClassMgr.getProcess(pid, paramRequest.getWebPage().getWebSite());
-        }
-        
-        if (process != null) {
-            boolean canStart = false;
-            Iterator<StartEvent> startEvents = StartEvent.ClassMgr.listStartEventByContainer(process);
-            while(startEvents.hasNext()) {
-                StartEvent sevt = startEvents.next();
-                if (user.haveAccess(sevt) && process.isValid()) {
-                    canStart = true;
-                }
+        if (!getResourceBase().getAttribute("asMenu", "").equals("")) {
+            doShowMenu(request, response, paramRequest);
+        } else {
+            if (label.equals("") && paramRequest.getArguments().containsKey("label")) {
+                label = (String)paramRequest.getArguments().get("label");
             }
-            
-            if (canStart) {
-                url.setParameter("prid", process.getId());
-                url.setAction("CREATE");
-                out.println("<a href=\""+url.toString()+"\">"+label+"</a>");
+
+            if (pid.equals("") && paramRequest.getArguments().containsKey("process")) {
+                pid = (String)paramRequest.getArguments().get("process");
+            }
+
+            if (paramRequest.getWebPage() instanceof WrapperProcessWebPage) {
+                process = ((WrapperProcessWebPage)paramRequest.getWebPage()).getProcess();
+            } else if (!pid.trim().equals("")) {
+                process = Process.ClassMgr.getProcess(pid, paramRequest.getWebPage().getWebSite());
+            }
+
+            if (process != null) {
+                boolean canStart = false;
+                Iterator<StartEvent> startEvents = StartEvent.ClassMgr.listStartEventByContainer(process);
+                while(startEvents.hasNext()) {
+                    StartEvent sevt = startEvents.next();
+                    if (user.haveAccess(sevt) && process.isValid()) {
+                        canStart = true;
+                    }
+                }
+
+                if (canStart) {
+                    url.setParameter("prid", process.getId());
+                    url.setAction("CREATE");
+                    out.println("<a href=\""+url.toString()+"\">"+label+"</a>");
+                }
             }
         }
     }
