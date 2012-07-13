282073982b391c859bd03d016149d2ae0f93384e
Hasdai Pacheco <haxdai@gmail.com>
2012-07-13 23:49:04 +0000

    Agregada configuración para modos de despliegue.

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/tracer/base/ProcessTracerBase.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/tracer/base/ProcessTracerBase.java
@@ -3,11 +3,16 @@ package org.semanticwb.process.resources.tracer.base;
 
 public abstract class ProcessTracerBase extends org.semanticwb.portal.api.GenericSemResource 
 {
+    public static final org.semanticwb.platform.SemanticProperty ptrace_filterByCreator=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://org.semanticwb.process.resources/ProcessTracer#filterByCreator");
     public static final org.semanticwb.platform.SemanticClass swb_WebPage=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/ontology#WebPage");
     public static final org.semanticwb.platform.SemanticProperty ptrace_displayMapPage=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://org.semanticwb.process.resources/ProcessTracer#displayMapPage");
+    public static final org.semanticwb.platform.SemanticProperty ptrace_itemsPerPage=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://org.semanticwb.process.resources/ProcessTracer#itemsPerPage");
+    public static final org.semanticwb.platform.SemanticProperty ptrace_processID=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://org.semanticwb.process.resources/ProcessTracer#processID");
     public static final org.semanticwb.platform.SemanticProperty ptrace_viewJSP=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://org.semanticwb.process.resources/ProcessTracer#viewJSP");
     public static final org.semanticwb.platform.SemanticClass swb_Resource=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/ontology#Resource");
     public static final org.semanticwb.platform.SemanticProperty swb_semanticResourceInv=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.semanticwebbuilder.org/swb4/ontology#semanticResourceInv");
+    public static final org.semanticwb.platform.SemanticProperty ptrace_userID=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://org.semanticwb.process.resources/ProcessTracer#userID");
+    public static final org.semanticwb.platform.SemanticProperty ptrace_viewMode=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://org.semanticwb.process.resources/ProcessTracer#viewMode");
     public static final org.semanticwb.platform.SemanticClass ptrace_ProcessTracer=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://org.semanticwb.process.resources/ProcessTracer#ProcessTracer");
     public static final org.semanticwb.platform.SemanticClass sclass=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://org.semanticwb.process.resources/ProcessTracer#ProcessTracer");
 
@@ -41,6 +46,24 @@ public abstract class ProcessTracerBase extends org.semanticwb.portal.api.Generi
     {
         if(obj==null)return false;
         return hashCode()==obj.hashCode();
+    }
+
+/**
+* Gets the FilterByCreator property
+* @return boolean with the FilterByCreator
+*/
+    public boolean isFilterByCreator()
+    {
+        return getSemanticObject().getBooleanProperty(ptrace_filterByCreator);
+    }
+
+/**
+* Sets the FilterByCreator property
+* @param value long with the FilterByCreator
+*/
+    public void setFilterByCreator(boolean value)
+    {
+        getSemanticObject().setBooleanProperty(ptrace_filterByCreator, value);
     }
    /**
    * Sets the value for the property DisplayMapPage
@@ -79,6 +102,42 @@ public abstract class ProcessTracerBase extends org.semanticwb.portal.api.Generi
              ret=(org.semanticwb.model.WebPage)obj.createGenericInstance();
          }
          return ret;
+    }
+
+/**
+* Gets the ItemsPerPage property
+* @return int with the ItemsPerPage
+*/
+    public int getItemsPerPage()
+    {
+        return getSemanticObject().getIntProperty(ptrace_itemsPerPage);
+    }
+
+/**
+* Sets the ItemsPerPage property
+* @param value long with the ItemsPerPage
+*/
+    public void setItemsPerPage(int value)
+    {
+        getSemanticObject().setIntProperty(ptrace_itemsPerPage, value);
+    }
+
+/**
+* Gets the ProcessID property
+* @return String with the ProcessID
+*/
+    public String getProcessID()
+    {
+        return getSemanticObject().getProperty(ptrace_processID);
+    }
+
+/**
+* Sets the ProcessID property
+* @param value long with the ProcessID
+*/
+    public void setProcessID(String value)
+    {
+        getSemanticObject().setProperty(ptrace_processID, value);
     }
 
 /**
@@ -135,5 +194,41 @@ public abstract class ProcessTracerBase extends org.semanticwb.portal.api.Generi
              ret=(org.semanticwb.model.Resource)obj.createGenericInstance();
          }
          return ret;
+    }
+
+/**
+* Gets the UserID property
+* @return String with the UserID
+*/
+    public String getUserID()
+    {
+        return getSemanticObject().getProperty(ptrace_userID);
+    }
+
+/**
+* Sets the UserID property
+* @param value long with the UserID
+*/
+    public void setUserID(String value)
+    {
+        getSemanticObject().setProperty(ptrace_userID, value);
+    }
+
+/**
+* Gets the ViewMode property
+* @return int with the ViewMode
+*/
+    public int getViewMode()
+    {
+        return getSemanticObject().getIntProperty(ptrace_viewMode);
+    }
+
+/**
+* Sets the ViewMode property
+* @param value long with the ViewMode
+*/
+    public void setViewMode(int value)
+    {
+        getSemanticObject().setIntProperty(ptrace_viewMode, value);
     }
 }
