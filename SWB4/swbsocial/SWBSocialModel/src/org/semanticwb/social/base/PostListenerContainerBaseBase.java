63329588d4bcc8cc5762925b255bf9eb03eb4ca6
george24 <jajs1975@users.noreply.github.com>
2012-08-08 00:35:59 +0000

    SWBSocial

--- SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/base/PostListenerContainerBaseBase.java
+++ SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/base/PostListenerContainerBaseBase.java
@@ -263,24 +263,6 @@ public abstract class PostListenerContainerBaseBase extends org.semanticwb.model
     }
 
 /**
-* Gets the Created property
-* @return java.util.Date with the Created
-*/
-    public java.util.Date getCreated()
-    {
-        return getSemanticObject().getDateProperty(swb_created);
-    }
-
-/**
-* Sets the Created property
-* @param value long with the Created
-*/
-    public void setCreated(java.util.Date value)
-    {
-        getSemanticObject().setDateProperty(swb_created, value);
-    }
-
-/**
 * Gets the Updated property
 * @return java.util.Date with the Updated
 */
@@ -296,6 +278,24 @@ public abstract class PostListenerContainerBaseBase extends org.semanticwb.model
     public void setUpdated(java.util.Date value)
     {
         getSemanticObject().setDateProperty(swb_updated, value);
+    }
+
+/**
+* Gets the Created property
+* @return java.util.Date with the Created
+*/
+    public java.util.Date getCreated()
+    {
+        return getSemanticObject().getDateProperty(swb_created);
+    }
+
+/**
+* Sets the Created property
+* @param value long with the Created
+*/
+    public void setCreated(java.util.Date value)
+    {
+        getSemanticObject().setDateProperty(swb_created, value);
     }
    /**
    * Sets the value for the property Plcb_Post
