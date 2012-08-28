6d2fbc787c155f641d15734d9cb42a2fa4d48906
serch <serch@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-08-28 21:56:53 +0000

    Signature with low policy

--- SWB4/swb/applets/signatureApplet/src/signatureapplet/SignatureApplet.java
+++ SWB4/swb/applets/signatureApplet/src/signatureapplet/SignatureApplet.java
@@ -1,10 +1,5 @@
 package signatureapplet;
 
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import javax.swing.JApplet;
 import javax.swing.SwingUtilities;
 
@@ -14,7 +9,7 @@ import javax.swing.SwingUtilities;
  */
 public class SignatureApplet extends JApplet {
 
-    private String urlGetSignatureString = "http://localhost:8888/demo.jsp";
+   // private String urlGetSignatureString = "http://localhost:8888/demo.jsp";
     private String urlPostSignature = "http://localhost:8888/post.jsp";
     private String sessionid;
     private String message = "||TEST|RECORD|23232443434343|TOTAL|Acepted||";
@@ -30,7 +25,8 @@ public class SignatureApplet extends JApplet {
         //urlGetSignatureString=getParameter("urlMessage");
         //urlPostSignature = getParameter("urlPost");
         message = getParameter("message");
-        /*try {
+        if (null==message) message="sin cadena";
+          /*try {
             URL urlget = new URL(urlGetSignatureString);
             URLConnection ucon = urlget.openConnection();
             ucon.addRequestProperty("JSESSIONID", sessionid);
@@ -60,6 +56,7 @@ public class SignatureApplet extends JApplet {
             });
         } catch (Exception e) {
             System.err.println("createGUI didn't complete successfully");
+            e.printStackTrace(System.err);
         }
     }
     // TODO overwrite start(), stop() and destroy() methods
