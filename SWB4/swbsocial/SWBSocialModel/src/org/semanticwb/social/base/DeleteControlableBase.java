d3b7ae65ef521459c312f33cfa83afcb742e14ff
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-10-19 21:21:14 +0000

    SWBSocial

---
+++ SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/base/DeleteControlableBase.java
@@ -0,0 +1,12 @@
+package org.semanticwb.social.base;
+
+   /**
+   * Interface que define que las instancias de una clase pueden ser eliminadas 
+   */
+public interface DeleteControlableBase extends org.semanticwb.model.GenericObject
+{
+   /**
+   * Interface que define que las instancias de una clase pueden ser eliminadas 
+   */
+    public static final org.semanticwb.platform.SemanticClass social_DeleteControlable=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/social#DeleteControlable");
+}
