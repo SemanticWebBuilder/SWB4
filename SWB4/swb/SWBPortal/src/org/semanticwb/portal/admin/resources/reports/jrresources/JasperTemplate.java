a0f2ba3136d059c270722cbb091e9a98636b7ee4
cramos <cramos@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-12-04 17:52:43 +0000

    Plantilla para reportes excel

--- SWB4/swb/SWBPortal/src/org/semanticwb/portal/admin/resources/reports/jrresources/JasperTemplate.java
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/admin/resources/reports/jrresources/JasperTemplate.java
@@ -1,25 +1,27 @@
-/*
- * SemanticWebBuilder es una plataforma para el desarrollo de portales y aplicaciones de integración,
- * colaboración y conocimiento, que gracias al uso de tecnología semántica puede generar contextos de
- * información alrededor de algún tema de interés o bien integrar información y aplicaciones de diferentes
- * fuentes, donde a la información se le asigna un significado, de forma que pueda ser interpretada y
- * procesada por personas y/o sistemas, es una creación original del Fondo de Información y Documentación
- * para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite.
- *
- * INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público (‘open source’),
- * en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC lo ha diseñado y puesto a su disposición;
- * aprender de él; distribuirlo a terceros; acceder a su código fuente y modificarlo, y combinarlo o enlazarlo con otro software,
- * todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización
- * del SemanticWebBuilder 4.0.
- *
- * INFOTEC no otorga garantía sobre SemanticWebBuilder, de ninguna especie y naturaleza, ni implícita ni explícita,
- * siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los riesgos que puedan derivar
- * de la misma.
- *
- * Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposición la siguiente
- * dirección electrónica:
- *  http://www.semanticwebbuilder.org
- */
+/**  
+* SemanticWebBuilder es una plataforma para el desarrollo de portales y aplicaciones de integración, 
+* colaboración y conocimiento, que gracias al uso de tecnología semántica puede generar contextos de 
+* información alrededor de algún tema de interés o bien integrar información y aplicaciones de diferentes 
+* fuentes, donde a la información se le asigna un significado, de forma que pueda ser interpretada y 
+* procesada por personas y/o sistemas, es una creación original del Fondo de Información y Documentación 
+* para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite. 
+* 
+* INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público (‘open source’), 
+* en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC lo ha diseñado y puesto a su disposición; 
+* aprender de él; distribuirlo a terceros; acceder a su código fuente y modificarlo, y combinarlo o enlazarlo con otro software, 
+* todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización 
+* del SemanticWebBuilder 4.0. 
+* 
+* INFOTEC no otorga garantía sobre SemanticWebBuilder, de ninguna especie y naturaleza, ni implícita ni explícita, 
+* siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los riesgos que puedan derivar 
+* de la misma. 
+* 
+* Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposición la siguiente 
+* dirección electrónica: 
+*  http://www.semanticwebbuilder.org
+**/ 
+ 
+
 package org.semanticwb.portal.admin.resources.reports.jrresources;
 
 // TODO: Auto-generated Javadoc
@@ -33,100 +35,123 @@ public enum JasperTemplate {
     
     /** The RE s_ appeare d_ html. */
     RES_APPEARED_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/AppearedResHtmlRep.jasper"),
-    
+
+    //###My code
     /** The GLOBA l_ daily. */
     GLOBAL_DAILY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/GlobalDailyRep.jasper"),
+
+    GLOBAL_DAILY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/GlobalDailyRepExcel.jasper"),
     
     /** The GLOBA l_ dail y_ graph. */
     GLOBAL_DAILY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/GlobalDailyGraphRep.jasper"),
     
     /** The GLOBA l_ dail y_ html. */
     GLOBAL_DAILY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/GlobalDailyHtmlRep.jasper"),
-    
+
+    //###My Code
     /** The GLOBA l_ monthly. */
     GLOBAL_MONTHLY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/GlobalRep.jasper"),
-    
+
+    GLOBAL_MONTHLY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/GlobalRepExcel.jasper"),
+
     /** The GLOBA l_ monthl y_ graph. */
     GLOBAL_MONTHLY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/GlobalGraphRep.jasper"),
     
     /** The GLOBA l_ monthl y_ html. */
     GLOBAL_MONTHLY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/GlobalHtmlRep.jasper"),
-    
+
+    //###My Code
     /** The DEVIC e_ daily. */
     DEVICE_DAILY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/DevicesDailyRep.jasper"),
-    
-    /** The DEVIC e_ dail y_ graph. */
+    DEVICE_DAILY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/DevicesDailyRepExcel.jasper"),
     DEVICE_DAILY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/DevicesDailyGraphRep.jasper"),
     
     /** The DEVIC e_ dail y_ html. */
     DEVICE_DAILY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/DevicesDailyHtmlRep.jasper"),
-    
-    /** The DEVIC e_ monthly. */
     DEVICE_MONTHLY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/DevicesRep.jasper"),
-    
-    /** The DEVIC e_ monthl y_ graph. */
+    DEVICE_MONTHLY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/DevicesRepExcel.jasper"),
     DEVICE_MONTHLY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/DevicesGraphRep.jasper"),
     
     /** The DEVIC e_ monthl y_ html. */
     DEVICE_MONTHLY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/DevicesHtmlRep.jasper"),
-    
+
+    //My Code
     /** The LANGUAG e_ daily. */
     LANGUAGE_DAILY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/LanguageDailyRep.jasper"),
-    
+
+    LANGUAGE_DAILY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/LanguageDailyRepExcel.jasper"),
+
     /** The LANGUAG e_ dail y_ graph. */
     LANGUAGE_DAILY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/LanguageDailyGraphRep.jasper"),
     
     /** The LANGUAG e_ dail y_ html. */
     LANGUAGE_DAILY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/LanguageDailyHtmlRep.jasper"),
-    
+
+    //My Code
     /** The LANGUAG e_ monthly. */
     LANGUAGE_MONTHLY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/LanguagesRep.jasper"),
+
+    LANGUAGE_MONTHLY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/LanguagesRepExcel.jasper"),
     
     /** The LANGUAG e_ monthl y_ graph. */
     LANGUAGE_MONTHLY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/LanguagesGraphRep.jasper"),
     
     /** The LANGUAG e_ monthl y_ html. */
     LANGUAGE_MONTHLY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/LanguagesHtmlRep.jasper"),
-    
+
+    //###My Code
     /** The LOGGE d_ uniqu e_ daily. */
     LOGGED_UNIQUE_DAILY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UniqueUsersDailyRep.jasper"),
+
+    LOGGED_UNIQUE_DAILY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UniqueUsersDailyRepExcel.jasper"),
     
     /** The LOGGE d_ uniqu e_ dail y_ graph. */
     LOGGED_UNIQUE_DAILY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UniqueUsersDailyGraphRep.jasper"),
     
     /** The LOGGE d_ uniqu e_ dail y_ html. */
     LOGGED_UNIQUE_DAILY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UniqueUsersDailyHtmlRep.jasper"),
-    
+
+    //## My Code
     /** The LOGGE d_ uniqu e_ monthly. */
     LOGGED_UNIQUE_MONTHLY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UniqueUsersRep.jasper"),
-    
+
+    LOGGED_UNIQUE_MONTHLY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UniqueUsersRepExcel.jasper"),
+
     /** The LOGGE d_ uniqu e_ monthl y_ graph. */
     LOGGED_UNIQUE_MONTHLY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UniqueUsersGraphRep.jasper"),
     
     /** The LOGGE d_ uniqu e_ monthl y_ html. */
     LOGGED_UNIQUE_MONTHLY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UniqueUsersHtmlRep.jasper"),
-    
+
+    //##My code
     /** The USE r_ logge d_ daily. */
     USER_LOGGED_DAILY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UsersDailyRep.jasper"),
-    
+
+    USER_LOGGED_DAILY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UsersDailyRepExcel.jasper"),
+
     /** The USE r_ logge d_ dail y_ graph. */
     USER_LOGGED_DAILY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UsersDailyGraphRep.jasper"),
     
     /** The USE r_ logge d_ dail y_ html. */
     USER_LOGGED_DAILY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UsersDailyHtmlRep.jasper"),
     
+    //##My code
     /** The USE r_ logge d_ monthly. */
     USER_LOGGED_MONTHLY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UsersRep.jasper"),
+
+    USER_LOGGED_MONTHLY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UsersRepExcel.jasper"),
     
     /** The USE r_ logge d_ monthl y_ graph. */
     USER_LOGGED_MONTHLY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UsersGraphRep.jasper"),
     
     /** The USE r_ logge d_ monthl y_ html. */
     USER_LOGGED_MONTHLY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/UsersHtmlRep.jasper"),
-    
+
+    //##My code
     /** The SECTIO n_ daily. */
     SECTION_DAILY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SectionsDailyRep.jasper"),
-    
+
+    SECTION_DAILY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SectionsDailyRepExcel.jasper"),
     /** The SECTIO n_ dail y_ graph. */
     SECTION_DAILY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SectionsDailyGraphRep.jasper"),
     
@@ -161,16 +186,22 @@ public enum JasperTemplate {
     RESOURCE_MONTHLY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/ResourcesHtmlRep.jasper"),    
     
     /** The SESSIO n_ daily. */
+    //###My code
+    //SESSION_DAILY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsDailyRepMod.jasper"),
     SESSION_DAILY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsDailyRep.jasper"),
-    
+
+    SESSION_DAILY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsDailyRepExcel.jasper"),
     /** The SESSIO n_ dail y_ graph. */
     SESSION_DAILY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsDailyGraphRep.jasper"),
     
     /** The SESSIO n_ dail y_ html. */
+    //### My code
+    //SESSION_DAILY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsDailyHtmlRepMod.jasper"),
     SESSION_DAILY_HTML("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsDailyHtmlRep.jasper"),
-    
     /** The SESSIO n_ monthly. */
     SESSION_MONTHLY("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsRep.jasper"),
+
+    SESSION_MONTHLY_EXCEL("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsRepExcel.jasper"),
     
     /** The SESSIO n_ monthl y_ graph. */
     SESSION_MONTHLY_GRAPH("/org/semanticwb/portal/admin/resources/reports/jrresources/templates/SessionsGraphRep.jasper"),
