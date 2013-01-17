dcf8e115c78a752e195c3ffbc8a2a162e734b64c
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-01-17 23:03:14 +0000

    Adición de la funcionalidad para obtener información de Facebook

---
+++ SWB4/swbsocial/SWBSocialModel/src/org/semanticwb/social/ExternalPost.java
@@ -0,0 +1,265 @@
+package org.semanticwb.social;
+
+/**
+ * Represents any message, image or video posted in any of the social networks
+ * at which this application searches for information
+ * @author jose.jimenez
+ */
+public class ExternalPost {
+    
+    
+    /**
+     * The identification string or number for the message
+     */
+    private String postId;
+    
+    /**
+     * The message's creator's name
+     */
+    private String creatorName;
+    
+    /**
+     * The message's creator's identification number or string
+     */
+    private String creatorId;
+    
+    /**
+     * The message posted
+     */
+    private String message;
+    
+    /**
+     * The link or URL for a picture related to the message
+     */
+    private String picture;
+    
+    /**
+     * A link related to the message
+     */
+    private String link;
+    
+    /**
+     * The name with which this message was created
+     */
+    private String postName;
+    
+    /**
+     * The text that describes the link related to this message
+     */
+    private String linkDescription;
+    
+    /**
+     * A link to an icon representing the type of this post (in Facebook)
+     */
+    private String icon;
+    
+    /**
+     * The time the post was initially published
+     */
+    private String creationTime;
+    
+    /**
+     * The time of the last comment or update to this post
+     */
+    private String updateTime;
+    
+    /**
+     * Creator's friends number
+     */
+    private String friendsNumber;
+    
+    /**
+     * Creator's followers number
+     */
+    private String followers;
+    
+
+    /**
+     * Creates a new instance of ExternalPost
+     */
+    public ExternalPost() {
+    }
+    
+    /**
+     * @return the postId
+     */
+    public String getPostId() {
+        return postId;
+    }
+
+    /**
+     * @param postId the postId to set
+     */
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    /**
+     * @return the creatorName
+     */
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    /**
+     * @param creatorName the creatorName to set
+     */
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+
+    /**
+     * @return the creatorId
+     */
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    /**
+     * @param creatorId the creatorId to set
+     */
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the picture
+     */
+    public String getPicture() {
+        return picture;
+    }
+
+    /**
+     * @param picture the picture to set
+     */
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    /**
+     * @return the link
+     */
+    public String getLink() {
+        return link;
+    }
+
+    /**
+     * @param link the link to set
+     */
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    /**
+     * @return the postName
+     */
+    public String getPostName() {
+        return postName;
+    }
+
+    /**
+     * @param postName the postName to set
+     */
+    public void setPostName(String postName) {
+        this.postName = postName;
+    }
+
+    /**
+     * @return the linkDescription
+     */
+    public String getLinkDescription() {
+        return linkDescription;
+    }
+
+    /**
+     * @param linkDescription the linkDescription to set
+     */
+    public void setLinkDescription(String linkDescription) {
+        this.linkDescription = linkDescription;
+    }
+
+    /**
+     * @return the icon
+     */
+    public String getIcon() {
+        return icon;
+    }
+
+    /**
+     * @param icon the icon to set
+     */
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    /**
+     * @return the creationTime
+     */
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    /**
+     * @param creationTime the creationTime to set
+     */
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    /**
+     * @return the updateTime
+     */
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * @param updateTime the updateTime to set
+     */
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    /**
+     * @return the friendsNumber
+     */
+    public String getFriendsNumber() {
+        return friendsNumber;
+    }
+
+    /**
+     * @param friendsNumber the friendsNumber to set
+     */
+    public void setFriendsNumber(String friendsNumber) {
+        this.friendsNumber = friendsNumber;
+    }
+
+    /**
+     * @return the followers
+     */
+    public String getFollowers() {
+        return followers;
+    }
+
+    /**
+     * @param followers the followers to set
+     */
+    public void setFollowers(String followers) {
+        this.followers = followers;
+    }
+    
+}
