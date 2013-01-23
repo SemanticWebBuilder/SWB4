854a7240a14ce96efca9ea9eabc114da6e782ad5
martha <martha@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-01-23 23:07:57 +0000

    StreamsResource

--- SWB4/swbsocial/swbSocialResources/src/org/semanticwb/social/resources/FlickerAuthenticator.java
+++ SWB4/swbsocial/swbSocialResources/src/org/semanticwb/social/resources/FlickerAuthenticator.java
@@ -24,12 +24,27 @@ import org.semanticwb.social.SWBFlickrOauth;
 /**
  *
  * @author martha.jimenez
+ * Esta clase se encarga de obtener el permiso de acceso a una cuenta en Flickr para
+ * posteriormente almacenar dichas autorizaciones en objetos de tipo Flicker
  */
 public class FlickerAuthenticator extends GenericResource {
 
     Logger log = SWBUtils.getLogger(FlickerAuthenticator.class);
+    /**
+     * Se crea una instancia de tipo SWBFlickrOauth, la cual permite tener acceso a los metodos
+     * que se encargaran de conectarse al sitio para obtener los permisos del usuario
+     */
     SWBFlickrOauth oauthConnection = new SWBFlickrOauth();
 
+    /**
+     * Env&iacute;a a la vista que mostrar&aacute; la opci&oacute;n para obtener las
+     * credenciales de  una cuenta en Flickr
+     * @param request
+     * @param response
+     * @param paramRequest
+     * @throws SWBResourceException
+     * @throws IOException
+     */
     @Override
     public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
         try {
@@ -41,6 +56,17 @@ public class FlickerAuthenticator extends GenericResource {
         }
     }
 
+    /**
+     * Env&iacute;a un redireccionamiento al sitio de Flickr que har&aacute; la petici&oacute;
+     * de un token temporal para poder obtener las credenciales del usuario. Si todo esta
+     * correcto se mostrar&aacute; la ventana de Flickr para que el usuario autorize
+     * a la aplicaci&oacute;n de usar su cuenta
+     * @param request
+     * @param response
+     * @param paramRequest
+     * @throws SWBResourceException
+     * @throws IOException
+     */
     public void doAuthorization(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
         if(request.getParameter("uri") != null) {
             String uri = request.getParameter("uri");
@@ -70,6 +96,17 @@ public class FlickerAuthenticator extends GenericResource {
         }
     }
 
+    /**
+     * Procesa las distintas vistas para la obtención de credenciales de un usuario. Valida
+     * si en el llamado al recurso vienen adicionalmente los parámetros de que el usuario
+     * ha aceptado que la aplicaci&oacute;n utilize su cuenta y almacena los tokens permanentes
+     * en el objeto Flickr
+     * @param request
+     * @param response
+     * @param paramRequest
+     * @throws SWBResourceException
+     * @throws IOException
+     */
     @Override
     public void processRequest(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
         if(paramRequest.getMode().equals("firstStepAuth")){
