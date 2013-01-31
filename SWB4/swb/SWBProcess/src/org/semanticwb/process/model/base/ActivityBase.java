f7e61d3c97be8165bd9d7a052a69100942d5c153
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-01-31 18:01:05 +0000

    Código regenerado

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/ActivityBase.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/ActivityBase.java
@@ -1,7 +1,7 @@
 package org.semanticwb.process.model.base;
 
 
-public abstract class ActivityBase extends org.semanticwb.process.model.FlowNode implements org.semanticwb.model.Descriptiveable,org.semanticwb.model.UserGroupRefable,org.semanticwb.model.Referensable,org.semanticwb.model.RuleRefable,org.semanticwb.model.RoleRefable,org.semanticwb.model.Traceable,org.semanticwb.process.model.ResourceAssignmentable
+public abstract class ActivityBase extends org.semanticwb.process.model.FlowNode implements org.semanticwb.model.RoleRefable,org.semanticwb.model.RuleRefable,org.semanticwb.model.Referensable,org.semanticwb.process.model.ResourceAssignmentable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Traceable,org.semanticwb.model.UserGroupRefable
 {
     public static final org.semanticwb.platform.SemanticClass swp_Activity=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/process#Activity");
    /**
