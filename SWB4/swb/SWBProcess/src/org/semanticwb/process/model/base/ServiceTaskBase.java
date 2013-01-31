48bfb82c5f251ac42a7574f7152ecdc2a769ca5a
Hasdai Pacheco <haxdai@gmail.com>
2013-01-31 17:55:21 +0000

    Código regenerado

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/ServiceTaskBase.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/ServiceTaskBase.java
@@ -1,7 +1,7 @@
 package org.semanticwb.process.model.base;
 
 
-public abstract class ServiceTaskBase extends org.semanticwb.process.model.Task implements org.semanticwb.model.Descriptiveable,org.semanticwb.model.UserGroupRefable,org.semanticwb.model.Referensable,org.semanticwb.model.RuleRefable,org.semanticwb.model.RoleRefable,org.semanticwb.process.model.ActivityConfable,org.semanticwb.model.Traceable,org.semanticwb.process.model.ResourceAssignmentable
+public abstract class ServiceTaskBase extends org.semanticwb.process.model.Task implements org.semanticwb.model.RoleRefable,org.semanticwb.model.RuleRefable,org.semanticwb.model.Referensable,org.semanticwb.process.model.ResourceAssignmentable,org.semanticwb.model.Descriptiveable,org.semanticwb.process.model.ActivityConfable,org.semanticwb.model.Traceable,org.semanticwb.model.UserGroupRefable
 {
     public static final org.semanticwb.platform.SemanticClass swp_ProcessService=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/process#ProcessService");
     public static final org.semanticwb.platform.SemanticProperty swp_processService=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.semanticwebbuilder.org/swb4/process#processService");
