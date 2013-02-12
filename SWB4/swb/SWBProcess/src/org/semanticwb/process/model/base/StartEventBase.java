142e2a575f5b195aa3c07ded580b8bcb51cc4e26
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-02-12 18:48:16 +0000

    Código regenerado

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/StartEventBase.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/StartEventBase.java
@@ -1,7 +1,7 @@
 package org.semanticwb.process.model.base;
 
 
-public abstract class StartEventBase extends org.semanticwb.process.model.StartEventNode implements org.semanticwb.model.Referensable,org.semanticwb.model.RuleRefable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Traceable,org.semanticwb.model.UserGroupRefable,org.semanticwb.model.RoleRefable
+public abstract class StartEventBase extends org.semanticwb.process.model.StartEventNode implements org.semanticwb.model.Referensable,org.semanticwb.model.RuleRefable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.UserGroupRefable,org.semanticwb.model.RoleRefable,org.semanticwb.model.Traceable
 {
     public static final org.semanticwb.platform.SemanticClass swp_StartEvent=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/process#StartEvent");
    /**
