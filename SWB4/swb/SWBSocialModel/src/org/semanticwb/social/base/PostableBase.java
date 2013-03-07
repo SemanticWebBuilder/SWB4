bc5d64ba4a0e216432675c65e2ff99492ca3996a
jsolis <jsolis@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-03-07 00:56:08 +0000

    Autoversioning commit:  a non-deltaV client made a change to /SWB4/swb/SWBSocialModel/src/org/semanticwb/social/base/PostableBase.java

--- SWB4/swb/SWBSocialModel/src/org/semanticwb/social/base/PostableBase.java
+++
@@ -1,6 +0,0 @@
-package org.semanticwb.social.base;
-
-public interface PostableBase extends org.semanticwb.model.GenericObject
-{
-    public static final org.semanticwb.platform.SemanticClass social_Postable=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.owl-ontologies.com/socialNet#Postable");
-}
