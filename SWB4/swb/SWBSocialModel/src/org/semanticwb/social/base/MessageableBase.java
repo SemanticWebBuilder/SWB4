35f5b1d7d76722a2bc7599f29e81de79a752a9b3
Javier Solis <softjei@gmail.com>
2013-03-07 00:56:06 +0000

    Autoversioning commit:  a non-deltaV client made a change to /SWB4/swb/SWBSocialModel/src/org/semanticwb/social/base/MessageableBase.java

--- SWB4/swb/SWBSocialModel/src/org/semanticwb/social/base/MessageableBase.java
+++
@@ -1,6 +0,0 @@
-package org.semanticwb.social.base;
-
-public interface MessageableBase extends org.semanticwb.social.Postable
-{
-    public static final org.semanticwb.platform.SemanticClass social_Messageable=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.owl-ontologies.com/socialNet#Messageable");
-}
