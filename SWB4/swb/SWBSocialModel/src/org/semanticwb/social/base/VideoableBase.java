0a700cca26dbab150c3498a6e7136f8f1deb20b3
jsolis <jsolis@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-03-07 00:56:25 +0000

    Autoversioning commit:  a non-deltaV client made a change to /SWB4/swb/SWBSocialModel/src/org/semanticwb/social/base/VideoableBase.java

--- SWB4/swb/SWBSocialModel/src/org/semanticwb/social/base/VideoableBase.java
+++
@@ -1,6 +0,0 @@
-package org.semanticwb.social.base;
-
-public interface VideoableBase extends org.semanticwb.social.Postable
-{
-    public static final org.semanticwb.platform.SemanticClass social_Videoable=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.owl-ontologies.com/socialNet#Videoable");
-}
