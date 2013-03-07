2a81ff1d637921a44cfb0459323b5b242e57089b
Javier Solis <softjei@gmail.com>
2013-03-07 00:56:13 +0000

    Autoversioning commit:  a non-deltaV client made a change to /SWB4/swb/SWBSocialModel/src/org/semanticwb/social/base/PhotoableBase.java

--- SWB4/swb/SWBSocialModel/src/org/semanticwb/social/base/PhotoableBase.java
+++
@@ -1,6 +0,0 @@
-package org.semanticwb.social.base;
-
-public interface PhotoableBase extends org.semanticwb.social.Postable
-{
-    public static final org.semanticwb.platform.SemanticClass social_Photoable=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.owl-ontologies.com/socialNet#Photoable");
-}
