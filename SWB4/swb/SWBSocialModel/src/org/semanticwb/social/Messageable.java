92f48bf39b8ffe58cfc587b3a8da1919d8172aa7
Javier Solis <softjei@gmail.com>
2013-03-07 00:55:55 +0000

    Autoversioning commit:  a non-deltaV client made a change to /SWB4/swb/SWBSocialModel/src/org/semanticwb/social/Messageable.java

--- SWB4/swb/SWBSocialModel/src/org/semanticwb/social/Messageable.java
+++
@@ -1,9 +0,0 @@
-package org.semanticwb.social;
-
-import javax.servlet.http.HttpServletRequest;
-import org.semanticwb.portal.api.SWBActionResponse;
-
-public interface Messageable extends org.semanticwb.social.base.MessageableBase
-{
-    public void postMsg(Message message, HttpServletRequest request, SWBActionResponse response);
-}
