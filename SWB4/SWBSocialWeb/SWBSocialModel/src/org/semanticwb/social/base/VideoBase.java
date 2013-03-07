fb990868ce8e183c973db1df37194ba2868a7bf4
george24 <jajs1975@users.noreply.github.com>
2013-03-07 18:03:00 +0000

    xy

--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/VideoBase.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/VideoBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.social.base;
    /**
    * Clase que sera creada cuando un post sea de tipo video, como lo es para youtube y facebook (cuando se envía un video a facebook). 
    */
-public abstract class VideoBase extends org.semanticwb.social.PostOut implements org.semanticwb.model.Descriptiveable,org.semanticwb.model.Tagable,org.semanticwb.social.PostDataable,org.semanticwb.social.PostVideoable,org.semanticwb.model.Traceable
+public abstract class VideoBase extends org.semanticwb.social.PostOut implements org.semanticwb.social.PostVideoable,org.semanticwb.model.Tagable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Traceable,org.semanticwb.social.PostDataable
 {
    /**
    * Clase que sera creada cuando un post sea de tipo video, como lo es para youtube y facebook (cuando se envía un video a facebook).
