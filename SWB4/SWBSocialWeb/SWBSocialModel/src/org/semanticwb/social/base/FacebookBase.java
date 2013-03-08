1e3e5cac12f58a57d1f4ae1a00d6c6b323088d25
george24 <jajs1975@users.noreply.github.com>
2013-03-08 00:38:44 +0000

    xy

--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/FacebookBase.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/FacebookBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.social.base;
    /**
    * Clase que almacenara las diferentes cuentas de una organización para la red social Facebook. 
    */
-public abstract class FacebookBase extends org.semanticwb.social.SocialNetwork implements org.semanticwb.model.Traceable,org.semanticwb.social.Listenerable,org.semanticwb.social.Messageable,org.semanticwb.model.Descriptiveable,org.semanticwb.social.Oauthable,org.semanticwb.social.Secreteable,org.semanticwb.model.Activeable,org.semanticwb.social.Photoable,org.semanticwb.social.SocialNetPostable,org.semanticwb.social.Videoable
+public abstract class FacebookBase extends org.semanticwb.social.SocialNetwork implements org.semanticwb.social.Videoable,org.semanticwb.social.SocialNetPostable,org.semanticwb.social.Secreteable,org.semanticwb.model.Traceable,org.semanticwb.social.Listenerable,org.semanticwb.social.Photoable,org.semanticwb.social.Oauthable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Activeable,org.semanticwb.social.Messageable
 {
    /**
    * Eliminar despues esta propiedad, ver si Jose puede eliminar la propiedad login que se encuentra en la clase padre(SocialNetwork).
--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/SocialAdminBase.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/SocialAdminBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.social.base;
    /**
    * Objeto que define un Sitio Web de Administración de SWBSocial 
    */
-public abstract class SocialAdminBase extends org.semanticwb.model.AdminWebSite implements org.semanticwb.model.Traceable,org.semanticwb.model.FilterableNode,org.semanticwb.model.FilterableClass,org.semanticwb.model.OntologyDepable,org.semanticwb.model.Undeleteable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Trashable,org.semanticwb.model.Localeable,org.semanticwb.model.Countryable,org.semanticwb.model.Activeable,org.semanticwb.model.Indexable,org.semanticwb.model.Filterable
+public abstract class SocialAdminBase extends org.semanticwb.model.AdminWebSite implements org.semanticwb.model.Localeable,org.semanticwb.model.OntologyDepable,org.semanticwb.model.Traceable,org.semanticwb.model.Indexable,org.semanticwb.model.Filterable,org.semanticwb.model.Countryable,org.semanticwb.model.FilterableNode,org.semanticwb.model.Undeleteable,org.semanticwb.model.FilterableClass,org.semanticwb.model.Trashable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Activeable
 {
    /**
    * Es una pagina web utilizada para mostrar opciones del menu dentro de la administración de SWB
