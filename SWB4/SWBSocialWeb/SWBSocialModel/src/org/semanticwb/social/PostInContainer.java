66028eb80f3e7c6d0cd9dbcb3e2a2b9c1d47582d
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-04-05 23:54:55 +0000

    xxxxx

--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/PostInContainer.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/PostInContainer.java
@@ -1,5 +1,10 @@
 package org.semanticwb.social;
 
+import java.util.Date;
+import java.util.Iterator;
+import org.semanticwb.model.SWBModel;
+import org.semanticwb.platform.SemanticObject;
+
 
    /**
    * Clase a Cambiar despues por "Relacional".  En esta clase se guardan todos los post que lleguan por el listener, se estima que toda la info. que se guarde en este objeto debe de eliminarse aproximadamente c/mes, siendo este parametro configurable de acuerdo al tiempo que la organización quiera guardar  la información sobre los mensajes que lleguen por el listener. Cuando un post que llegue por el listener sea tomado como base para crear un nuevo post por la organización, se cree que debe copiarse la información de dicho post de esta clase hacia la clase PostListenerContainerBase.Se almacenan por mes y año, de esta manera sera mucho mas rapido hacer las busquedas sobre las instancias de esta clase. 
@@ -9,5 +14,44 @@ public class PostInContainer extends org.semanticwb.social.base.PostInContainerB
     public PostInContainer(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
+    }
+    
+    public static PostInContainer getPostListenerContainerByDate(Date date, SWBModel model)
+    {
+        //
+        //System.out.println("Borrado de todos los PostContainers...-No baorraraaaa");
+        /*
+        Iterator <PostContainer> itPostC=PostContainer.ClassMgr.listPostContainers(model);
+        while(itPostC.hasNext())
+        {
+            PostContainer postC=itPostC.next();
+            postC.remove();
+        }
+         * */
+
+        //System.out.println("Termina Borrado de todos los PostContainers...");
+        //
+
+        int year=1900+(date.getYear());
+        int month=1+(date.getMonth());
+        //System.out.println("year pa buscar:"+year);
+        //System.out.println("month pa buscar:"+month);
+        Iterator<SemanticObject> it=model.getSemanticModel().listSubjects(social_plc_year, year+""); //No encuentra
+        while(it.hasNext())
+        {
+            SemanticObject obj=it.next();
+            PostInContainer plcContainer=(PostInContainer)obj.createGenericInstance();
+            //System.out.println("postContainer encontrado:"+plcContainer+",year:"+plcContainer.getPlc_year()+", month:"+plcContainer.getPlc_month());
+            if(plcContainer.getPlc_month().equals(String.valueOf(month)));
+            {
+                //System.out.println("Regresa PostContainer:"+plcContainer);
+                return plcContainer;
+            }
+        }
+        //System.out.println("Va ha crear nuevo postContainer..");
+        PostInContainer plcContainer=PostInContainer.ClassMgr.createPostInContainer(model);
+        plcContainer.setPlc_year(year+"");
+        plcContainer.setPlc_month(month+"");
+        return plcContainer;
     }
 }
--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/PostOutContainer.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/PostOutContainer.java
@@ -1,5 +1,10 @@
 package org.semanticwb.social;
 
+import java.util.Date;
+import java.util.Iterator;
+import org.semanticwb.model.SWBModel;
+import org.semanticwb.platform.SemanticObject;
+
 
    /**
    * Clase a Cambiar despues por "Relacional". Clase que contiene TODOS los post de salida (PostOut) que han sido enviados a una determinada red social. La intención de crear esta clase es para que se agrupen los Post de cada red social por mes y año, y de esta manera sea mucho mas sencillo, optimo y rapido realizar las busquedas. De acuerdo al PostOut, puede buscar si este existe en la clase PostListenerContainerBase, si si existe, quiere decir que es un PostOut que se origino mediante la contestación de un PostIn, si no existe en esa clase, es que es un PostOut que se origino como nuevo, sin que haya sido contestación de un PostIn. 
@@ -9,5 +14,44 @@ public class PostOutContainer extends org.semanticwb.social.base.PostOutContaine
     public PostOutContainer(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
+    }
+    
+    public static PostOutContainer getPostContainerByDate(Date date, SWBModel model)
+    {
+        //
+        System.out.println("Borrado de todos los PostContainers...-No baorraraaaa");
+        /*
+        Iterator <PostContainer> itPostC=PostContainer.ClassMgr.listPostContainers(model);
+        while(itPostC.hasNext())
+        {
+            PostContainer postC=itPostC.next();
+            postC.remove();
+        }
+         * */
+
+        System.out.println("Termina Borrado de todos los PostContainers...");
+        //
+
+        int year=1900+(date.getYear());
+        int month=1+(date.getMonth());
+        System.out.println("year pa buscar:"+year);
+        System.out.println("month pa buscar:"+month);
+        Iterator<SemanticObject> it=model.getSemanticModel().listSubjects(social_year, year+""); //No encuentra
+        while(it.hasNext())
+        {
+            SemanticObject obj=it.next();
+            PostOutContainer postContainer=(PostOutContainer)obj.createGenericInstance();
+            System.out.println("postContainer encontrado:"+postContainer+",year:"+postContainer.getYear()+", month:"+postContainer.getMonth());
+            if(postContainer.getMonth().equals(String.valueOf(month)));
+            {
+                System.out.println("Regresa PostContainer:"+postContainer);
+                return postContainer;
+            }
+        }
+        System.out.println("Va ha crear nuevo postContainer..");
+        PostOutContainer postContainer=PostOutContainer.ClassMgr.createPostOutContainer(model);
+        postContainer.setYear(year+"");
+        postContainer.setMonth(month+"");
+        return postContainer;
     }
 }
