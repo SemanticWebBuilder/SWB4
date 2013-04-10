b5419c19023c52d189ee56fd909cf5f3967ad0e8
Carlos Álvarez <carlos19851@users.noreply.github.com>
2013-04-10 17:28:33 +0000

    Creado

---
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/reports/FileReport.java
@@ -0,0 +1,10 @@
+package org.semanticwb.process.resources.reports;
+
+
+public class FileReport extends org.semanticwb.process.resources.reports.base.FileReportBase 
+{
+    public FileReport(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/resources/reports/Report.java
@@ -0,0 +1,10 @@
+package org.semanticwb.process.resources.reports;
+
+
+public class Report extends org.semanticwb.process.resources.reports.base.ReportBase 
+{
+    public Report(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
