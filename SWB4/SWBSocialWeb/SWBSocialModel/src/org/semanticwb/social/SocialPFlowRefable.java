b544f648f71a7d9d4a7ae93b254187bdeffd3811
george24 <jajs1975@users.noreply.github.com>
2013-04-30 22:22:34 +0000

    xxxxx

---
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/SocialPFlowRefable.java
@@ -0,0 +1,8 @@
+package org.semanticwb.social;
+
+   /**
+   * Interfaz que define propiedades para elementos que pueden tener referencia a flujos de publicacion 
+   */
+public interface SocialPFlowRefable extends org.semanticwb.social.base.SocialPFlowRefableBase
+{
+}
