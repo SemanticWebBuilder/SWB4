6b7603046e2da8d49d1dee7f25d016bb93a23ae0
martha <martha@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-05-15 23:35:26 +0000

    Modificaciones al objeto Indicador

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/Indicator.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/Indicator.java
@@ -7,4 +7,29 @@ public class Indicator extends org.semanticwb.bsc.base.IndicatorBase
     {
         super(base);
     }
+
+    @Override
+    public String getFolio() {
+        String folio = super.getFolio();
+        Indicator indicator = (Indicator)getSemanticObject().createGenericInstance();
+        if(folio == null) {
+            String folioIndicator = "";
+            if(indicator.getParent() instanceof Objective) {
+                Objective objective = (Objective) indicator.getParent();
+                objective.setProperty("folioObjective", "F1");
+                folioIndicator = objective.getProperty("folioObjective") + "." + indicator.getId();
+                
+            }
+            return folioIndicator;
+        } else {
+            return super.getFolio();
+        }
+    }
+
+    @Override
+    public void setFolio(String value) {
+        super.setFolio(value);
+    }
+    
+    
 }
---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/tracing/Evidence.java
@@ -0,0 +1,13 @@
+package org.semanticwb.bsc.tracing;
+
+
+   /**
+   * Clase que permite definir los atributos de las evidencias de un Indicador 
+   */
+public class Evidence extends org.semanticwb.bsc.tracing.base.EvidenceBase 
+{
+    public Evidence(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
