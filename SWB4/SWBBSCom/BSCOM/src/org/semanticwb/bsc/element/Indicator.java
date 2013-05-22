b143518c9dce2d0760b36aed295cfc01330d4da6
martha <martha@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-05-22 01:04:08 +0000

    Documentación generada para el indicador

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/Indicator.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/Indicator.java
@@ -1,20 +1,30 @@
 package org.semanticwb.bsc.element;
 
-import java.util.Iterator;
-import org.semanticwb.bsc.BSC;
-import org.semanticwb.model.WebSite;
-
-
+/*
+ * Clase que persiste informaci&oacute;n de un indicador. Un indicador permite informar sobre el avance para alcanzar un objetivo.
+ */
 public class Indicator extends org.semanticwb.bsc.element.base.IndicatorBase 
 {
-    private String id;
-    
+   
     public Indicator(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
-//        this.id = "1";
     }
     
+    /**
+     * Obtiene el prefijo para identificar un indicador. 
+     * En caso de que el prefijo sea nulo, asigna un prefijo en autom&aacute;tico. 
+     * @return {@code String} Regresa un string con el prefijo asociado al identificador, 
+     * si el identificador es asignado en autom&aacute;tico utiliza el prefijo del objetivo
+     * seguido de un número consecutivo asignado por un motor.
+     * <p>
+     * Ejemplo:
+     * </p> 
+     * <p>
+     * El motor asigna un n&uacute;mero 1 para el Objetivo F1, se obtendr&iacute;a el siguiente identificador: 
+     *  {@literal  F1.1 }
+     * </p>
+     */
     @Override
     public String getPrefix() {
         String prefix = super.getPrefix();
@@ -29,10 +39,13 @@ public class Indicator extends org.semanticwb.bsc.element.base.IndicatorBase
         return prefix;
     }
 
+    /**
+     * Asigna un prefijo al indicador
+     * @param value 
+     */
     @Override
     public void setPrefix(String value) {
         super.setPrefix(value);
-           
     }
     
 }
