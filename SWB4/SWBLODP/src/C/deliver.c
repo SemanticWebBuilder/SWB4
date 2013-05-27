9f30560a2274e12d3dea32f9a885b0578cd0af44
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2013-05-27 23:54:39 +0000

    Security review

--- SWB4/SWBLODP/src/C/deliver.c
+++ SWB4/SWBLODP/src/C/deliver.c
@@ -106,6 +106,8 @@ int main(int argc, char** argv) {
                                 flag = 1;
                             }
                         }
+                    } else {
+                        printf("\n\n\n");
                     }
                 }
 
