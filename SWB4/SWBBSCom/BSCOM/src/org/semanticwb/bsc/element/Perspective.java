7fe3c10258e8644241021b6ab946682a115e5b53
cramos <ciramosi@users.noreply.github.com>
2013-06-07 23:28:15 +0000

    Se agregó funcionalidad para REQ_03-Valor del Prefijo por omisión

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/Perspective.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/Perspective.java
@@ -19,4 +19,23 @@ public class Perspective extends org.semanticwb.bsc.element.base.PerspectiveBase
         super.setSerial(value);
     }
     
+    @Override
+    public String getPrefix() {
+        String prefix = super.getPrefix();
+        if(prefix==null) {
+            try {
+                prefix = getTitle().trim().substring(0, 1).toUpperCase();
+            }catch(Exception e) {
+                prefix = "Untitled";
+            }
+            setPrefix(prefix);
+        }
+        return prefix;
+    }
+
+    @Override
+    public void setPrefix(String value) {
+        super.setPrefix(value);           
+    }
+    
 }
