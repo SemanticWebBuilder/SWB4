52138ba745254df73cfee70acc3f439b76d4260d
cramos <ciramosi@users.noreply.github.com>
2013-06-12 20:23:34 +0000

    Validar ordinal para objetivos StateMachinable

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/Machinable.java
@@ -0,0 +1,8 @@
+package org.semanticwb.bsc;
+
+   /**
+   * Cualquier elemento que se comporte como una máquina de estados. Estado inicial, estados de transición y estado final. 
+   */
+public interface Machinable extends org.semanticwb.bsc.base.MachinableBase
+{
+}
