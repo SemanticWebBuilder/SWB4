17ac3c13c2e017be964e8cfe0c7057402a6404d1
Martha <marthajimenez@users.noreply.github.com>
2013-06-18 18:32:59 +0000

    Producto Seleccion de Objetivos

--- SWB4/SWBBSCom/BSCI/src/org/semanticwb/bsc/admin/resources/SelectPeriods.java
+++ SWB4/SWBBSCom/BSCI/src/org/semanticwb/bsc/admin/resources/SelectPeriods.java
@@ -23,32 +23,35 @@ import org.semanticwb.portal.api.SWBResourceException;
 import org.semanticwb.portal.api.SWBResourceURL;
 
 /**
- * SelectPeriods es una clase que permite asociar o  desasociar periodos a un objetivo
- * 
- * 
- * @author      Martha Elvia Jiménez Salgado
- * @version     %I%, %G%
- * @since       1.0
+ * SelectPeriods es una clase que permite asociar o desasociar periodos a un
+ * objetivo
+ *
+ *
+ * @author Martha Elvia Jiménez Salgado
+ * @version %I%, %G%
+ * @since 1.0
  */
 public class SelectPeriods extends GenericResource {
 
     /**
-     * Método que se encarga de presentar la forma al usuario para que elija un conjunto de periodos
+     * Método que se encarga de presentar la forma al usuario para que elija un
+     * conjunto de periodos
+     *
      * @param request
      * @param response
      * @param paramRequest
      * @throws SWBResourceException
-     * @throws IOException 
+     * @throws IOException
      */
     @Override
     public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
         PrintWriter out = response.getWriter();
         String suri = request.getParameter("suri");
-        
+
         SemanticObject semObj = SemanticObject.getSemanticObject(suri);
         if (semObj != null) {
             Objective obj = (Objective) semObj.createGenericInstance();
-            List objetivesCurrent =  new ArrayList();
+            List objetivesCurrent = new ArrayList();
             Iterator<Period> itObj = obj.listPeriodObjetives();
             while (itObj.hasNext()) {
                 Period periodSave = itObj.next();
@@ -66,30 +69,33 @@ public class SelectPeriods extends GenericResource {
                 while (itPeriods.hasNext()) {
                     Period period = itPeriods.next();
                     String select = "";
-                    if(objetivesCurrent.contains(period)) {
+                    if (objetivesCurrent.contains(period)) {
                         select = "checked";
                     }
-                    out.println("<li><input id=\"" + period.getId() + "\" name=\"period\" type=\"checkbox\" value=\"" + period.getId() +"\" "
+                    out.println("<li><input id=\"" + period.getId() + "\" name=\"period\" type=\"checkbox\" value=\"" + period.getId() + "\" "
                             + " data-dojo-type=\"dijit.form.CheckBox\" " + select + ">"
                             + "<label for=\"" + period.getId() + "\">" + period.getTitle()
                             + "</label></li>");
                 }
                 out.println("</ul>");
                 out.println("<button dojoType=\"dijit.form.Button\" type=\"submit\">Guardar</button>");
+                out.println("<button dojoType=\"dijit.form.Button\" type=\"button\" onClick=\"reloadTab('" + obj.getURI() + "');\">Cancelar</button>");
                 out.println("</form>");
             } else {
-                out.println("<p>Debe crearse periodos o asignarse</p>");
+                out.println("<p>Debe crearse periodos</p>");
             }
 
         }
     }
-    
+
     /**
-     * Método que se encarga de persistir la información de forma segura en un objetivo
+     * Método que se encarga de persistir la información de forma segura en un
+     * objetivo
+     *
      * @param request
      * @param response
      * @throws SWBResourceException
-     * @throws IOException 
+     * @throws IOException
      */
     @Override
     public void processAction(HttpServletRequest request, SWBActionResponse response) throws SWBResourceException, IOException {
@@ -100,15 +106,9 @@ public class SelectPeriods extends GenericResource {
         if (SWBResourceURL.Action_ADD.equals(action) && ws != null) {
             String[] periods = request.getParameterValues("period");
             String suri = request.getParameter("suri");
-            objective= (Objective) SemanticObject.getSemanticObject(suri).createGenericInstance();
-            if (objective != null) {
-                Iterator<Period> itPeriods = objective.listPeriodObjetives();
-                if (itPeriods.hasNext()) {
-                    while (itPeriods.hasNext()) {
-                        Period period = itPeriods.next();
-                        objective.removePeriodObjetive(period);
-                    }
-                }
+            objective = (Objective) SemanticObject.getSemanticObject(suri).createGenericInstance();
+            if (objective != null && periods != null && periods.length > 0) {
+                removePeriodsInObj(objective);
                 if (periods != null && periods.length > 0) {
                     for (int i = 0; i < periods.length; i++) {
                         Period periodCheck = Period.ClassMgr.getPeriod(periods[i], ws);
@@ -118,7 +118,21 @@ public class SelectPeriods extends GenericResource {
                     }
                 }
             }
+            if (periods == null) {
+                removePeriodsInObj(objective);
+            }
         }
+
         response.setMode(SWBResourceURL.Mode_VIEW);
+    }
+
+    private void removePeriodsInObj(Objective objective) {
+        Iterator<Period> itPeriods = objective.listPeriodObjetives();
+        if (itPeriods.hasNext()) {
+            while (itPeriods.hasNext()) {
+                Period period = itPeriods.next();
+                objective.removePeriodObjetive(period);
+            }
+        }
     }
 }
