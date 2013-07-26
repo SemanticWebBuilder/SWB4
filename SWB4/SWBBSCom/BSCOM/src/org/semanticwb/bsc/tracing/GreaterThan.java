cb020aa418ea990cc24eac1a80b0f6b74fad4328
cramos <ciramosi@users.noreply.github.com>
2013-07-26 06:22:35 +0000

    Se agregó nuevas operaciones, mayor que y menor que

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/tracing/GreaterThan.java
@@ -0,0 +1,10 @@
+package org.semanticwb.bsc.tracing;
+
+
+public class GreaterThan extends org.semanticwb.bsc.tracing.base.GreaterThanBase 
+{
+    public GreaterThan(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/tracing/Operation.java
@@ -0,0 +1,10 @@
+package org.semanticwb.bsc.tracing;
+
+
+public class Operation extends org.semanticwb.bsc.tracing.base.OperationBase 
+{
+    public Operation(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
