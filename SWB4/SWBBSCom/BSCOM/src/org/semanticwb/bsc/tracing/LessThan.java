319bf9b4e2559f46e7332ea41ad85ad85f6ebc87
cramos <cramos@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-07-26 06:22:35 +0000

    Se agregó nuevas operaciones, mayor que y menor que

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/tracing/LessThan.java
@@ -0,0 +1,10 @@
+package org.semanticwb.bsc.tracing;
+
+
+public class LessThan extends org.semanticwb.bsc.tracing.base.LessThanBase 
+{
+    public LessThan(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
