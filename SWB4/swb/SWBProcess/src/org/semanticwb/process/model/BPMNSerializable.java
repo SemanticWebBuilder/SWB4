427d481c4fead5b1dd9a11f922ad6778af9c58c1
Hasdai Pacheco <haxdai@gmail.com>
2013-08-16 18:47:10 +0000

    Agregada interfaz BPMNSerializable para serialización de elementos de BPMN Los objetos Process y SubProcess ahora son subclases de BPMNSerializable

---
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/BPMNSerializable.java
@@ -0,0 +1,8 @@
+package org.semanticwb.process.model;
+
+   /**
+   * Comportamiento que incluye las propiedades para elementos del BPMS que pueden serializarse en algún formato 
+   */
+public interface BPMNSerializable extends org.semanticwb.process.model.base.BPMNSerializableBase
+{
+}
