7f8c57c8f8fe8e104c5a166bcb8371bb56b61289
cramos <ciramosi@users.noreply.github.com>
2013-08-22 03:55:51 +0000

    Diferenciadores y grupos de diferenciadores

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/accessory/DifferentiatorGroup.java
@@ -0,0 +1,13 @@
+package org.semanticwb.bsc.accessory;
+
+
+   /**
+   * Un DifferentiatorGroup es una clase que permitir contener uno o varios Differentiator que se dibujan en el mapa estratégico del scorecard. 
+   */
+public class DifferentiatorGroup extends org.semanticwb.bsc.accessory.base.DifferentiatorGroupBase 
+{
+    public DifferentiatorGroup(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
