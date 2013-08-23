d07458639324c38370e36660453aba2d81d97042
redentor <JRedentor@users.noreply.github.com>
2013-08-23 17:06:36 +0000

    Re-organización de las clases utilizadas en las vistas resumen.

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/utils/SummaryView.java
@@ -0,0 +1,13 @@
+package org.semanticwb.bsc.utils;
+
+
+   /**
+   * Define la estructura de propiedades a mostrar en las vistas resumen. 
+   */
+public class SummaryView extends org.semanticwb.bsc.utils.base.SummaryViewBase 
+{
+    public SummaryView(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
