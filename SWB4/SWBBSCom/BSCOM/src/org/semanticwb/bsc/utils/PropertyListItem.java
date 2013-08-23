ce9441a07fdd680bb69937ceeede0a628aa012c9
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-08-23 17:06:36 +0000

    Re-organización de las clases utilizadas en las vistas resumen.

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/utils/PropertyListItem.java
@@ -0,0 +1,13 @@
+package org.semanticwb.bsc.utils;
+
+
+   /**
+   * Define el orden en que debe aparecer una propiedad en la vista resumen correspondiente. 
+   */
+public class PropertyListItem extends org.semanticwb.bsc.utils.base.PropertyListItemBase 
+{
+    public PropertyListItem(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
