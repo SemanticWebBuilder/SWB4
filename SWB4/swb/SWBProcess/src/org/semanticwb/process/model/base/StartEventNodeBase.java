192b57d0b3597cd44af63ecb39d033fcf2027831
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-09-12 00:22:07 +0000

    Código regenerado

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/StartEventNodeBase.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/StartEventNodeBase.java
@@ -2,14 +2,14 @@ package org.semanticwb.process.model.base;
 
 
    /**
-   * Nodo Raiz de Eventos Iniciales 
+   * Nodo raiz de Eventos Iniciales 
    */
-public abstract class StartEventNodeBase extends org.semanticwb.process.model.CatchEvent implements org.semanticwb.model.Descriptiveable,org.semanticwb.model.Traceable
+public abstract class StartEventNodeBase extends org.semanticwb.process.model.CatchEvent implements org.semanticwb.model.Sortable,org.semanticwb.model.Traceable,org.semanticwb.model.Descriptiveable
 {
     public static final org.semanticwb.platform.SemanticClass swp_FlowNode=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/process#FlowNode");
     public static final org.semanticwb.platform.SemanticProperty swp_ie_next=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.semanticwebbuilder.org/swb4/process#ie_next");
    /**
-   * Nodo Raiz de Eventos Iniciales
+   * Nodo raiz de Eventos Iniciales
    */
     public static final org.semanticwb.platform.SemanticClass swp_StartEventNode=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/process#StartEventNode");
    /**
