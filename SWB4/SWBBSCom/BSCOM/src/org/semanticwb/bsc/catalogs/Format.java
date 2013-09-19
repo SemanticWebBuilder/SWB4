655219e5ae54c60fcbbaf366ef330ac2bcf3dfc1
cramos <ciramosi@users.noreply.github.com>
2013-09-19 00:30:04 +0000

    listas de valores válidos

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/catalogs/Format.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/catalogs/Format.java
@@ -3,6 +3,8 @@ package org.semanticwb.bsc.catalogs;
 
 public class Format extends org.semanticwb.bsc.catalogs.base.FormatBase 
 {
+    public static final String defaultFormatPattern = "#,##0.0#";
+    
     public Format(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
