9ce250c9c4c6f464670ff6255c4c8b63a14a176b
redentor <JRedentor@users.noreply.github.com>
2013-09-27 23:04:03 +0000

    Adición del recurso para administración de vistas detalle

---
+++ SWB4/swb/SWBAdmin/src/swbadmin/js/fckeditor/editor/plugins/insertHtmlCode/propertiesData.js
@@ -0,0 +1,7 @@
+[
+    {name: "Prop1", uri: "bsc:prop1"},
+    {name: "Prop2", uri: "bsc:prop2"},
+    {name: "Prop3", uri: "bsc:prop3"},
+    {name: "Prop4", uri: "bsc:prop4"},
+    {name: "Prop5", uri: "bsc:prop5"}
+]
