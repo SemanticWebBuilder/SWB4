01f56d0acf8d21a05a53448d60b875be4ba8cdd3
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-10-01 21:18:31 +0000

    Cambios para reclasificar despues de que ya hayan llegado con anterioridad mensajes a un Stream

--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/YoutubeBase.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/YoutubeBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.social.base;
    /**
    * Clase que almacenara las diferentes cuentas de una organización para la red social YouTube. 
    */
-public abstract class YoutubeBase extends org.semanticwb.social.SocialNetwork implements org.semanticwb.social.Oauthable,org.semanticwb.model.Trashable,org.semanticwb.model.Activeable,org.semanticwb.social.Secreteable,org.semanticwb.social.Videoable,org.semanticwb.social.Messageable,org.semanticwb.model.Descriptiveable,org.semanticwb.social.DeveloperKeyable,org.semanticwb.social.Listenerable,org.semanticwb.social.SocialMonitorable,org.semanticwb.social.SocialNetPostable,org.semanticwb.model.Traceable,org.semanticwb.model.Tagable
+public abstract class YoutubeBase extends org.semanticwb.social.SocialNetwork implements org.semanticwb.model.Traceable,org.semanticwb.social.Secreteable,org.semanticwb.social.SocialMonitorable,org.semanticwb.social.SocialNetPostable,org.semanticwb.model.Descriptiveable,org.semanticwb.social.Oauthable,org.semanticwb.social.DeveloperKeyable,org.semanticwb.social.Videoable,org.semanticwb.social.Messageable,org.semanticwb.model.Tagable,org.semanticwb.model.Activeable,org.semanticwb.social.Listenerable,org.semanticwb.model.Trashable
 {
    /**
    * Categorias a llenar en una colección.
