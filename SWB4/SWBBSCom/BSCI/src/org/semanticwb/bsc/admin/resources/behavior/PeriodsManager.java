02fdfce0a32031f5ec3aa2fb3b9569f00b4d223b
redentor <JRedentor@users.noreply.github.com>
2013-10-08 16:58:32 +0000

    Adición de etiqueta para los períodos Anterior y Siguiente no asignados y despliegue de la columna "En tiempo".

--- SWB4/SWBBSCom/BSCI/src/org/semanticwb/bsc/admin/resources/behavior/PeriodsManager.java
+++ SWB4/SWBBSCom/BSCI/src/org/semanticwb/bsc/admin/resources/behavior/PeriodsManager.java
@@ -65,23 +65,7 @@ public class PeriodsManager extends GenericResource {
         out.println("  dojo.require('dijit.form.Form');\n");
         out.println("  dojo.require('dijit.form.CheckBox');\n");
         out.println("  dojo.require('dijit.form.Button');\n");
-        //incluir funcion de javascript para recorrer los elementos de la forma y marcar o desmarcar los checkbox, de acuerdo a un parametro
-        /*out.println("  function toggleChecks(formId, checkValue) {");
-        out.println("    var formPeriods = dijit.byId(formId);");
-        out.println("    if (formPeriods != undefined) {");
-        out.println("      for (var i = 0; i < formPeriods.getChildren().length; i++) {");
-        out.println("        var widget = formPeriods.getChildren()[i];");
-        out.println("        if (widget.declaredClass == \"dijit.form.CheckBox\") {");
-        out.println("          widget.setChecked(checkValue);");
-        out.println("        }");
-        out.println("      }");
-        out.println("    }");
-        out.println("  }");*/
         out.println("</script>\n");
-        out.println("  ");
-        out.println("  ");
-        out.println("  ");
-        out.println("  ");
 
         if (semObj != null) {
             BSC bsc = (BSC) semObj.getModel().getModelObject().createGenericInstance();
@@ -137,8 +121,9 @@ public class PeriodsManager extends GenericResource {
                 
                 while (itPeriods.hasNext()) {
                     Period period = itPeriods.next();
-
-                    if (  (period.isValid() && user.haveAccess(period)) || (!period.isActive() && seasonable.hasPeriod(period) && user.haveAccess(period))  ) {
+                    if (  (period.isValid() && user.haveAccess(period)) ||
+                            (!period.isActive() && seasonable.hasPeriod(period) &&
+                            user.haveAccess(period))  ) {
                         String select = "";
                         String title = period.getTitle(user.getLanguage()) == null
                                        ? period.getTitle()
@@ -150,13 +135,19 @@ public class PeriodsManager extends GenericResource {
                         
                         if (period.getPrevius() != null && period.getPrevius() instanceof Period) {
                             Period former = (Period) period.getPrevius();
-                            titleFormer = former.getTitle(user.getLanguage()) == null ? former.getTitle() : former.getTitle(user.getLanguage());
+                            titleFormer = former.getTitle(user.getLanguage()) == null 
+                                          ? former.getTitle() : former.getTitle(user.getLanguage());
                             hasFormer = true;
+                        } else {
+                            titleFormer = paramRequest.getLocaleString("lbl_notAssigned");
                         }
                         if (period.getNext() != null && period.getNext() instanceof Period) {
                             Period next = (Period) period.getNext();
-                            titleNext = next.getTitle(user.getLanguage()) == null ? next.getTitle() : next.getTitle(user.getLanguage());
+                            titleNext = next.getTitle(user.getLanguage()) == null
+                                        ? next.getTitle() : next.getTitle(user.getLanguage());
                             hasNext = true;
+                        } else {
+                            titleFormer = paramRequest.getLocaleString("lbl_notAssigned");
                         }
 
                         if (objetivesCurrent.contains(period)) {
@@ -177,7 +168,9 @@ public class PeriodsManager extends GenericResource {
                             out.print("              <a href=\"#\" onclick=\"addNewTab('" + period.getPrevius().getURI() + "','");
                             out.print(SWBPlatform.getContextPath() + "/swbadmin/jsp/objectTab.jsp" + "','" + 
                                     (titleFormer != null ? titleFormer : paramRequest.getLocaleString("lbl_noTitle")));
-                            out.println("');return false;\" >" + (titleFormer != null ? titleFormer : paramRequest.getLocaleString("lbl_noTitle")) + "</a>");
+                            out.println("');return false;\" >" +
+                                    (titleFormer != null ? titleFormer : paramRequest.getLocaleString("lbl_noTitle")) +
+                                    "</a>");
                         }
                         out.println("          </td>");
                         out.println("          <td>");
@@ -185,11 +178,17 @@ public class PeriodsManager extends GenericResource {
                             out.print("              <a href=\"#\" onclick=\"addNewTab('" + period.getNext().getURI() + "','");
                             out.print(SWBPlatform.getContextPath() + "/swbadmin/jsp/objectTab.jsp" + "','" + 
                                     (titleNext != null ? titleNext : paramRequest.getLocaleString("lbl_noTitle")));
-                            out.println("');return false;\" >" + (titleNext != null ? titleNext : paramRequest.getLocaleString("lbl_noTitle")) + "</a>");
+                            out.println("');return false;\" >" +
+                                        (titleNext != null ? titleNext : paramRequest.getLocaleString("lbl_noTitle")) +
+                                        "</a>");
                         }
                         out.println("          </td>");
                         //TODO: Definir como se obtiene el dato de esta columna
-                        out.println("          <td>Preguntar a Carlos</td>");            
+                        out.println("          <td>" + 
+                                (period.isBlocked()
+                                 ? paramRequest.getLocaleString("lbl_isInTime")
+                                 : paramRequest.getLocaleString("lbl_isNotInTime")) +
+                                "</td>");            
                         out.println("          <td align=\"center\"><input id=\"" + period.getId() + "\" name=\"period" + data
                                 + "\" type=\"checkbox\" value=\"" + period.getId() + "\" onclick=\"submitForm('frmAdd" + data + "');return false;\""
                                 + " data-dojo-type=\"dijit.form.CheckBox\"" + select + "></td>");
@@ -257,106 +256,6 @@ public class PeriodsManager extends GenericResource {
                 out.println("    </button>");
                 out.println("  </fieldset>");
                 out.println("</div>");
-                out.println("</form>");
-            } else {
-                out.println("<p>" + paramRequest.getLocaleString("periodsEstablished") + "</p>");
-            }
-            if (request.getParameter("statusMsg") != null
-                    && !request.getParameter("statusMsg").isEmpty()) {
-                out.println("<div dojoType=\"dojox.layout.ContentPane\">");
-                out.println("<script type=\"dojo/method\">");
-                out.println("showStatus('" + request.getParameter("statusMsg") + "');\n");
-                out.println("</script>\n");
-                out.println("</div>");
-            }
-        }
-    }
-
-    public void doViewAnterior(HttpServletRequest request, HttpServletResponse response,
-            SWBParamRequest paramRequest) throws SWBResourceException, IOException {
-        PrintWriter out = response.getWriter();
-        String suri = request.getParameter("suri");
-        SemanticObject semObj = SemanticObject.getSemanticObject(suri);
-        User user = paramRequest.getUser();
-        out.println("<script type=\"text/javascript\">\n");
-        out.println("  dojo.require('dojo.parser');\n");
-        out.println("  dojo.require('dijit.layout.ContentPane');\n");
-        out.println("  dojo.require('dijit.form.Form');\n");
-        out.println("  dojo.require('dijit.form.CheckBox');\n");
-        out.println("</script>\n");
-
-        if (semObj != null) {
-            BSC bsc = (BSC) semObj.getModel().getModelObject().createGenericInstance();
-            Iterator<Period> itObj = new GenericIterator<Period>(
-                    semObj.listObjectProperties(Seasonable.bsc_hasPeriod));
-            List objetivesCurrent = new ArrayList();
-            Iterator<Period> itPeriods = null;
-            GenericObject genericObject = semObj.createGenericInstance();
-//if(genericObject instanceof Seasonable) {
-//System.out.println("semObj "+semObj+" es seasonable");
-//}else {
-//System.out.println("semObj "+semObj+" NO es seasonable");
-//}
-            SWBResourceURL url = paramRequest.getActionUrl().setAction(SWBResourceURL.Action_ADD);
-
-            while (itObj.hasNext()) {
-                Period periodSave = itObj.next();
-
-                objetivesCurrent.add(periodSave);
-            }
-            /*if (genericObject instanceof Objective) {
-                itPeriods = Period.ClassMgr.listPeriods(bsc);
-            } else*/ 
-            if (genericObject instanceof Indicator) {
-                SemanticObject objParent = semObj.getObjectProperty(Indicator.bsc_objectiveInv);
-
-                itPeriods = new GenericIterator<Period>(
-                        objParent.listObjectProperties(Seasonable.bsc_hasPeriod));
-            } else {
-                itPeriods = Period.ClassMgr.listPeriods(bsc);
-            }
-            
-            if (itPeriods != null && itPeriods.hasNext()) {
-                String data = semObj.getSemanticClass().getName() + semObj.getId();
-
-                out.println("<form method=\"post\" id=\"frmAdd" + data + "\" action=\" " + url
-                        + "\" class=\"swbform\" type=\"dijit.form.Form\" onsubmit=\""
-                        + "submitForm('frmAdd" + data + "');return false;\">");
-                out.println("<input type=\"hidden\" name=\"suri\" value=\"" + semObj.getURI()
-                        + "\">");
-                out.println("<input type=\"hidden\" name=\"ws\" value=\"" + bsc.getId() + "\">");
-                out.println("<fieldset>\n");
-                out.println("<label>" + paramRequest.getLocaleString("selectPeriods") + "</label>");
-                out.println("<ul>");
-                
-                Seasonable seasonable = (Seasonable)semObj.createGenericInstance();
-                
-                while (itPeriods.hasNext()) {
-                    Period period = itPeriods.next();
-
-//                    if (period.isActive() && user.haveAccess(period)) {
-                    if (  (period.isActive() && user.haveAccess(period)) || (!period.isActive() && seasonable.hasPeriod(period) && user.haveAccess(period))  ) {
-                        String select = "";
-                        String title = period.getTitle(user.getLanguage()) == null ? period.getTitle()
-                                : period.getTitle(user.getLanguage());
-
-                        if (objetivesCurrent.contains(period)) {
-                            select = "checked";
-                        }
-                        out.println("<li><input id=\"" + period.getId() + "\" name=\"period" + data
-                                + "\" type=\"checkbox\" value=\"" + period.getId() + "\" "
-                                + " data-dojo-type=\"dijit.form.CheckBox\" " + select + ">"
-                                + "<label for=\"" + period.getId() + "\">" + title
-                                + "</label></li>");
-                    }
-                }
-                out.println("</ul>");
-                out.println("</fieldset>\n");
-                out.println("<button dojoType=\"dijit.form.Button\" type=\"submit\">"
-                        + paramRequest.getLocaleString("save") + "</button>");
-                out.println("<button dojoType=\"dijit.form.Button\" type=\"button\" "
-                        + "onClick=\"reloadTab('" + semObj.getURI() + "');\">"
-                        + paramRequest.getLocaleString("cancel") + "</button>");
                 out.println("</form>");
             } else {
                 out.println("<p>" + paramRequest.getLocaleString("periodsEstablished") + "</p>");
