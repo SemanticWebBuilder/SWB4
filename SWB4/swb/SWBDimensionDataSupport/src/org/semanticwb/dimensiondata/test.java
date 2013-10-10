aa38f09a924debdf27c30957db009250ae458cb7
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2013-10-10 21:42:38 +0000

    DimensionData test

---
+++ SWB4/swb/SWBDimensionDataSupport/src/org/semanticwb/dimensiondata/test.java
@@ -0,0 +1,81 @@
+/*
+ * SemanticWebBuilder es una plataforma para el desarrollo de portales y aplicaciones de integración,
+ * colaboración y conocimiento, que gracias al uso de tecnología semántica puede generar contextos de
+ * información alrededor de algún tema de interés o bien integrar información y aplicaciones de diferentes
+ * fuentes, donde a la información se le asigna un significado, de forma que pueda ser interpretada y
+ * procesada por personas y/o sistemas, es una creación original del Fondo de Información y Documentación
+ * para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite.
+ * 
+ * INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público (‘open source’),
+ * en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC lo ha diseñado y puesto a su disposición;
+ * aprender de él; distribuirlo a terceros; acceder a su código fuente y modificarlo, y combinarlo o enlazarlo con otro software,
+ * todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización
+ * del SemanticWebBuilder 4.0.
+ * 
+ * INFOTEC no otorga garantía sobre SemanticWebBuilder, de ninguna especie y naturaleza, ni implícita ni explícita,
+ * siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los riesgos que puedan derivar
+ * de la misma.
+ * 
+ * Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposición la siguiente
+ * dirección electrónica:
+ * http://www.semanticwebbuilder.org
+ */
+package org.semanticwb.dimensiondata;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+import javax.net.ssl.HttpsURLConnection;
+import sun.misc.BASE64Encoder;
+
+/**
+ *
+ * @author serch
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+//        URL url = new URL("https://api.opsourcecloud.net/oec/0.9/myaccount");
+//        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+//        connection.setRequestMethod("GET");
+        BASE64Encoder enc = new sun.misc.BASE64Encoder();
+      String userpassword = "ss_api_user" + ":" + "4rfv5tgb";
+      String encodedAuthorization = enc.encode( userpassword.getBytes() );
+        System.out.println("resutl:"+getRESTget("https://api.opsourcecloud.net/oec/0.9/myaccount", encodedAuthorization));
+//      connection.setRequestProperty("Authorization", "Basic "+
+//            encodedAuthorization);
+//      connection.connect();
+//        Reader stream = new InputStreamReader(connection.getInputStream());
+//        BufferedReader reader =  new BufferedReader(stream);
+//        String line;
+//        while ((line=reader.readLine())!=null){
+//            System.out.println("line:"+line);
+//        }
+//        reader.close();
+    }
+    
+    public static String getRESTget(String url, String encodedAuth) throws IOException{
+        String ret = "";
+        URL lurl = new URL(url);
+        HttpsURLConnection connection = (HttpsURLConnection)lurl.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Authorization", "Basic "+encodedAuth);
+        connection.connect();
+        BufferedInputStream bis = new BufferedInputStream(connection.getInputStream());
+        byte [] buff = new byte[8192];
+        int length;
+        while ((length=bis.read(buff))>0){
+            ret = ret + new String(buff,0,length);
+        }
+        bis.close();
+        return ret;
+    }
+}
