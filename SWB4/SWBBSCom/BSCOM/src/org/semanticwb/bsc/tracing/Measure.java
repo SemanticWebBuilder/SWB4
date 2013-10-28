1f4e673b3fbd965e6ef1469223bf666a409abfba
cramos <cramos@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-10-28 18:58:18 +0000

    Evaluación de objetivo

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/tracing/Measure.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/tracing/Measure.java
@@ -1,10 +1,35 @@
 package org.semanticwb.bsc.tracing;
 
 import java.util.Iterator;
+import org.semanticwb.bsc.accessory.Period;
+import org.semanticwb.platform.SemanticObject;
+import org.semanticwb.platform.SemanticObserver;
 
 
 public class Measure extends org.semanticwb.bsc.tracing.base.MeasureBase 
 {
+    static {
+        bsc_value.registerObserver(new SemanticObserver() {
+            @Override
+            public void notify(SemanticObject obj, Object prop, String lang, String action)
+            {
+                if("SET".equalsIgnoreCase(action)) {
+                    Measure m = (Measure)obj.getGenericInstance();
+                    Period p = m.getEvaluation().getPeriod();
+                    Iterator<Series> serieses = m.getSeries().getIndicator().listValidSerieses().iterator();
+                    while(serieses.hasNext()) {
+                        Series s = serieses.next();
+                        if(s.equals(m.getSeries())) {
+                            continue;
+                        }
+                        Measure measure = s.getMeasure(p);
+                        measure.evaluate();
+                    }
+                }
+            }
+        });
+    }
+    
     public Measure(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
