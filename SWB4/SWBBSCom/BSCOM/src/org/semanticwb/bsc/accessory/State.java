1929c3e856c6cc7990404995e313305d5f8b39e0
cramos <ciramosi@users.noreply.github.com>
2013-10-28 18:45:19 +0000

    Validación por nulos en compareTo

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/accessory/State.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/accessory/State.java
@@ -19,6 +19,9 @@ public class State extends org.semanticwb.bsc.accessory.base.StateBase implement
     
     @Override
     public int compareTo(State anotherState) {
+        if(anotherState==null) {
+            return 1;
+        }
         int compare = 0;
         compare = getOrden() > anotherState.getOrden() ? 1 : -1;
         return compare;
