abe58094abef5d0bc82172cc544841187dbc21d4
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-10-30 01:00:51 +0000

    Asignación de interface Updateable para Objective, Indicator e Initiative.

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/base/UpdateableBase.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/base/UpdateableBase.java
@@ -5,17 +5,12 @@ package org.semanticwb.bsc.base;
    */
 public interface UpdateableBase extends org.semanticwb.model.GenericObject
 {
-    public static final org.semanticwb.platform.SemanticProperty bsc_percentageProgress=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.semanticwebbuilder.org/swb4/bsc#percentageProgress");
     public static final org.semanticwb.platform.SemanticProperty bsc_recommendations=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.semanticwebbuilder.org/swb4/bsc#recommendations");
     public static final org.semanticwb.platform.SemanticProperty bsc_analysis=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.semanticwebbuilder.org/swb4/bsc#analysis");
    /**
    * Cualquier elemento BSC que se pueda actualizar periodicamente 
    */
     public static final org.semanticwb.platform.SemanticClass bsc_Updateable=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/bsc#Updateable");
-
-    public float getPercentageProgress();
-
-    public void setPercentageProgress(float value);
 
     public String getRecommendations();
 
