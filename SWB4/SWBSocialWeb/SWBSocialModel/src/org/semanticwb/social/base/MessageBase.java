b9e3dd5de8fbad55c2333d5660714ae4a083d3e8
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-11-05 00:32:40 +0000

    Cambios para ordenamientos, querys sparql

--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/MessageBase.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/MessageBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.social.base;
    /**
    * Clase que sera creada cuando un post sea de tipo mensaje 
    */
-public abstract class MessageBase extends org.semanticwb.social.PostOut implements org.semanticwb.model.CalendarRefable,org.semanticwb.model.Tagable,org.semanticwb.social.PostDataable,org.semanticwb.model.Referensable,org.semanticwb.model.Traceable,org.semanticwb.social.PostTextable
+public abstract class MessageBase extends org.semanticwb.social.PostOut implements org.semanticwb.model.Referensable,org.semanticwb.social.PostDataable,org.semanticwb.model.Tagable,org.semanticwb.social.PostTextable,org.semanticwb.model.CalendarRefable,org.semanticwb.model.Traceable
 {
    /**
    * Clase que sera creada cuando un post sea de tipo mensaje
