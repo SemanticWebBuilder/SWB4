6ca6cd030a2b264d7266a6fea696ed7dad640d1c
redentor <JRedentor@users.noreply.github.com>
2013-11-14 16:43:18 +0000

    Cambio de periodo por petición con Ajax, para no cambiar la url del navegador.

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/resources/PeriodSelector.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/resources/PeriodSelector.java
@@ -1,5 +1,6 @@
 package org.semanticwb.bsc.resources;
 
+import com.hp.hpl.jena.graph.query.Element;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.GregorianCalendar;
@@ -31,13 +32,15 @@ public class PeriodSelector extends GenericResource {
         User user = SWBContext.getSessionUser();
         GregorianCalendar left;
         GregorianCalendar right;        
-        GregorianCalendar current = new GregorianCalendar(TimeZone.getDefault(), new Locale(user.getLanguage()==null?"es":user.getLanguage(), user.getCountry()==null?"MX":user.getCountry()));
-        for(Period period:periods) {
+        GregorianCalendar current = new GregorianCalendar(TimeZone.getDefault(),
+                new Locale(user.getLanguage() == null ? "es" : user.getLanguage(),
+                user.getCountry() == null ? "MX" : user.getCountry()));
+        for (Period period : periods) {
             left = new GregorianCalendar();
             left.setTime(period.getStart());
             right = new GregorianCalendar();
             right.setTime(period.getEnd());
-            if(current.compareTo(left)>=0 && current.compareTo(right)<=0) {
+            if (current.compareTo(left) >= 0 && current.compareTo(right) <= 0) {
                 return period;
             }
         }
@@ -50,7 +53,7 @@ public class PeriodSelector extends GenericResource {
         
         User user = paramRequest.getUser();
         //Validar usuario autorizado
-        if(!user.isSigned()) {
+        if (!user.isSigned()) {
             response.flushBuffer();
         }
         
@@ -59,45 +62,53 @@ public class PeriodSelector extends GenericResource {
         //Obtener el Scorecard actual
         BSC currentBsc = (BSC) paramRequest.getWebPage().getWebSite();
         
-        //Obtener listado de instancias de Scorecards
+        //Obtener listado de instancias de Periodos en el Scorecard
         Iterator<Period> allPeriods = Period.ClassMgr.listPeriods(currentBsc);
         List<Period> periods = SWBUtils.Collections.filterIterator(allPeriods, new GenericFilterRule<Period>() {
-                                                        @Override
-                                                        public boolean filter(Period s) {
-                                                            User user = SWBContext.getSessionUser();
-                                                            return !s.isValid() || !user.haveAccess(s);
-                                                        }            
-                                                    }
-                );
+                @Override
+                public boolean filter(Period s) {
+                    User user = SWBContext.getSessionUser();
+                    return !s.isValid() || !user.haveAccess(s);
+                }
+            });
         
         
         //Obtener el Periodo actual
         String periodId = (String) request.getSession().getAttribute(currentBsc.getId());
-//Verificar que exista el periodo válido en sesión. Sino existe, ponerlo
-Period nearestPeriod = null;
-if(!Period.ClassMgr.hasPeriod(periodId, currentBsc)) {
-    nearestPeriod = getNearestPeriod(periods);
-    if(nearestPeriod != null) {
-        periodId = nearestPeriod.getId();
-        request.getSession(true).setAttribute(currentBsc.getId(), nearestPeriod.getId());
-    }
-}else {
-    nearestPeriod = Period.ClassMgr.getPeriod(periodId, currentBsc);
-}
+        //Verificar que exista el periodo válido en sesión. Si no existe, ponerlo
+        Period nearestPeriod = null;
+        if (!Period.ClassMgr.hasPeriod(periodId, currentBsc)) {
+            nearestPeriod = getNearestPeriod(periods);
+            if (nearestPeriod != null) {
+                periodId = nearestPeriod.getId();
+                request.getSession(true).setAttribute(currentBsc.getId(), nearestPeriod.getId());
+            }
+        } else {
+            nearestPeriod = Period.ClassMgr.getPeriod(periodId, currentBsc);
+        }
 
-//        Period currentPeriod = Period.ClassMgr.getPeriod(periodId, currentBsc);
-        
         int periodCount = 0;
-        
         String actionUrl = paramRequest.getActionUrl().setAction("setPeriod").toString();
-        System.out.println("PeriodoSesion: " + ((String) request.getSession().getAttribute(currentBsc.getId())));
         
         //funcion de javascript para mandar la forma que contiene el select
         output.append("<script type=\"text/javascript\">\n");
         output.append("  function settingPeriod(element) {\n");
         output.append("      if (element && element[element.selectedIndex].value != \"\") {\n");
-        output.append("	       element.form.submit();\n");
-//        output.append("	       location.reload(true);\n");
+        output.append("        var urlToGo = element.form.action;\n");
+        output.append("        urlToGo += ('?periodId=' + element[element.selectedIndex].value);\n");
+        output.append("        var xmlhttp;\n");
+        output.append("        if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari\n");
+        output.append("          xmlhttp=new XMLHttpRequest();\n");
+        output.append("        } else {// code for IE6, IE5\n");
+        output.append("          xmlhttp=new ActiveXObject(\"Microsoft.XMLHTTP\");\n");
+        output.append("        }\n");
+        output.append("        xmlhttp.onreadystatechange=function() {\n");
+        output.append("          if (xmlhttp.readyState==4 && xmlhttp.status==200) {\n");
+        output.append("            location.reload(true);\n");
+        output.append("          }\n");
+        output.append("        }\n");
+        output.append("        xmlhttp.open(\"GET\", urlToGo, true);\n");
+        output.append("        xmlhttp.send();\n");
         output.append("      }\n");
         output.append("  }\n");
         output.append("</script>\n");
@@ -106,16 +117,18 @@ if(!Period.ClassMgr.hasPeriod(periodId, currentBsc)) {
         output.append("  <select name=\"periodId\" onChange=\"settingPeriod(this);\">\n");
         if (!periods.isEmpty()) {
         //Recorrer el listado de Scorecards
-//            while (allPeriods.hasNext()) {
             for(Period nextPeriod:periods) {
-//                Period nextPeriod = (Period) allPeriods.next();
-//                if (nextPeriod.isValid() && user.haveAccess(nextPeriod)) {
-                    periodCount++;
-                    String optionValue = nextPeriod != nearestPeriod ? nextPeriod.getId() : "";
-                    output.append("    <option value=\"" + optionValue + "\"" + (nextPeriod != nearestPeriod ? "" : " selected=\"selected\"") + ">" + nextPeriod.getTitle() + "</option>\n");
-//                }
+                periodCount++;
+                String optionValue = nextPeriod != nearestPeriod ? nextPeriod.getId() : "";
+                output.append("    <option value=\"");
+                output.append(optionValue);
+                output.append("\"");
+                output.append((nextPeriod != nearestPeriod ? "" : " selected=\"selected\""));
+                output.append(">");
+                output.append(nextPeriod.getTitle());
+                output.append("</option>\n");
             }
-        }else {
+        } else {
             output.append("    <option value=\"\">No hay otros Per&iacute;odos</option>\n");
         }
         output.append("  </select>\n");
