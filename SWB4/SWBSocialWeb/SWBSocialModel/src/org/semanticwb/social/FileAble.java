b7708eaea243cfd132517112c81ec65430c7233d
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-12-03 23:50:31 +0000

    Cambios para ordenamientos, querys sparql

---
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/FileAble.java
@@ -0,0 +1,8 @@
+package org.semanticwb.social;
+
+   /**
+   * Interface en la que se define un elemento de tipo file, que funciona para subir archivos al servidor. 
+   */
+public interface FileAble extends org.semanticwb.social.base.FileAbleBase
+{
+}
