01433aa3de484330d745910da725b1a01a9dc0b1
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-12-11 00:56:48 +0000

    Cambios para parseo de urls

--- SWB4/SWBSocialWeb/SWBSocialResources/src/org/semanticwb/social/admin/resources/StreamMap.java
+++ SWB4/SWBSocialWeb/SWBSocialResources/src/org/semanticwb/social/admin/resources/StreamMap.java
@@ -13,10 +13,9 @@ import org.semanticwb.Logger;
 import org.semanticwb.SWBPlatform;
 import org.semanticwb.SWBUtils;
 import org.semanticwb.platform.SemanticObject;
-import org.semanticwb.portal.api.GenericAdmResource;
+import org.semanticwb.portal.api.GenericResource;
 import org.semanticwb.portal.api.SWBParamRequest;
 import org.semanticwb.portal.api.SWBResourceException;
-import org.semanticwb.portal.api.SWBResourceURL;
 import org.semanticwb.social.PostIn;
 import org.semanticwb.social.SocialNetworkUser;
 
@@ -24,7 +23,7 @@ import org.semanticwb.social.SocialNetworkUser;
  *
  * @author jorge.jimenez
  */
-public class StreamMap extends GenericAdmResource{
+public class StreamMap extends GenericResource{
     
     private static Logger log = SWBUtils.getLogger(StreamMap.class);
     public static final String Mode_SHOWMARKERDETAILS = "showMarkDet";
@@ -49,6 +48,11 @@ public class StreamMap extends GenericAdmResource{
         PrintWriter out = response.getWriter();
         String suri=request.getParameter("suri");
         SemanticObject semObj=SemanticObject.getSemanticObject(suri);
+        String streamMapView="";
+        if(request.getParameter("streamMapView")!=null) streamMapView=request.getParameter("streamMapView");
+        String date=request.getParameter("mapSinceDate");
+        
+        out.println("<div class=\"divMap\">");
         out.println("<div class=\"swbform\">");
         out.println("<form type=\"dijit.form.Form\" id=\"showMap_"+semObj.getId()+"\" action=\"" +  paramRequest.getRenderUrl().setParameter("suri", suri) + "\" method=\"post\" onsubmit=\"submitForm('showMap_" + semObj.getId() + "'); return false;\">");            
         out.println("<table width=\"100%\" border=\"0px\">");            
@@ -56,18 +60,65 @@ public class StreamMap extends GenericAdmResource{
         out.println("       <td style=\"text-align: center;\">Mostrar mapa con datos desde:</td>");        
         out.println("   </tr>");
         out.println("   <tr>");
-        out.println("       <td style=\"text-align: center;\">");
-        out.println("           <input type=\"text\" name=\"mapSinceDate\" id=\"mapSinceDate"+semObj.getId()+"\" dojoType=\"dijit.form.DateTextBox\"  size=\"11\" style=\"width:110px;\" hasDownArrow=\"true\">");
-        out.println("           <button dojoType=\"dijit.form.Button\" type=\"submit\">Mostrar</button>");
-        out.println("       </td>");
+        out.println("    <td style=\"text-align: center;\">");
+        out.println("      <input type=\"text\" name=\"mapSinceDate\" id=\"mapSinceDate"+semObj.getId()+"\" dojoType=\"dijit.form.DateTextBox\"  value=\""+date+"\" size=\"11\" style=\"width:110px;\" hasDownArrow=\"true\">");
+        out.println("    </td>");
+        out.println("   </tr>");
+        out.println("   <tr>");
+        out.println("    <td>");
+        out.println("      Solo Contadores<input type=\"radio\" id=\"usedas0\" name=\"streamMapView\" label=\"Solo Contadores:\" value=\"1\"");
+        if(streamMapView.equals("1")) out.println(" checked=\"true\"");
+        out.println(" />");
+        out.println("    </td>");
+        out.println("   </tr>");
+        out.println("   <tr>");
+        out.println("    <td>");
+        out.println("      Solo Mensajes<input type=\"radio\" id=\"usedas1\" name=\"streamMapView\" label=\"Solo Mensajes:\" value=\"2\" ");
+        if(streamMapView.equals("2")) out.println(" checked=\"true\"");
+        out.println(" />");
+        out.println("    </td>");
+        out.println("   </tr>");
+        out.println("   <tr>");
+        out.println("    <td>");
+        out.println("      Mensajes y Contadores<input type=\"radio\" id=\"usedas2\" name=\"streamMapView\" label=\"Mensajes y Contadores:\" value=\"3\"");
+        if(streamMapView.equals("3")) out.println(" checked=\"true\"");
+        out.println(" />");
+        out.println("    </td>");
+        out.println("   </tr>");
+        out.println("   <tr>");
+        out.println("    <td>");
+        out.println("      Mostrar solo positivos<input type=\"radio\" id=\"usedas4\" name=\"streamMapView\" label=\"Mostrar solo positivos:\" value=\"5\"");
+        if(streamMapView.equals("5")) out.println(" checked=\"true\"");
+        out.println(" />");
+        out.println("    </td>");
+        out.println("   </tr>");
+        out.println("   <tr>");
+        out.println("    <td>");
+        out.println("      Mostrar solo negativos<input type=\"radio\" id=\"usedas5\" name=\"streamMapView\" label=\"Mostrar solo negativos:\" value=\"6\"");
+        if(streamMapView.equals("6")) out.println(" checked=\"true\"");
+        out.println(" />");
+        out.println("    </td>");
+        out.println("   </tr>");
+        out.println("   <tr>");
+        out.println("    <td>");
+        out.println("      Mostrar todo<input type=\"radio\" id=\"usedas3\" name=\"streamMapView\" label=\"Mostrar todo:\" value=\"4\"");
+        if(streamMapView.equals("4")) out.println(" checked=\"true\"");
+        out.println(" />");
+        out.println("    </td>");
+        out.println("   </tr>");
+        out.println("   <tr>");
+        out.println("    <td style=\"text-align: center;\">");
+        out.println("      <button dojoType=\"dijit.form.Button\" type=\"submit\">Mostrar</button>");
+        out.println("    </td>");
         out.println("   </tr>");
         out.println("</table>");
         out.println("</form>");
         out.println("</div>");
+        out.println("</div>");
         if(request.getParameter("mapSinceDate")!=null)
         {
             out.println("<div class=\"swbSocialMapIframe\">");
-            out.println("   <iframe width=\"100%\" height=\"100%\" src=\""+paramRequest.getRenderUrl().setMode(Mode_showMap).setParameter("suri", request.getParameter("suri")).setParameter("mapSinceDate"+semObj.getId(), request.getParameter("mapSinceDate")) +"\"></iframe> ");
+            out.println("   <iframe width=\"100%\" height=\"100%\" src=\""+paramRequest.getRenderUrl().setMode(Mode_showMap).setParameter("suri", request.getParameter("suri")).setParameter("mapSinceDate"+semObj.getId(), request.getParameter("mapSinceDate")).setParameter("streamMapView", request.getParameter("streamMapView")) +"\"></iframe> ");
             out.println("</div>");
         }
     }
@@ -106,7 +157,7 @@ public class StreamMap extends GenericAdmResource{
                if (dis != null) {
                    try {
                        request.setAttribute("swbSocialUser", swbSocialUser.getSemanticObject());
-                       System.out.println("Map/doShowDetails:"+request.getParameter("suri"));
+                       //System.out.println("Map/doShowDetails:"+request.getParameter("suri"));
                        request.setAttribute("suri", request.getParameter("suri"));
                        request.setAttribute("paramRequest", paramRequest);
                        dis.include(request, response);
