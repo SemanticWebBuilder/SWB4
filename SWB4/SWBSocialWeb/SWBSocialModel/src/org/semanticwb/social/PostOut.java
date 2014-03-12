f2b4fa925753330725ea311e6081c17088b75c59
francisco <francisco@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-03-12 18:54:37 +0000

    Check if some post of the timelines has been sent from SWBSocial

--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/PostOut.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/PostOut.java
@@ -1,6 +1,7 @@
 package org.semanticwb.social;
 
 import java.util.Iterator;
+import org.semanticwb.model.SWBModel;
 import org.semanticwb.platform.SemanticObject;
 import org.semanticwb.platform.SemanticObserver;
 
@@ -11,5 +12,25 @@ public class PostOut extends org.semanticwb.social.base.PostOutBase
     {
         super(base);
     }
-    
+
+    public static PostOut getPostOutbySocialMsgId(SWBModel model, String socialMsgId)
+    {
+        //System.out.println("PostIn/getPostInbySocialMsgId-LLega:"+socialMsgId);
+        Iterator<SemanticObject> it=model.getSemanticModel().listSubjects(PostOutNet.social_po_socialNetMsgID, socialMsgId);
+        while(it.hasNext())
+        {
+            SemanticObject obj=it.next();
+            //System.out.println("objJorge:"+obj);
+            //System.out.println("objJorge GenInst:"+obj.createGenericInstance());
+            if(obj.createGenericInstance() instanceof PostOutNet)
+            {
+                PostOutNet postOutNet=(PostOutNet)obj.createGenericInstance();
+                //System.out.println("postOutNetGeoge:"+postOutNet.getPo_socialNetMsgID());
+                //System.out.println("postOutGeoge:"+postOutNet.getSocialPost());
+                //return (PostIn)obj.createGenericInstance();
+                return postOutNet.getSocialPost();
+            }
+        }
+        return null;
+    }    
 }
