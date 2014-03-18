032d6f07f74c14a0c69ae295257271e42439f816
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-03-18 22:28:26 +0000

    Agregada validación para cuando los datos de los pies vengan en ceros (workaround a bug de d3)

--- SWB4/swbp/web/swbadmin/jsp/process/taskInbox/userTaskInboxUtils.js
+++ SWB4/swbp/web/swbadmin/jsp/process/taskInbox/userTaskInboxUtils.js
@@ -25,130 +25,131 @@ function updateChart(chartContainer, title, data) {
         radius,
         offset = 20,
         outerradius;
-
-    var pie = d3.layout.pie()
-        .sort(null)
-        .value(function(d) {
-            return d.value; 
-        });
         
     var sum = d3.sum(data, function(d){
         return d.value;
     });
-
-    height = width = $(chartContainer).parent().width();
-    radius = (height-(height*.2)) / 2;
-    outerradius = radius * 0.08;
     
-    //replace container content
-    $(chartContainer).html("<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'></svg>");
-    
-    //create arcs function
-    var arc = d3.svg.arc()
-        .outerRadius(radius - outerradius)
-        .innerRadius(0);
-
-    //create outter arcs function
-    var arcOver = d3.svg.arc()
-        .outerRadius(radius - outerradius)
-        .innerRadius(radius);
-
-    //Add graph contents
-    var svg = d3.select(chartContainer+" svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + (height / 2 + offset)+")");
-
-    //Outter arcs selector
-    var gl = svg.selectAll(".arcOver")
-        .data(pie(data))
-        .enter().append("g")
-        .attr("class", "arcOver")
-        .style("visibility","hidden");
-
-    gl.append("path")
-        .attr("d", arcOver)
-        .style("fill-opacity", "0.6")
-        .style("fill", function(d, i) {
-            return color(i);
-        });
-
-    //Create tooltips
-    var tooltips = svg.select("body")
-        .data(pie(data))
-        .enter().append("div")
-        .attr("class","chartToolTip")
-        .style("display", "none")
-        .style("position", "absolute")
-        .style("z-index", "10");
-
-    tooltips.append("p")
-        .append("span")
-        .html(function(d) {
-            var pc = ((d.data.value / sum) *100);
-            if (pc % 1 > 0) {
-                pc = pc.toFixed(3);
-            }
-            return "<strong>"+d.data.label+"</strong><br>"+d.data.value+" ("+pc+"%)";
-        });
-
-    //Arcs selector
-    var g = svg.selectAll(".arc")
-        .data(pie(data))
-        .enter().append("g")
-        .attr("class", "arc")
-        .on("mouseover", function(d, i) {
-            d3.select(gl[0][i]).style("visibility","visible"); 
-            d3.select(tooltips[0][i])
-                .style("display","block");
-        })
-        .on("mouseout", function(d, i) {
-            d3.select(gl[0][i]).style("visibility","hidden"); 
-            d3.select(tooltips[0][i])
-                .style("display","none");
-            d3.select(gl[0][i]).style("fill",function(d) {
-                return color(d.data.label);
+    if (sum > 0) {
+        var pie = d3.layout.pie()
+            .sort(null)
+            .value(function(d) {
+                console.log(d.value);
+                return d.value; 
             });
-        })
-        .on("mousemove", function(d, i) {
-            d3.select(tooltips[0][i])
-                .style("top", d3.event.pageY-10+"px")
-                .style("left", d3.event.pageX+10+"px")
-        });
 
-    //Create slices
-    g.append("path")
-        .attr("d", arc)
-        .style("stroke", "white")
-        .style("stroke-width", "2")
-        .style("fill", function(d, i) {
-            return color(i);
-        });
+        height = width = $(chartContainer).parent().width();
+        radius = (height-(height*.2)) / 2;
+        outerradius = radius * 0.08;
 
-    svg
-        .append("text")
-        .text(title)
-        .style("text-anchor","middle")
-        .style("fill","black")
-        .style("font-size","10pt")
-        .style("font-weight","bold")
-        .attr("x","0")
-        .attr("y",function(d) {
-            return - width/2;
-        });
+        //replace container content
+        $(chartContainer).html("<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'></svg>");
 
-    /*g.append("text")
-        .attr("transform", function(d) {
-            return "translate(" + arc.centroid(d) + ")";
-        })
-        .attr("dy", ".35em")
-        .style("text-anchor", "middle")
-        .style("fill", "white")
-        .style("font-size", "12pt")
-        .text(function(d) {
-            return d.data.label;
-        });*/
+        //create arcs function
+        var arc = d3.svg.arc()
+            .outerRadius(radius - outerradius)
+            .innerRadius(0);
+
+        //create outter arcs function
+        var arcOver = d3.svg.arc()
+            .outerRadius(radius - outerradius)
+            .innerRadius(radius);
+
+        //Add graph contents
+        var svg = d3.select(chartContainer+" svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + (height / 2 + offset)+")");
+
+        //Outter arcs selector
+        var gl = svg.selectAll(".arcOver")
+            .data(pie(data))
+            .enter().append("g")
+            .attr("class", "arcOver")
+            .style("visibility","hidden");
+
+            gl.append("path")
+            .attr("d", arcOver)
+            .style("fill-opacity", "0.6")
+            .style("fill", function(d, i) {
+                return color(i);
+            });
+
+        //Create tooltips
+        var tooltips = svg.select("body")
+            .data(pie(data))
+            .enter().append("div")
+            .attr("class","chartToolTip")
+            .style("display", "none")
+            .style("position", "absolute")
+            .style("z-index", "10");
+
+        tooltips.append("p")
+            .append("span")
+            .html(function(d) {
+                var pc = ((d.data.value / sum) *100);
+                if (pc % 1 > 0) pc = pc.toFixed(3);
+                return "<strong>"+d.data.label+"</strong><br>"+d.data.value+" ("+pc+"%)";
+            });
+
+        //Arcs selector
+        var g = svg.selectAll(".arc")
+            .data(pie(data))
+            .enter().append("g")
+            .attr("class", "arc")
+            .on("mouseover", function(d, i) {
+                d3.select(gl[0][i]).style("visibility","visible"); 
+                d3.select(tooltips[0][i])
+                    .style("display","block");
+            })
+            .on("mouseout", function(d, i) {
+                d3.select(gl[0][i]).style("visibility","hidden"); 
+                d3.select(tooltips[0][i])
+                    .style("display","none");
+                d3.select(gl[0][i]).style("fill",function(d) {
+                    return color(d.data.label);
+                });
+            })
+            .on("mousemove", function(d, i) {
+                d3.select(tooltips[0][i])
+                    .style("top", d3.event.pageY-10+"px")
+                    .style("left", d3.event.pageX+10+"px")
+            });
+
+        //Create slices
+        g.append("path")
+            .attr("d", arc)
+            .style("stroke", "white")
+            .style("stroke-width", "2")
+            .style("fill", function(d, i) {
+                return color(i);
+            });
+
+        svg
+            .append("text")
+            .text(title)
+            .style("text-anchor","middle")
+            .style("fill","black")
+            .style("font-size","10pt")
+            .style("font-weight","bold")
+            .attr("x","0")
+            .attr("y",function(d) {
+                return - width/2;
+            });
+
+        /*g.append("text")
+            .attr("transform", function(d) {
+                return "translate(" + arc.centroid(d) + ")";
+            })
+            .attr("dy", ".35em")
+            .style("text-anchor", "middle")
+            .style("fill", "white")
+            .style("font-size", "12pt")
+            .text(function(d) {
+                return d.data.label;
+            });*/
+    }
 }
 
 function showWaitDialog(title, loadmsg) {
