d6f9d94c27e715e2ffc1dd6239072c1b0c571fdb
martha <martha@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-03-28 01:04:40 +0000

    Recurso Administración de Riesgos(dependencia)

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/formelement/ElementBSCRelated.java
@@ -0,0 +1,10 @@
+package org.semanticwb.bsc.formelement;
+
+
+public class ElementBSCRelated extends org.semanticwb.bsc.formelement.base.ElementBSCRelatedBase 
+{
+    public ElementBSCRelated(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
