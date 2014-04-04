1414cd4801a78f7ef56d54ff5710709cde20c234
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-04-04 17:23:24 +0000

    Adición de dos propiedades inversas una a Objetivo y otra a Iniciativa

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/base/ObjectiveBase.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/base/ObjectiveBase.java
@@ -1,7 +1,7 @@
 package org.semanticwb.bsc.element.base;
 
 
-public abstract class ObjectiveBase extends org.semanticwb.bsc.element.BSCElement implements org.semanticwb.bsc.Sortable,org.semanticwb.bsc.Preference,org.semanticwb.model.Referensable,org.semanticwb.bsc.PeriodStatusAssignable,org.semanticwb.model.Traceable,org.semanticwb.bsc.FixedMeasurable,org.semanticwb.bsc.Help,org.semanticwb.bsc.Causal,org.semanticwb.model.UserGroupable,org.semanticwb.bsc.Serializable,org.semanticwb.bsc.Status,org.semanticwb.model.FilterableClass,org.semanticwb.model.Descriptiveable,org.semanticwb.bsc.Updateable,org.semanticwb.bsc.Seasonable,org.semanticwb.model.FilterableNode,org.semanticwb.model.RuleRefable,org.semanticwb.model.Activeable,org.semanticwb.model.Roleable,org.semanticwb.bsc.Recognizable,org.semanticwb.model.Filterable
+public abstract class ObjectiveBase extends org.semanticwb.bsc.element.BSCElement implements org.semanticwb.model.Roleable,org.semanticwb.model.FilterableClass,org.semanticwb.bsc.FixedMeasurable,org.semanticwb.bsc.Help,org.semanticwb.model.Traceable,org.semanticwb.bsc.Recognizable,org.semanticwb.bsc.Sortable,org.semanticwb.bsc.PeriodStatusAssignable,org.semanticwb.model.UserGroupable,org.semanticwb.model.RuleRefable,org.semanticwb.model.Descriptiveable,org.semanticwb.bsc.Seasonable,org.semanticwb.bsc.Preference,org.semanticwb.bsc.Causal,org.semanticwb.model.Referensable,org.semanticwb.bsc.Status,org.semanticwb.bsc.Updateable,org.semanticwb.model.Activeable,org.semanticwb.model.FilterableNode,org.semanticwb.bsc.Serializable,org.semanticwb.model.Filterable
 {
    /**
    * Persiste los atributos de un indicador
