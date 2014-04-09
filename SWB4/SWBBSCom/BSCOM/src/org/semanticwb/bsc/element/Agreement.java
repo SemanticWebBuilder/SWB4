5eed9e5e1c19cb7dc44ef5700a8bede2b06075a6
ana <ana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-04-09 22:30:22 +0000

    S e cambio de paquete a element para realizar vista detalle

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/Agreement.java
@@ -0,0 +1,13 @@
+package org.semanticwb.bsc.element;
+
+
+   /**
+   * Define las características de un Acuerdo. 
+   */
+public class Agreement extends org.semanticwb.bsc.element.base.AgreementBase 
+{
+    public Agreement(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
