219db8f69fdc68de7f95e72bbce1d86aea91ceeb
george24 <jajs1975@users.noreply.github.com>
2014-04-11 18:58:04 +0000

    Actualizaciones

--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/SendEmailBase.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/SendEmailBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.social.base;
    /**
    * Acción específica mediante la cual se envía un correo electrónico 
    */
-public abstract class SendEmailBase extends org.semanticwb.social.ActionMsg implements org.semanticwb.model.Descriptiveable,org.semanticwb.social.Emailable,org.semanticwb.model.FilterableNode,org.semanticwb.social.PostTextable,org.semanticwb.model.Filterable,org.semanticwb.model.Traceable,org.semanticwb.social.PostDataable,org.semanticwb.model.FilterableClass
+public abstract class SendEmailBase extends org.semanticwb.social.ActionMsg implements org.semanticwb.social.PostTextable,org.semanticwb.model.Filterable,org.semanticwb.social.PostDataable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.FilterableClass,org.semanticwb.social.Emailable,org.semanticwb.model.Traceable,org.semanticwb.model.FilterableNode
 {
    /**
    * Acción específica mediante la cual se envía un correo electrónico
