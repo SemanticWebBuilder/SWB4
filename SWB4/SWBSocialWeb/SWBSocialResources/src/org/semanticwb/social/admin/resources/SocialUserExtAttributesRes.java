82bc972d3735b4c3933841da6e16bceaa1a5b4b9
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-04-14 21:16:23 +0000

    Actualizaciones

--- SWB4/SWBSocialWeb/SWBSocialResources/src/org/semanticwb/social/admin/resources/SocialUserExtAttributesRes.java
+++ SWB4/SWBSocialWeb/SWBSocialResources/src/org/semanticwb/social/admin/resources/SocialUserExtAttributesRes.java
@@ -6,17 +6,22 @@ package org.semanticwb.social.admin.resources;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Enumeration;
+import java.util.Iterator;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.semanticwb.Logger;
+import org.semanticwb.SWBException;
 import org.semanticwb.SWBPlatform;
 import org.semanticwb.SWBUtils;
 import org.semanticwb.model.SWBContext;
 import org.semanticwb.model.User;
 import org.semanticwb.model.WebSite;
 import org.semanticwb.platform.SemanticObject;
+import org.semanticwb.platform.SemanticProperty;
 import org.semanticwb.portal.api.GenericResource;
 import org.semanticwb.portal.api.SWBActionResponse;
 import org.semanticwb.portal.api.SWBParamRequest;
@@ -73,23 +78,111 @@ public class SocialUserExtAttributesRes extends GenericResource{
     }
       
       
+   private void setUserExtendedAttributes(HttpServletRequest request, SWBActionResponse response) {
+        try {
+            System.out.println("setUserExtendedAttributes-1:"+request.getParameter("suri"));
+            if(request.getParameter("suri")!=null)
+            {
+                WebSite wsite=SWBContext.getAdminWebSite();
+                
+                
+                SemanticObject semObj=SemanticObject.createSemanticObject(request.getParameter("suri"));
+                User user=(User)semObj.createGenericInstance();
+
+                Iterator<SemanticProperty> list = org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/social#SocialUserExtAttributes").listProperties();
+                SocialUserExtAttributes socialExtAtt = SocialUserExtAttributes.ClassMgr.getSocialUserExtAttributes(user.getId(), wsite);
+                if (socialExtAtt == null) {
+                    socialExtAtt = SocialUserExtAttributes.ClassMgr.createSocialUserExtAttributes(user.getId(), wsite);  
+                }
+             
+                
+                System.out.println("setUserExtendedAttributes-2:"+list.hasNext()+",usr:"+user);
+                while (list.hasNext()) {
+                    SemanticProperty sp = list.next();
+                    if (null == request.getParameter(sp.getName())) {
+                        user.removeExtendedAttribute(sp);
+                        user.setExtendedAttribute(sp, false);
+                        user.setExtendedAttribute(sp, Boolean.valueOf(false));
+                        if(sp.getName().equals(SocialUserExtAttributes.social_userCanReTopicMsg.getName())) socialExtAtt.setUserCanReTopicMsg(false);
+                        if(sp.getName().equals(SocialUserExtAttributes.social_userCanReValueMsg.getName())) socialExtAtt.setUserCanReValueMsg(false);
+                        if(sp.getName().equals(SocialUserExtAttributes.social_userCanRemoveMsg.getName())) socialExtAtt.setUserCanRemoveMsg(false);
+                        if(sp.getName().equals(SocialUserExtAttributes.social_userCanRespondMsg.getName())) socialExtAtt.setUserCanRespondMsg(false);
+                        System.out.println("setUserExtendedAttributes-3REMGeorge24:"+sp+",user prop:"+user.getExtendedAttribute(sp));
+                    } else {
+                        System.out.println("setUserExtendedAttributes-4:"+sp);
+                        String [] values = request.getParameterValues(sp.getName());
+                        if (values.length > 1) {
+                            String value = "";
+                            for (int i = 0; i < values.length; i++) {
+                                value += values[i];
+                                if (i < values.length -1) {
+                                    value += "|";
+                                }
+                            }
+                            user.setExtendedAttribute(sp, SWBUtils.XML.replaceXMLChars(value));
+                            user.getSemanticObject().setProperty(sp, SWBUtils.XML.replaceXMLChars(value));
+                        } else if (sp.isString()) {
+                            user.setExtendedAttribute(sp, SWBUtils.XML.replaceXMLChars(request.getParameter(sp.getName())));
+                            user.getSemanticObject().setProperty(sp, SWBUtils.XML.replaceXMLChars(request.getParameter(sp.getName())));
+                        } else if (sp.isBoolean()) {
+                            user.setExtendedAttribute(sp, Boolean.valueOf(true));
+                            if(sp.getName().equals(SocialUserExtAttributes.social_userCanReTopicMsg.getName())) socialExtAtt.setUserCanReTopicMsg(true);
+                            if(sp.getName().equals(SocialUserExtAttributes.social_userCanReValueMsg.getName())) socialExtAtt.setUserCanReValueMsg(true);
+                            if(sp.getName().equals(SocialUserExtAttributes.social_userCanRemoveMsg.getName())) socialExtAtt.setUserCanRemoveMsg(true);
+                            if(sp.getName().equals(SocialUserExtAttributes.social_userCanRespondMsg.getName())) socialExtAtt.setUserCanRespondMsg(true);
+                            System.out.println("sp:"+sp+", a True");
+                        } else if (sp.isInt()) {
+                            try {
+                                Integer val = Integer.valueOf(request.getParameter(sp.getName()));
+                                user.setExtendedAttribute(sp, val);
+                            } catch (Exception ne) {
+                            }
+                        } else if (sp.isDouble()) {
+                            try {
+                                Double val = Double.valueOf(request.getParameter(sp.getName()));
+                                user.setExtendedAttribute(sp, val);
+                            } catch (Exception ne) {
+                            }
+                        } else if (sp.isDate()) {
+                            try {
+                                SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+                                Date val = sf.parse(request.getParameter(sp.getName()));
+                                user.setExtendedAttribute(sp, val);
+                            } catch (Exception ne) {
+                                ne.printStackTrace();
+                            }
+                        }
+
+                    }
+                }
+            }
+        } catch (SWBException nex) {
+            log.error(nex);
+        }
+    }   
+      
+      
+      
+      
+    /*  
     private void setUserExtendedAttributes(HttpServletRequest request, SWBActionResponse response) {
         //User user=response.getUser();
         //Tomando en cuenta que todos los usuarios que se modificaran en sus propiedades extendidas, sean usuarios del repositorio de Admin.
+        System.out.println("setUserExtendedAttributes-JJ0");
         if(request.getParameter("suri")!=null)
         {
             WebSite wsite=SWBContext.getAdminWebSite();
 
-            SemanticObject semObj=SemanticObject.getSemanticObject(request.getParameter("suri"));
-            User user=(User)semObj.getGenericInstance();
+            SemanticObject semObj=SemanticObject.createSemanticObject(request.getParameter("suri"));
+            User user=(User)semObj.createGenericInstance();
 
             SocialUserExtAttributes socialextatt = SocialUserExtAttributes.ClassMgr.getSocialUserExtAttributes(user.getId(), wsite);
-            System.out.println("user.getId():"+user.getId()+",socialextatt-1:"+socialextatt);
+            System.out.println("user.getId()--GEORGE:"+user.getId()+",socialextatt-1:"+socialextatt);
             if (socialextatt == null) {
                 socialextatt = SocialUserExtAttributes.ClassMgr.createSocialUserExtAttributes(user.getId(), wsite);
                 System.out.println("user.getId():"+user.getId()+",socialextatt-2:"+socialextatt);
             }
-            
+        */    
             /*
             Enumeration enParams=request.getParameterNames();
             while(enParams.hasMoreElements())
@@ -97,7 +190,7 @@ public class SocialUserExtAttributesRes extends GenericResource{
                 String paramName=(String)enParams.nextElement();
                 System.out.println("param:"+paramName+",value:"+request.getParameter(paramName));
             }*/
-
+/*
             if (request.getParameter(SocialUserExtAttributes.social_userCanRemoveMsg.getName()) != null) {
                 System.out.println(SocialUserExtAttributes.social_userCanRemoveMsg.getName()+"---True");
                 socialextatt.setUserCanRemoveMsg(true);
@@ -135,5 +228,6 @@ public class SocialUserExtAttributesRes extends GenericResource{
             }
         }
     }
+    * **/
     
 }
