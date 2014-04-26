60acc11203b68008655d6b8cbc37ae0cb891f296
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-04-26 00:15:33 +0000

    Recarga de método getAgreementNumber()

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/Agreement.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/Agreement.java
@@ -1,13 +1,37 @@
 package org.semanticwb.bsc.element;
 
+import java.util.GregorianCalendar;
+import org.semanticwb.bsc.tracing.Meeting;
+
 
    /**
    * Define las características de un Acuerdo. 
    */
-public class Agreement extends org.semanticwb.bsc.element.base.AgreementBase 
-{
-    public Agreement(org.semanticwb.platform.SemanticObject base)
-    {
+public class Agreement extends org.semanticwb.bsc.element.base.AgreementBase {
+    
+    
+    public Agreement(org.semanticwb.platform.SemanticObject base) {
         super(base);
     }
+
+    @Override
+    public String getAgreementNumber() {
+        String agreementNumber = super.getAgreementNumber();
+        
+        if (agreementNumber == null) {
+            Agreement agreement = (Agreement) getSemanticObject().createGenericInstance();
+            Meeting meeting = agreement.getMeeting();
+            int meetingId = Integer.getInteger(meeting.getId());
+            int agreementId = Integer.getInteger(this.getId());
+            java.util.GregorianCalendar systemDate = new java.util.GregorianCalendar();
+            int anio = systemDate.get(GregorianCalendar.YEAR) % 100;
+            agreementNumber = agreement.getBSC().getTitle() + "-" + meeting.getMeetingType().toUpperCase() +
+                    (meetingId > 9 ? "" : "0") + meetingId + "-" +
+                    (agreementId > 9 ? "" : "0") + agreementId + anio;
+            this.setAgreementNumber(agreementNumber);
+        }
+        return agreementNumber;
+    }
+    
+    
 }
