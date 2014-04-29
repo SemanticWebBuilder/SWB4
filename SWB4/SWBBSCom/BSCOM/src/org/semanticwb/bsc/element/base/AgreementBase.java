2440af728fcd103e6867c95d0e3b59706f56cadb
redentor <JRedentor@users.noreply.github.com>
2014-04-29 00:11:32 +0000

    Eliminación de las propiedades selectingAlignment y selectingAlignmentDescr de Risk y adición del método getMeeting a Agreement

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/base/AgreementBase.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/element/base/AgreementBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.bsc.element.base;
    /**
    * Define las características de un Acuerdo. 
    */
-public abstract class AgreementBase extends org.semanticwb.bsc.element.BSCElement implements org.semanticwb.model.FilterableNode,org.semanticwb.model.Filterable,org.semanticwb.bsc.Help,org.semanticwb.model.Roleable,org.semanticwb.model.Referensable,org.semanticwb.model.UserGroupable,org.semanticwb.model.Activeable,org.semanticwb.model.RuleRefable,org.semanticwb.model.Traceable,org.semanticwb.model.Descriptiveable
+public abstract class AgreementBase extends org.semanticwb.bsc.element.BSCElement implements org.semanticwb.model.RuleRefable,org.semanticwb.bsc.Help,org.semanticwb.model.Descriptiveable,org.semanticwb.model.UserGroupable,org.semanticwb.model.Activeable,org.semanticwb.model.Filterable,org.semanticwb.model.Roleable,org.semanticwb.model.Referensable,org.semanticwb.model.Traceable,org.semanticwb.model.FilterableNode
 {
    /**
    * Define el número de un acuerdo, tiene las siguientes características: Título del scorecard+ "-S" + número de sesión a 2 dígitos+ "-" + consecutivo del acuerdo por sesión a 2 dígitos + año actual a 2 dígitos
