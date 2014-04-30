8d598c2c13790052f4018f58eb530789438c557e
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-04-30 15:16:09 +0000

    Actualizaciones

---
+++ SWB4/SWBSocialWeb/SWBSocialResources/src/org/semanticwb/social/admin/resources/PostOutLinksHits.java
@@ -0,0 +1,114 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.semanticwb.social.admin.resources;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBPlatform;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.model.WebSite;
+import org.semanticwb.platform.SemanticObject;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+
+/**
+ *
+ * @author jorge.jimenez
+ */
+public class PostOutLinksHits extends GenericResource {
+
+    private static Logger log = SWBUtils.getLogger(RemoveMessagesResource.class);
+    
+    @Override
+    public void processRequest(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        final String mode = paramRequest.getMode();
+        if("linkHitsData".equals(mode)){
+            doShowLinksHitsData(request, response, paramRequest);
+        }else if ("preview".equals(mode)) {
+            doPreview(request, response, paramRequest);
+        } else {
+            super.processRequest(request, response, paramRequest);
+        }
+    }
+    
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        final String myPath = SWBPlatform.getContextPath() + "/work/models/" + paramRequest.getWebPage().getWebSiteId() + "/jsp/socialTopic/topicLinksHits.jsp";
+        RequestDispatcher dis = request.getRequestDispatcher(myPath);
+        if (dis != null) {
+            try {
+                request.setAttribute("paramRequest", paramRequest);
+                dis.include(request, response);
+            } catch (Exception ex) {
+                log.error(ex);
+                ex.printStackTrace(System.out);
+            }
+        }
+        
+    }
+    
+    
+    /*
+     * Shows the Hits of a PostOut Link
+     */
+    private void doShowLinksHitsData(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) {
+        final String myPath = SWBPlatform.getContextPath() + "/work/models/" + paramRequest.getWebPage().getWebSiteId() + "/jsp/review/showPostOutLinksHitsData.jsp";
+        response.setContentType("text/html; charset=ISO-8859-1");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Pragma", "no-cache");
+        RequestDispatcher dis = request.getRequestDispatcher(myPath);
+        if (dis != null) {
+            try {
+                request.setAttribute("uri", request.getParameter("uri"));
+                request.setAttribute("paramRequest", paramRequest);
+                dis.include(request, response);
+            } catch (Exception e) {
+                log.error(e);
+                e.printStackTrace(System.out);
+            }
+        }
+    }
+    
+    
+    /**
+     * Shows the preview of the content.
+     *
+     * @param request , this holds the parameters
+     * @param response , an answer to the user request
+     * @param paramRequest , a list of objects like user, webpage, Resource, ...
+     * @throws SWBResourceException, a Resource Exception
+     * @throws IOException, an In Out Exception
+     * @throws SWBResourceException the sWB resource exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public void doPreview(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        String postUri = request.getParameter("postUri");
+        try {
+            final String path = SWBPlatform.getContextPath() + "/work/models/" + paramRequest.getWebPage().getWebSiteId() + "/jsp/review/showPostOut.jsp";
+            if (request != null) {
+                RequestDispatcher dis = request.getRequestDispatcher(path);
+                if (dis != null) {
+                    try {
+                        SemanticObject semObject = SemanticObject.createSemanticObject(postUri);
+                        request.setAttribute("postOut", semObject);
+                        request.setAttribute("paramRequest", paramRequest);
+                        dis.include(request, response);
+                    } catch (Exception e) {
+                        log.error(e);
+                        e.printStackTrace(System.out);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("Error while getting content string ,id:" + postUri, e);
+        }
+    }
+    
+}
