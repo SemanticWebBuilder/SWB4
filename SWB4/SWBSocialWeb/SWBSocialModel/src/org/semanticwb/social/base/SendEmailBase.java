764f96a38c82dc059ec5b29bbf28eae525693a7a
george24 <jajs1975@users.noreply.github.com>
2014-05-19 23:12:32 +0000

    Actualizaciones

--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/SendEmailBase.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/SendEmailBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.social.base;
    /**
    * Acción específica mediante la cual se envía un correo electrónico 
    */
-public abstract class SendEmailBase extends org.semanticwb.social.ActionMsg implements org.semanticwb.model.Traceable,org.semanticwb.model.Filterable,org.semanticwb.model.FilterableClass,org.semanticwb.model.Descriptiveable,org.semanticwb.social.Emailable,org.semanticwb.model.FilterableNode
+public abstract class SendEmailBase extends org.semanticwb.social.ActionMsg implements org.semanticwb.model.Traceable,org.semanticwb.model.Filterable,org.semanticwb.social.Emailable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.FilterableNode,org.semanticwb.model.FilterableClass
 {
    /**
    * Acción específica mediante la cual se envía un correo electrónico
--- SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/SocialRuleBase.java
+++ SWB4/SWBSocialWeb/SWBSocialModel/src/org/semanticwb/social/base/SocialRuleBase.java
@@ -4,7 +4,7 @@ package org.semanticwb.social.base;
    /**
    * Clase principal para manejo de reglas en swbSocial 
    */
-public abstract class SocialRuleBase extends org.semanticwb.model.SWBClass implements org.semanticwb.model.Traceable,org.semanticwb.model.Filterable,org.semanticwb.model.FilterableClass,org.semanticwb.model.XMLable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.FilterableNode,org.semanticwb.social.Relationable
+public abstract class SocialRuleBase extends org.semanticwb.model.SWBClass implements org.semanticwb.model.Traceable,org.semanticwb.model.Filterable,org.semanticwb.model.XMLable,org.semanticwb.social.Relationable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.FilterableNode,org.semanticwb.model.FilterableClass
 {
    /**
    * Clase padre de todas las acciones posibles en swbsocial
