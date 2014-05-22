98accc3a1a74c5484a4b27b47ab05c519b39b486
Javier Solis <softjei@gmail.com>
2014-05-22 22:47:36 +0000

    Correccion de bugs, modelador y soporte a multiples grupos y papelera de grupos

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/ScalationStartEventBase.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/ScalationStartEventBase.java
@@ -1,7 +1,7 @@
 package org.semanticwb.process.model.base;
 
 
-public abstract class ScalationStartEventBase extends org.semanticwb.process.model.StartEventNode implements org.semanticwb.model.Traceable,org.semanticwb.process.model.ActionCodeable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Sortable
+public abstract class ScalationStartEventBase extends org.semanticwb.process.model.StartEventNode implements org.semanticwb.model.Traceable,org.semanticwb.model.Sortable,org.semanticwb.model.Descriptiveable,org.semanticwb.process.model.ActionCodeable
 {
     public static final org.semanticwb.platform.SemanticClass swp_ScalationStartEvent=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/process#ScalationStartEvent");
    /**
