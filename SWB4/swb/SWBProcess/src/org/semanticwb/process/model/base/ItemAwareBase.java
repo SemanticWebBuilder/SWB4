311463f8147980eeb0eac488bd06a9619235ecc2
Javier Solis <softjei@gmail.com>
2014-05-22 22:47:02 +0000

    Correccion de bugs, modelador y soporte a multiples grupos y papelera de grupos

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/ItemAwareBase.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/ItemAwareBase.java
@@ -1,7 +1,7 @@
 package org.semanticwb.process.model.base;
 
 
-public abstract class ItemAwareBase extends org.semanticwb.process.model.GraphicalElement implements org.semanticwb.model.Traceable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Sortable
+public abstract class ItemAwareBase extends org.semanticwb.process.model.GraphicalElement implements org.semanticwb.model.Descriptiveable,org.semanticwb.model.Traceable,org.semanticwb.model.Sortable
 {
     public static final org.semanticwb.platform.SemanticClass swb_Class=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/ontology#Class");
     public static final org.semanticwb.platform.SemanticProperty swp_dataObjectClass=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.semanticwebbuilder.org/swb4/process#dataObjectClass");
--- SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/SignalEndEventBase.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/base/SignalEndEventBase.java
@@ -1,7 +1,7 @@
 package org.semanticwb.process.model.base;
 
 
-public abstract class SignalEndEventBase extends org.semanticwb.process.model.EndEventNode implements org.semanticwb.model.Traceable,org.semanticwb.model.Sortable,org.semanticwb.process.model.ActionCodeable,org.semanticwb.model.Descriptiveable
+public abstract class SignalEndEventBase extends org.semanticwb.process.model.EndEventNode implements org.semanticwb.model.Traceable,org.semanticwb.model.Sortable,org.semanticwb.model.Descriptiveable,org.semanticwb.process.model.ActionCodeable
 {
     public static final org.semanticwb.platform.SemanticClass swp_SignalEndEvent=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.semanticwebbuilder.org/swb4/process#SignalEndEvent");
    /**
