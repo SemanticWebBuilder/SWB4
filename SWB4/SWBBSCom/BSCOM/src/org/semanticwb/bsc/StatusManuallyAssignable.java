77e1384940d35db8632088f58bfc682d79f3567e
redentor <JRedentor@users.noreply.github.com>
2014-06-10 18:50:24 +0000

    Se creo esta interface para los entregables y las iniciativas

---
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/StatusManuallyAssignable.java
@@ -0,0 +1,8 @@
+package org.semanticwb.bsc;
+
+   /**
+   * Define la posibilidad de que un usuario pueda asignar  directamente un estado a un objeto 
+   */
+public interface StatusManuallyAssignable extends org.semanticwb.bsc.base.StatusManuallyAssignableBase
+{
+}
