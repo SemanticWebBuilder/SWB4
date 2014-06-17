9a8381de7664c0ab0c85ec1ef9a0c14cee16ab9a
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-06-17 22:27:50 +0000

    Se generalizó la forma en que se evalúa si se debe agregar el vínculo al título de los elementos mostrados

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/formelement/MultiSelect.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/formelement/MultiSelect.java
@@ -2,10 +2,15 @@ package org.semanticwb.bsc.formelement;
 
 import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
+import org.semanticwb.bsc.InitiativeAssignable;
+import org.semanticwb.bsc.element.Agreement;
+import org.semanticwb.bsc.element.BSCElement;
 import org.semanticwb.bsc.element.Deliverable;
 import org.semanticwb.bsc.element.Indicator;
 import org.semanticwb.bsc.element.Initiative;
 import org.semanticwb.bsc.element.Objective;
+import org.semanticwb.bsc.element.Risk;
+import org.semanticwb.model.GenericObject;
 import org.semanticwb.model.SWBClass;
 import org.semanticwb.platform.SemanticClass;
 import org.semanticwb.platform.SemanticLiteral;
@@ -50,14 +55,24 @@ public class MultiSelect extends org.semanticwb.bsc.formelement.base.MultiSelect
             viewString.append("<ul>");
             
             if (prop.isObjectProperty()) {
-                if (rangeClass.equals(Objective.bsc_Objective) ||
-                        rangeClass.equals(Indicator.bsc_Indicator) ||
-                        rangeClass.equals(Initiative.bsc_Initiative) ||
-                        rangeClass.equals(Deliverable.bsc_Deliverable)) {
-                    showLink = true;
-                }
-                Iterator<SemanticObject> values = obj.listObjectProperties(prop);
                 
+                if (rangeClass.getRootClass().equals(BSCElement.bsc_BSCElement)) {
+                    showLink = true;
+                } else if (rangeClass.getRootClass().isSWBInterface()) {
+                    SemanticObject value = obj.listObjectProperties(prop) != null ? obj.listObjectProperties(prop).next() : null;
+                    if (value != null) {
+                        GenericObject generic = value.createGenericInstance();
+                        if (generic instanceof Indicator) {
+                            rangeClass = Indicator.bsc_Indicator;
+                            showLink = true;
+                        } else if (generic instanceof Risk) {
+                            rangeClass = Risk.bsc_Risk;
+                            showLink = true;
+                        }
+                    }
+                }
+                
+                Iterator<SemanticObject> values = obj.listObjectProperties(prop);
                 while (values.hasNext()) {
                     SemanticObject semanticObject = values.next();
                     if(semanticObject.getSemanticClass().isSWBClass()) {
