e5f1c4855b21d641b44cd7847f2a0d63ee231669
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-06-19 18:41:38 +0000

    Adición de la funcionalidad para los modos: inlineEdit y view

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/formelement/NumberSpinner.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/formelement/NumberSpinner.java
@@ -1,11 +1,12 @@
 package org.semanticwb.bsc.formelement;
 
 import javax.servlet.http.HttpServletRequest;
+import org.semanticwb.model.FormElementURL;
 import org.semanticwb.platform.SemanticObject;
 import org.semanticwb.platform.SemanticProperty;
 
 /**
- * Form Element que presentará la vista para un sppiner, el cual permitirá
+ * Presenta la vista para un sppiner, el cual permite
  * ajustar un valor dentro de un cuadro de texto.
  * 
  * @author Martha Elvia Jim&eacute;nez Salgado
@@ -25,7 +26,7 @@ public class NumberSpinner extends org.semanticwb.bsc.formelement.base.NumberSpi
 
     /**
      * 
-     * Genera el código HTML para representar un sppiner.
+     * Genera el c&oacute;digo HTML para representar un sppiner cuando el {@code type} recibido es {@literal dojo}.
      *
      * @param request la petici&oacute;n HTTP hecha por el cliente
      * @param obj el objeto a quien pertenece la propiedad asociada a este
@@ -36,17 +37,18 @@ public class NumberSpinner extends org.semanticwb.bsc.formelement.base.NumberSpi
      * el valor {@code dojo}
      * @param mode el modo en que se presentar&aacute; el despliegue del
      * FormElement. Los modos soportados son:
-     * {@literal edit}, {@literal create}, {@literal filter} y {@literal view}
+     * {@literal edit}, {@literal create}, {@literal inlineEdit} y {@literal view}
      * @param lang el lenguaje utilizado en la generaci&oacute;n del
      * c&oacute;digo HTML a regresar
      * @return el objeto String que representa el c&oacute;digo HTML con la
      * vista correspondiente al modo especificado para este elemento de forma.
-     * 
      */
     @Override
     public String renderElement(HttpServletRequest request, SemanticObject obj, 
     SemanticProperty prop, String propName, String type, String mode, String lang) {
-        StringBuilder buffer = new StringBuilder();
+        
+        System.out.println("En NumberSpinner: \nobj: " + obj + "\nprop: " + prop + "\ntype: " + type + "\nmode: " + mode);
+        StringBuilder buffer = new StringBuilder(256);
         String value = null;
         if (obj.getProperty(prop) != null) {
             value = obj.getProperty(prop);
@@ -57,26 +59,84 @@ public class NumberSpinner extends org.semanticwb.bsc.formelement.base.NumberSpi
         int valMax = getValueMax();
         int valMin = getValueMin();
         int increase = getIncrease();
-        buffer.append("<input data-dojo-type=\"dijit.form.NumberSpinner\" ");
-        buffer.append("id=\"");
-        buffer.append(propName);
-        buffer.append("_");
-        buffer.append(obj.getId());
-        buffer.append("\"");
-        buffer.append(" value=\"");
-        buffer.append(value);
-        buffer.append("\"");
-        buffer.append(" data-dojo-props=\"smallDelta:");
-        buffer.append(increase);
-        buffer.append(", constraints:{");
-        buffer.append("min:");
-        buffer.append(valMin);
-        buffer.append(",max:");
-        buffer.append(valMax);
-        buffer.append(",places:0}\"");
-        buffer.append(" name=\"");
-        buffer.append(propName);
-        buffer.append("\"/>");
+        boolean isDojoType = type != null && type.equalsIgnoreCase("dojo");
+        String dojoAttributes = " data-dojo-type=\"dijit.form.NumberSpinner\" data-dojo-props=\"smallDelta:" +
+               increase + ", constraints:{min:" + valMin + ", max:" + valMax + ", places:0}\"";
+        
+        if (mode == null) {
+            mode = "view";
+        }
+        if (mode.equals("edit") || mode.equals("create")) {
+            buffer.append("<input type=\"text\" id=\"");
+            buffer.append(propName);
+            buffer.append("_");
+            buffer.append(obj.getId());
+            buffer.append("\" value=\"");
+            buffer.append(value);
+            buffer.append("\"");
+            if (isDojoType) {
+                buffer.append(dojoAttributes);
+            }
+            buffer.append(" name=\"");
+            buffer.append(propName);
+            buffer.append("\"/>");
+        } else if (mode.equals("inlineEdit")) {
+            /*Al utilizar este modo, se debe incluir en el HTML las instrucciones 
+             * dojo.require del dijit.form.NumberSpinner, al menos*/
+            FormElementURL urlProcess = getProcessURL(obj, prop);
+            String objectId = obj.getSemanticClass().getClassCodeName() + obj.getId() + propName;
+
+            buffer.append("<script type=\"text/javascript\">\n");
+            buffer.append("  <!--\n");
+            buffer.append("    var iledit_");
+            buffer.append(objectId);
+            buffer.append(";\n");
+            buffer.append("    dojo.addOnLoad( function() {\n");
+            buffer.append("      iledit_");
+            buffer.append(objectId);
+            buffer.append(" = new dijit.InlineEditBox({\n");
+            buffer.append("        id: \"");
+            buffer.append(objectId);
+            buffer.append("\",\n");
+            buffer.append("        autoSave: false,\n");
+            buffer.append("        editor: \"dijit.form.NumberSpinner\",\n");
+            buffer.append("        editorParams: {required: false, width: '50px', constraints: {places:0, min:");
+            buffer.append(valMin);
+            buffer.append(", max:");
+            buffer.append(valMax);
+            buffer.append("}, smallDelta: ");
+            buffer.append(increase);
+            buffer.append("},\n");
+            buffer.append("        onChange: function(value) {\n");
+            buffer.append("          getSyncHtml('");
+            buffer.append(urlProcess.toString());
+            buffer.append("&");
+            buffer.append(prop.getName());
+            buffer.append("='+value);\n");
+            buffer.append("        }\n");
+            buffer.append("      }, 'eb_");
+            buffer.append(objectId);
+            buffer.append("');\n");
+            buffer.append("      iledit_");
+            buffer.append(objectId);
+            buffer.append(".startup();\n");
+            buffer.append("    });\n");
+            buffer.append("-->\n");
+            buffer.append("</script>\n");
+            buffer.append("<span id=\"eb_");
+            buffer.append(objectId);
+            buffer.append("\" class=\"swb-ile\">");
+            buffer.append(value);
+            buffer.append("</span>");
+        } else if (mode.equals("view")) {
+            buffer.append("<span id=\"");
+            buffer.append(propName);
+            buffer.append("_");
+            buffer.append(obj.getId());
+            buffer.append("\">");
+            buffer.append(value);
+            buffer.append("</span>");
+        }
         return buffer.toString();
     }
 }
