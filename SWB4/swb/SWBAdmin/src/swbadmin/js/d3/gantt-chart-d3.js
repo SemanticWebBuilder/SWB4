1307a0863f9019bdf300d1bf9a13350d976d2aa2
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-06-27 14:57:07 +0000

    Modificaciones para que se calcule el espacio necesario para las etiquetas del eje Y y se muestren tooltips al pasar el mouse sobre los bloques correspondientes a las actividades.

--- SWB4/swb/SWBAdmin/src/swbadmin/js/d3/gantt-chart-d3.js
+++ SWB4/swb/SWBAdmin/src/swbadmin/js/d3/gantt-chart-d3.js
@@ -7,15 +7,16 @@
 d3.gantt = function() {
     var selection = "#ganttChart";
     var container = d3.select(selection);
-    
+    var that = container;
+    var longestTaskName = 0;
+    var offsetX = 0;
     var FIT_TIME_DOMAIN_MODE = "fit";
     var FIXED_TIME_DOMAIN_MODE = "fixed";
-    
     var margin = {
-        top : 20,
-        right : 40,
+        top : 10,
+        right : 0,
         bottom : 20,
-        left : 150
+        left : 20
     };
     var timeDomainStart = d3.time.day.offset(new Date(), -3);
     var timeDomainEnd = d3.time.hour.offset(new Date(), +3);
@@ -23,31 +24,21 @@ d3.gantt = function() {
     var taskTypes = [];
     var taskStatus = [];
     var availableWidth = ((container.style && container.style('width') && (parseInt(container.style('width')) > 460) ? parseInt(container.style('width')) : false) || 460) - margin.left - margin.right; //
-    
     var availableHeight = ((container.style && container.style('height') && (parseInt(container.style('height')) > 250) ? parseInt(container.style('height')) : false) || 250) - margin.top - margin.bottom; //
-    
-    var height = availableHeight - margin.top - margin.bottom-5;
-    var width = availableWidth - margin.right - margin.left-5;
-
+    var height = availableHeight - 5; //- margin.top - margin.bottom
+    var width = availableWidth - 5;  //- margin.right - margin.left
     var tickFormat = "%H:%M"; //  %d/%m/%Y
-
     var keyFunction = function(d) {
         return d.startDate + d.taskName + d.endDate;
     };
-
     var rectTransform = function(d) {
-        return "translate(" + x(d.startDate) + "," + y(d.taskName) + ")";
+        return "translate(" + (x(d.startDate) + gantt.offsetX()) + "," + (y(d.taskName) - margin.top - margin.bottom) + ")";
     };
-
     var x = d3.time.scale().domain([ timeDomainStart, timeDomainEnd ]).range([ 0, width ]).clamp(true);
-
-    var y = d3.scale.ordinal().domain(taskTypes).rangeRoundBands([ 0, height - margin.top - margin.bottom ], .1);
-    
+    var y = d3.scale.ordinal().domain(taskTypes).rangeRoundBands([ 0, height ], .1);  // - margin.top - margin.bottom
     var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format(tickFormat)).tickSubdivide(true)
 	    .tickSize(8).tickPadding(8);
-
     var yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
-
     var initTimeDomain = function(tasks) {
         if (timeDomainMode === FIT_TIME_DOMAIN_MODE) {
             if (tasks === undefined || tasks.length < 1) {
@@ -64,22 +55,42 @@ d3.gantt = function() {
             });
             timeDomainStart = tasks[0].startDate;
         }
+        for (var i = 0; i < tasks.length; i++) {
+            if (tasks[i].taskName.length > longestTaskName) {
+                longestTaskName = tasks[i].taskName.length;
+            }
+        }
     };
-
     var initAxis = function() {
         x = d3.time.scale().domain([ timeDomainStart, timeDomainEnd ]).range([ 0, width ]).clamp(true);
-        y = d3.scale.ordinal().domain(taskTypes).rangeRoundBands([ 0, height - margin.top - margin.bottom ], .1);
+        y = d3.scale.ordinal().domain(taskTypes).rangeRoundBands([ 0, height ], .1); //height - margin.top - margin.bottom
         xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format(tickFormat)).tickSubdivide(true)
             .tickSize(8).tickPadding(8);
 
         yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
     };
     
+    //Agregadas para manejo de tooltips
+    var tooltips = true;
+    var tooltip = function(key, x, y, e, graph) {
+        return '<h3>' + key + '</h3>' +
+               '<p>Del ' +  new Date(x).toLocaleDateString() + ' al ' + new Date(y).toLocaleDateString() + '</p>';
+      }
+    var showTooltip = function(e, offsetElement) {
+        var left = e.pos[0] + ( offsetElement.offsetLeft || 0 ),
+            top = e.pos[1] + ( offsetElement.offsetTop || 0),
+            x = xAxis.tickFormat()(gantt.x()(e.point, e.pointIndex)),
+            y = yAxis.tickFormat()(gantt.y()(e.point, e.pointIndex)),
+            content = tooltip(e.series.key, x, y, e, gantt);
+        nv.tooltip.show([left, top], content, e.value < 0 ? 'n' : 's', null, offsetElement);
+    };
+    
     function gantt(tasks) {
 	
         initTimeDomain(tasks);
         initAxis();
-	
+        gantt.offsetX((gantt.longestLabel() > 0 ? gantt.longestLabel() * 6 : margin.left) + margin.left); //calcula el ancho de las etiquetas
+        
         var svg = d3.select(selection)
             .append("svg")
             .attr("class", "chart")
@@ -87,19 +98,15 @@ d3.gantt = function() {
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
                 .attr("class", "gantt-chart")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+            .attr("width", width)    //+ margin.left + margin.right
+            .attr("height", height)  // + margin.top + margin.bottom
+            .attr("transform", "translate(" + margin.left + ", " + (margin.top + margin.bottom) + ")");
 	
         svg.selectAll(".chart")
             .data(tasks, keyFunction).enter()
             .append("rect")
             .attr("rx", 5)
             .attr("ry", 5)
-/*            .attr("class", function(d){ 
-                if(taskStatus[d.status] == null){ return "bar";}
-                    return taskStatus[d.status];
-                })*/
             .attr("style", function(d){ 
                 if (!d.status) { return "fill: #33b5e5;";}
                     return ("fill: " + d.status + ";");
@@ -107,19 +114,23 @@ d3.gantt = function() {
             .attr("y", 0)
             .attr("transform", rectTransform)
             .attr("height", function(d) { return y.rangeBand(); })
-            .attr("width", function(d) { 
-                return (x(d.endDate) - x(d.startDate)); 
-                });
-	
+            .attr("width", function(d) {
+                return (x(d.endDate) - x(d.startDate) - gantt.offsetX()); 
+                })
+            .attr("onmouseover", "gantt.tooltipShow(evt, this)")
+            .attr("onmouseout", "gantt.tooltipHide()");
+	/* ----- Para el despliegue de tooltips ----- */
+    
         svg.append("g")
             .attr("class", "x axis")
-            .attr("transform", "translate(0, " + (height - margin.top - margin.bottom) + ")")
-//            .attr('transform', function(d,i) { return 'translate(0,' + ((i % 2 == 0 ? 0 : 12) + (height - margin.top - margin.bottom)) + ')' });
+            .attr("transform", "translate(" + gantt.offsetX() + ", " + (height - margin.top - margin.bottom) + ")")
             .transition()
             .call(xAxis);
          
-        svg.append("g").attr("class", "y axis").transition().call(yAxis);
-         
+        svg.append("g").attr("class", "y axis")
+            .attr("transform", "translate(" + gantt.offsetX() + ", " + (0 - margin.top - margin.bottom) + ")")
+            .transition().call(yAxis);
+         outterSvg = svg;
         return gantt;
 
     };
@@ -138,10 +149,6 @@ d3.gantt = function() {
             .insert("rect",":first-child")
             .attr("rx", 5)
             .attr("ry", 5)
-/*            .attr("class", function(d) {
-                if (taskStatus[d.status] == null) { return "bar";}
-                return taskStatus[d.status];
-            }) */
             .attr("style", function(d){ 
                 if (!d.status) { return "fill: #33b5e5;";}
                     return ("fill: " + d.status + ";");
@@ -151,13 +158,13 @@ d3.gantt = function() {
             .attr("transform", rectTransform)
             .attr("height", function(d) { return y.rangeBand(); })
             .attr("width", function(d) { 
-                return (x(d.endDate) - x(d.startDate)); 
+                return (x(d.endDate) - x(d.startDate) - gantt.offsetX()); 
              });
         rect.transition()
             .attr("transform", rectTransform)
             .attr("height", function(d) { return y.rangeBand(); })
             .attr("width", function(d) { 
-                return (x(d.endDate) - x(d.startDate)); 
+                return (x(d.endDate) - x(d.startDate) - gantt.offsetX()); 
             });
         rect.exit().remove();
 
@@ -224,6 +231,62 @@ d3.gantt = function() {
         if (!arguments.length)
             return tickFormat;
         tickFormat = value;
+        return gantt;
+    };
+    
+    gantt.longestLabel = function(value) {
+        if (!arguments.length)
+            return longestTaskName;
+        longestTaskName = value;
+        return gantt;
+    };
+    
+    gantt.offsetX = function(value) {
+        if (!arguments.length)
+            return offsetX;
+        offsetX = value;
+        return gantt;
+    };
+    
+    gantt.tooltip = function(_) {
+      if (!arguments.length) return tooltip;
+      tooltip = _;
+      return gantt;
+    };
+
+    gantt.tooltips = function(_) {
+      if (!arguments.length) return tooltips;
+      tooltips = _;
+      return gantt;
+    };
+
+    gantt.tooltipContent = function(_) {
+      if (!arguments.length) return tooltip;
+      tooltip = _;
+      return gantt;
+    };
+    gantt.tooltipShow = function(evt, element) {
+        var left = evt.clientX + ( element.offsetLeft || 0 ),
+            top = evt.clientY + ( element.offsetTop || 0),
+            content = tooltip(element.__data__.taskName, element.__data__.startDate, element.__data__.endDate, evt, gantt);
+        nv.tooltip.show([left, top], content, 'n', null, element.parentNode);
+      return gantt;
+    };
+    gantt.tooltipHide = function() {
+        if (tooltips) nv.tooltip.cleanup();
+        return gantt;
+    };
+    
+    gantt.x = function(value) {
+        if (!arguments.length)
+            return x;
+        x = value;
+        return gantt;
+    };
+    gantt.y = function(value) {
+        if (!arguments.length)
+            return y;
+        y = value;
         return gantt;
     };
     
