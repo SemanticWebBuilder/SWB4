a449d3e6e86c3d048024f32c9e49789a5f61882c
francisco <dendral@users.noreply.github.com>
2014-07-02 22:51:52 +0000

    Import facebook pages  and page tabs of a Facebook Account.

---
+++ SWB4/SWBSocialWeb/SWBSocialResources/src/org/semanticwb/social/admin/resources/ImportFanPages.java
@@ -0,0 +1,468 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.semanticwb.social.admin.resources;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBPlatform;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.model.WebPage;
+import org.semanticwb.model.WebSite;
+import org.semanticwb.platform.SemanticObject;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBActionResponse;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.portal.api.SWBResourceURL;
+import org.semanticwb.social.FacePageTab;
+import org.semanticwb.social.Facebook;
+import org.semanticwb.social.FacebookFanPage;
+
+/**
+ *
+ * @author francisco.jimenez
+ */
+public class ImportFanPages extends GenericResource{
+    public static Logger log = SWBUtils.getLogger(ImportFanPages.class);
+    public static String IMPORT_FB_PAGES = "importFBPages";
+    
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        System.out.println("loading JSP importFP");
+        final String path = SWBPlatform.getContextPath() + "/work/models/" + paramRequest.getWebPage().getWebSiteId() + "/jsp/fanPages/importFPFacebook.jsp";
+        request.setAttribute("paramRequest", paramRequest);
+        
+        try{
+            RequestDispatcher rd = request.getRequestDispatcher(path);
+            rd.include(request, response);
+        }catch(Exception e){
+            log.error("Error in: " + e);
+        }
+    }
+
+    @Override
+    public void processAction(HttpServletRequest request, SWBActionResponse response) throws SWBResourceException, IOException {
+        String action = response.getAction();
+        System.out.println("process action:" + action);
+        
+        if(action.equals(IMPORT_FB_PAGES)){
+            String suri = request.getParameter("suri") == null ? "" : request.getParameter("suri");
+            String site = request.getParameter("site") == null ? "" : request.getParameter("site");
+            String pages[] = request.getParameterValues("pages") == null ? null : request.getParameterValues("pages");
+            System.out.println("suri:" + suri);
+            System.out.println("site:" + site);
+            System.out.println("pages[]:" + pages);
+            //add facebook pages
+            if(suri.isEmpty() || site.isEmpty() || pages == null){
+                response.setMode(SWBResourceURL.Mode_HELP);
+                return;
+            }
+            
+            Facebook facebook = (Facebook) SemanticObject.createSemanticObject(suri).createGenericInstance();
+            WebSite wsite = WebSite.ClassMgr.getWebSite(site);
+            
+            WebPage homePage = wsite.getHomePage();
+            
+            WebPage rootFP = WebPage.ClassMgr.getWebPage(facebook.getFacebookUserId(), wsite);
+            if(rootFP == null){
+                rootFP = WebPage.ClassMgr.createWebPage(facebook.getFacebookUserId(), wsite);
+            }
+            
+            rootFP.setTitle(facebook.getTitle() + " Fan Pages");
+            rootFP.setParent(homePage);
+            HashMap<String, String> params = new HashMap<String, String>(2);
+        
+            params.put("access_token", facebook.getAccessToken());
+            String respFanPage = postRequest(params, "https://graph.facebook.com/me/accounts",
+                                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95", "GET");
+            //System.out.println("respFanPage:" + respFanPage);
+            try{
+                JSONObject responseFP = new JSONObject(respFanPage);
+                if(!responseFP.isNull("data")){
+                    if(responseFP.getJSONArray("data").length()>0){
+                        JSONArray jarr = responseFP.getJSONArray("data");
+                        for(int i = 0 ; i <  jarr.length() ; i++){                            
+                            JSONObject entryFP = jarr.getJSONObject(i);
+                            boolean selectedFanPage = false;
+                            for(int j = 0; j < pages.length; j++){//check if the page was selected
+                                if(pages[j].equals(entryFP.getString("id"))){
+                                    selectedFanPage = true;
+                                    break;
+                                }
+                            }
+                            
+                            if(selectedFanPage){//import this page                                
+                                String id = "";
+                                String name = "";
+                                String token = "";
+                                if(!entryFP.isNull("id")){
+                                    id = entryFP.getString("id");
+                                }
+                                if(!entryFP.isNull("name")){
+                                    name = entryFP.getString("name");
+                                }
+                                if(!entryFP.isNull("access_token")){
+                                    token = entryFP.getString("access_token");
+                                }
+                                if(!id.isEmpty() && !token.isEmpty()){//Add the Fan Pages
+                                    FacebookFanPage ffp = FacebookFanPage.ClassMgr.getFacebookFanPage(id, wsite);
+                                    if(ffp == null){
+                                        ffp = FacebookFanPage.ClassMgr.createFacebookFanPage(id, wsite);
+                                    }
+                                    ffp.setPage_id(id);
+                                    ffp.setTitle(name);
+                                    ffp.setPageAccessToken(token);
+                                    ffp.setParent(rootFP);
+
+                                    createFPTabs(facebook, ffp, wsite);
+                                }
+                            }                            
+                        }
+                    }
+                }
+            }catch(JSONException jsone){
+                log.error("Unable to add facebook pages", jsone);
+            }
+            
+            
+            /*for(int i = 0; i < pages.length; i++){
+                String pageId = pages[i];
+                
+                try{
+                    JSONObject page = new JSONObject(respFanPage);
+                    if(page.isNull("error")){
+                        log.error("Fan page with id does not exist." + page);
+                        continue;
+                    }
+                    String id = "";
+                    String name = "";
+                    String description ="";
+                    String token = "";
+                }catch(JSONException jsone){
+                    log.error("Unable to add fan page ", jsone );
+                    continue;
+                }
+                
+            }*/
+            response.setRenderParameter("accountName", facebook.getTitle());
+            response.setMode(SWBResourceURL.Mode_HELP);
+        }
+    }
+
+    @Override
+    public void doHelp(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Las páginas de Fans de la cuenta <b>" + request.getParameter("accountName") + "</b> fueron importadas correctamente.");
+    }
+    
+    private boolean createFPTabs(Facebook facebook, FacebookFanPage fp, WebSite wsite){
+    
+        if(fp == null || facebook == null){
+                return false;
+        }
+
+        try {
+            HashMap<String, String> params = new HashMap<String, String>(2);
+            params.put("access_token", fp.getPageAccessToken());
+            String pageTabs = postRequestParams(params, "https://graph.facebook.com/" + fp.getPage_id() +"/tabs",
+                    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95", "GET");
+            //System.out.println("\n\n--------------------\n" + pageTabs);
+            JSONObject jsonObject = new JSONObject(pageTabs);            
+            if(jsonObject.has("data")){
+                JSONArray data = jsonObject.getJSONArray("data");
+                if(data.length() > 0 ){
+                    for(int i =0 ; i < data.length() ; i++){
+                        JSONObject tmp = data.getJSONObject(i);
+                        String id = "";
+                        String name = "";
+                        String position = "";
+                        String appId = "";
+                        if(!tmp.isNull("id")){
+                            id = tmp.getString("id");
+                            id = id.replace('/', '_');
+                            appId = id.substring(id.lastIndexOf("_") + 1);
+                        }
+                        if(!tmp.isNull("name")){
+                            name = tmp.getString("name");
+                        }
+                        if(!tmp.isNull("position")){
+                            position = tmp.getLong("position") +"";
+                        }
+                        
+                        //System.out.println("\n\n\n\nid:" + id  + " name:" + name + " position:" + position);
+                        if(id.isEmpty() || name.isEmpty() || position.isEmpty()){
+                            log.error("Fan page tab not added to Social:" + tmp);
+                            continue;
+                        }
+                        
+                        FacePageTab tmpffp = FacePageTab.ClassMgr.getFacePageTab(id, wsite);
+                        if(tmpffp == null){
+                            tmpffp = FacePageTab.ClassMgr.createFacePageTab(id, wsite);
+                        }
+                        tmpffp.setTitle(name);
+                        tmpffp.setFace_appid(appId);
+                        tmpffp.setSortName(position);
+                        tmpffp.setParent(fp);
+                        /*if(tmp.has("application")){
+                            JSONObject app = tmp.getJSONObject("application");
+                            if(app.has("id")){
+                                String appId = app.getString("id");                            
+                                ///do something here
+                            }
+                        }*/
+                    }
+                }
+            }
+            //System.out.println("SE ENCUENTRA ACTIVO EL TAB: " + isActive);
+        } catch (Exception ex) {
+            log.error("Problem getting list of current tabs from page ", ex );
+        }
+        return false;
+    }
+    
+    
+    /**
+     * Realiza peticiones al grafo de Facebook que deban ser enviadas por
+     * alg&uacute;n m&eacute;todo en particular
+     *
+     * @param params contiene los par&aacute;metros a enviar a Facebook para
+     * realizar la operaci&oacute;n deseada
+     * @param url especifica el objeto del grafo de Facebook con el que se desea
+     * interactuar
+     * @param userAgent indica el navegador de Internet utilizado en la
+     * petici&oacute;n a realizar
+     * @param method indica el m&eacute;todo de la petici&oacute; HTTP requerido
+     * por Facebook para realizar una operaci&oacute;n, como:
+     * {@literal POST}, {@literal DELETE} o {@literal GET}
+     * @return un {@code String} que representa la respuesta generada por el
+     * grafo de Facebook
+     * @throws IOException en caso de que se produzca un error al generar la
+     * petici&oacute;n o recibir la respuesta del grafo de Facebook
+     */
+    public static String postRequest(Map<String, String> params, String url,
+            String userAgent, String method) throws IOException {
+
+        CharSequence paramString = (null == params) ? "" : delimit(params.entrySet(), "&", "=", true);
+        //System.out.println("2URL : " + url + "?" + paramString);
+        URL serverUrl = new URL(url + "?" + paramString);
+
+        HttpURLConnection conex = null;
+        OutputStream out = null;
+        InputStream in = null;
+        String response = null;
+
+        if (method == null) {
+            method = "POST";
+        }
+        try {
+            conex = (HttpURLConnection) serverUrl.openConnection();
+            if (userAgent != null) {
+                conex.setRequestProperty("user-agent", userAgent);
+            }
+            conex.setConnectTimeout(30000);
+            conex.setReadTimeout(60000);
+            conex.setRequestMethod(method);
+            conex.setDoOutput(true);
+            conex.connect();
+
+            //System.out.println("CONNECT:" + conex);
+            //   out = conex.getOutputStream();
+            //   out.write(paramString.toString().getBytes("UTF-8"));
+            in = conex.getInputStream();
+            response = getResponse(in);
+
+        } catch (java.io.IOException ioe) {
+            if (conex.getResponseCode() >= 400) {
+                //System.out.println("ERROR:" +   getResponse(conex.getErrorStream()));
+                response = getResponse(conex.getErrorStream());
+                log.error("\n\nERROR:" + response);
+            }
+            //ioe.printStackTrace();
+        } finally {
+            close(in);
+            //close(out);
+            if (conex != null) {
+                conex.disconnect();
+            }
+        }
+        if (response == null) {
+            response = "";
+        }
+        return response;
+    }
+
+    public static String getRequest(Map<String, String> params, String url,
+            String userAgent) throws IOException {
+
+        CharSequence paramString = (null == params) ? "" : delimit(params.entrySet(), "&", "=", true);
+        URL serverUrl = new URL(url + "?" + paramString);
+        HttpURLConnection conex = null;
+        InputStream in = null;
+        String response = null;
+
+        try {
+            conex = (HttpURLConnection) serverUrl.openConnection();
+            if (userAgent != null) {
+                conex.setRequestProperty("user-agent", userAgent);
+            }
+            conex.setConnectTimeout(30000);
+            conex.setReadTimeout(60000);
+            conex.setRequestMethod("GET");
+            conex.setDoOutput(true);
+            conex.connect();
+            in = conex.getInputStream();
+            response = getResponse(in);
+            //System.out.println("RESPONSE:" + response);
+
+        } catch (java.io.IOException ioe) {
+            if (conex.getResponseCode() >= 400) {
+                response = getResponse(conex.getErrorStream());
+                log.error("\n\n\nERROR:" + response);
+            }
+            ioe.printStackTrace();
+        } finally {
+            close(in);
+            if (conex != null) {
+                conex.disconnect();
+            }
+        }
+        if (response == null) {
+            response = "";
+        }
+        return response;
+    }
+
+    public static CharSequence delimit(Collection<Map.Entry<String, String>> entries,
+            String delimiter, String equals, boolean doEncode)
+            throws UnsupportedEncodingException {
+
+        if (entries == null || entries.isEmpty()) {
+            return null;
+        }
+        StringBuilder buffer = new StringBuilder(64);
+        boolean notFirst = false;
+        for (Map.Entry<String, String> entry : entries) {
+            if (notFirst) {
+                buffer.append(delimiter);
+            } else {
+                notFirst = true;
+            }
+            CharSequence value = entry.getValue();
+            buffer.append(entry.getKey());
+            buffer.append(equals);
+            buffer.append(doEncode ? encode(value) : value);
+        }
+        return buffer;
+    }
+
+    /**
+     * Codifica el valor de {@code target} de acuerdo al c&oacute;digo de
+     * caracteres UTF-8
+     *
+     * @param target representa el texto a codificar
+     * @return un {@code String} que representa el valor de {@code target} de
+     * acuerdo al c&oacute;digo de caracteres UTF-8
+     * @throws UnsupportedEncodingException en caso de ocurrir algun problema en
+     * la codificaci&oacute;n a UTF-8
+     */
+    private static String encode(CharSequence target) throws UnsupportedEncodingException {
+
+        String result = "";
+        if (target != null) {
+            result = target.toString();
+            result = URLEncoder.encode(result, "UTF8");
+        }
+        return result;
+    }
+
+    public static String getResponse(InputStream data) throws IOException {
+
+        Reader in = new BufferedReader(new InputStreamReader(data, "UTF-8"));
+        StringBuilder response = new StringBuilder(256);
+        char[] buffer = new char[1000];
+        int charsRead = 0;
+        while (charsRead >= 0) {
+            response.append(buffer, 0, charsRead);
+            charsRead = in.read(buffer);
+        }
+        in.close();
+        return response.toString();
+    }
+
+    public static void close(Closeable c) {
+        if (c != null) {
+            try {
+                c.close();
+            } catch (IOException ex) {
+                log.error("Error at closing object: " + c.getClass().getName(),
+                        ex);
+            }
+        }
+    }
+    
+    public static String postRequestParams(Map<String, String> params, String url,
+            String userAgent, String method) throws IOException {
+        CharSequence paramString = (null == params) ? "" : delimit(params.entrySet(), "&", "=", true);
+        URL serverUrl = new URL(url + "?" + paramString);
+        HttpURLConnection conex = null;
+        OutputStream out = null;
+        InputStream in = null;
+        String response = null;
+
+        if (method == null) {
+            method = "POST";
+        }
+        try {
+            conex = (HttpURLConnection) serverUrl.openConnection();
+            if (userAgent != null) {
+                conex.setRequestProperty("user-agent", userAgent);
+            }
+            conex.setConnectTimeout(30000);
+            conex.setReadTimeout(60000);
+            conex.setRequestMethod(method);
+            conex.setDoOutput(true);
+            conex.connect();
+
+
+            in = conex.getInputStream();
+            response = getResponse(in);
+
+        } catch (java.io.IOException ioe) {
+            if (conex.getResponseCode() >= 400) {
+                response = getResponse(conex.getErrorStream());
+                log.error("\n\nERROR for url:" + serverUrl + " -- " + response);
+            }
+        } finally {
+            close(in);
+            if (conex != null) {
+                conex.disconnect();
+            }
+        }
+        if (response == null) {
+            response = "";
+        }        
+        return response;
+    }
+}
---
+++ SWB4/SWBSocialWeb/web/work/models/SWBAdmin/jsp/fanPages/importFPFacebook.jsp
@@ -0,0 +1,143 @@
+<%-- 
+    Document   : importFPFacebook
+    Created on : 1/07/2014, 01:52:52 PM
+    Author     : francisco.jimenez
+--%>
+
+<%@page import="org.semanticwb.SWBPlatform"%>
+<%@page import="org.semanticwb.social.SocialSite"%>
+<%@page import="org.semanticwb.model.SWBContext"%>
+<%@page import="org.semanticwb.social.SocialNetwork"%>
+<%@page import="org.apache.commons.validator.UrlValidator"%>
+<%@page import="org.semanticwb.social.util.SWBSocialUtil"%>
+<%@page import="org.semanticwb.social.PostIn"%>
+<%@page import="org.semanticwb.model.WebSite"%>
+<%@page import="org.semanticwb.model.SWBModel"%>
+<%@page import="org.semanticwb.social.Facebook"%>
+<%@page import="org.semanticwb.platform.SemanticObject"%>
+<%@page import="java.util.Iterator"%>
+<%@page import="java.net.URLEncoder"%>
+<%@page import="java.io.Reader"%>
+<%@page import="java.io.InputStreamReader"%>
+<%@page import="java.io.BufferedReader"%>
+<%@page import="java.io.InputStream"%>
+<%@page import="java.io.OutputStream"%>
+<%@page import="java.net.HttpURLConnection"%>
+<%@page import="java.net.URL"%>
+<%@page import="java.io.IOException"%>
+<%@page import="java.util.Map"%>
+<%@page import="java.util.regex.Matcher"%>
+<%@page import="java.util.regex.Pattern"%>
+<%@page import="org.semanticwb.portal.api.SWBParamRequest"%>
+<%@page import="java.io.Writer"%>
+<%@page import="java.util.Date"%>
+<%@page import="java.util.TimeZone"%>
+<%@page import="java.text.SimpleDateFormat"%>
+<%@page import="java.text.DateFormat"%>
+<%@page import="org.semanticwb.portal.api.SWBResourceURL"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="static org.semanticwb.social.admin.resources.ImportFanPages.*"%>
+<%@page import="org.json.JSONArray"%>
+<%@page import="org.json.JSONException"%>
+<%@page import="org.json.JSONObject"%>
+<jsp:useBean id="paramRequest" scope="request" type="org.semanticwb.portal.api.SWBParamRequest"/>
+
+<%@page contentType="text/html" pageEncoding="x-iso-8859-11"%>
+
+<%
+    String suri = request.getParameter("suri");
+    SocialNetwork sn = (SocialNetwork) SemanticObject.createSemanticObject(suri).createGenericInstance();
+    Facebook fb = null;
+    if(!(sn instanceof Facebook)){return;}else{fb = (Facebook)sn;}
+    
+    try{        
+        HashMap<String, String> params = new HashMap<String, String>(2);
+        
+        //Access token de Facebook (normal)        
+        //params.put("access_token", "CAAIhP9SXDVgBAPXsemvmlXZBJVQZBu0MYLOc3JWjH91l6Q9NXGZAE9TpPxXRiowHIZBR8qsnm3Kds3D2I4A3k4YGZASYj8lch7p0eXHDZAoW3bApH47McsjvdX0QQZBpIZAGuULUAypOfGNIVqRdIZBJUWeJuY77azOJoM27n08tEwOtSpMgWVJR1YJNT7UIXHbEZD
+        params.put("access_token", fb.getAccessToken());
+        String respFanPage = postRequest(params, "https://graph.facebook.com/me/accounts",
+                                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95", "GET");
+        //System.out.println("pageAccessToken:" + respFanPage);
+        JSONObject responseFP = new JSONObject(respFanPage);        
+        if(!responseFP.isNull("data")){
+            if(responseFP.getJSONArray("data").length()>0){
+                JSONArray jarr = responseFP.getJSONArray("data");
+%>
+
+
+<form name="formSites<%=fb.getFacebookUserId()%>" id="formSites<%=fb.getFacebookUserId()%>" action="<%=paramRequest.getActionUrl().setAction(IMPORT_FB_PAGES).setParameter("suri", suri)%>" onsubmit="submitForm('formSites<%=fb.getFacebookUserId()%>'); try{document.getElementById('csLoading<%=fb.getFacebookUserId()%>').style.display='inline';}catch(noe){}; return false;">
+<table width="50%" border="0px">            
+   <tr>
+       <td colspan="3" style="text-align: center;" class="titulo">
+           <div id="msj-eliminar">
+                 <div class="bloque bloque2" style="margin-left: 20%;">
+                    <p class="bloqtit">Paginas de Facebook</p>
+                    <select name="pages" multiple size="5" class="bloqsel">
+<%
+                for(int i = 0 ; i <  jarr.length() ; i++){
+                    //System.out.println(jarr.getJSONObject(i));
+                    JSONObject entryFP = jarr.getJSONObject(i);                    
+                    if(!entryFP.isNull("id") && !entryFP.isNull("name")){
+%>
+                      <option value="<%=entryFP.getString("id")%>"><%=entryFP.getString("name")%></option>
+<%
+                    }
+                }
+%>
+                    </select>
+                </div>                    
+            </div>           
+       </td>
+   </tr>
+</table>
+    
+<div class="swbform" style="width:50%;">
+<div id="pub-detalle">
+
+<table width="100%" border="0px">            
+   <tr>
+       <td colspan="3" style="text-align: center;" class="titulo">Importar en el sitio:</td>
+   </tr>
+   <tr>
+       <td colspan="3" style="text-align: center;">&nbsp;</td>        
+   </tr>
+   <tr>
+        <td style="text-align: center;">
+            <select name="site" id="site"> 
+                 <option value="" selected="selected">Seleccione un sitio...</option>
+                 <%
+                    Iterator<WebSite> sites = SWBContext.listWebSites();
+                    while(sites.hasNext()){
+                        WebSite site = sites.next();
+                        if(!(site instanceof SocialSite)){
+                            if(!site.equals(SWBContext.getAdminWebSite()) && !site.equals(SWBContext.getGlobalWebSite()) && site.getHomePage()!=null){
+                                %>
+                                <option value="<%=site.getId()%>" selected="selected"><%=site.getTitle()%></option>
+                                <%
+                            }
+                        }
+                    }
+                 %>
+            </select>
+        </td>
+   </tr>
+</table>
+</div>
+</div>
+            
+<div style="width:50%;" align="center">
+    <button dojoType="dijit.form.Button" type="submit">Importar</button>
+</div>
+</form>
+
+<span id="csLoading<%=fb.getFacebookUserId()%>" style="width: 100px; display: none" align="center"><img src="<%=SWBPlatform.getContextPath()%>/swbadmin/images/loading.gif"/></span>
+<%
+            }
+        }
+%>
+    <%
+        }catch(Exception e){
+        out.print("Problem displaying News feed: " + e.getMessage());
+    }   
+%>
