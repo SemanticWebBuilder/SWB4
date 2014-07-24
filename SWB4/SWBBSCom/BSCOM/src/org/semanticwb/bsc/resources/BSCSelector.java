210900f12c1529d42e6c24b4311f25ebd1d3d4ce
redentor <JRedentor@users.noreply.github.com>
2014-07-24 14:57:33 +0000

    Adecuaciones del código HTML generado por los recursos a fin de utilizar las funcionalidades proporcionadas por bootstrap.

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/resources/BSCSelector.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/resources/BSCSelector.java
@@ -5,8 +5,6 @@ import java.io.PrintWriter;
 import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.semanticwb.SWBPlatform;
-import org.semanticwb.SWBPortal;
 import org.semanticwb.bsc.BSC;
 import org.semanticwb.model.User;
 import org.semanticwb.portal.api.GenericResource;
@@ -35,53 +33,71 @@ public class BSCSelector extends GenericResource {
             SWBParamRequest paramRequest) throws SWBResourceException, IOException {
         
         PrintWriter out = response.getWriter();
-        StringBuilder output = new StringBuilder(64);
+        StringBuilder output = new StringBuilder(128);
         //Obtener el Scorecard actual
         BSC currentBsc = (BSC) paramRequest.getWebPage().getWebSite();
         //Obtener listado de instancias de Scorecards
         Iterator<BSC> todosBsc = BSC.ClassMgr.listBSCs();
         int bscCount = 0;
         User user = paramRequest.getUser();
+        StringBuilder aux = new StringBuilder(64);
         
-        //funcion de javascript para mandar la forma que contiene el select
-        output.append("<script type=\"text/javascript\">\n");
-        output.append("function showScorecard(element) {\n");
-	output.append("  var context = \"");
-        output.append(SWBPortal.getDistributorPath());
-        output.append((SWBPlatform.getContextPath() != "" ? "/" + SWBPlatform.getContextPath() : ""));
-        output.append("\";\n");
-	output.append("  var path;\n");
-        output.append("  if (element && element[element.selectedIndex].value != \"\") {\n");
-        output.append("    var value = element[element.selectedIndex].value.split(',');\n");
-        output.append("    path = context + \"/\" + value[0] + \"/\" + value[1];\n");
-        output.append("    element.form.action = path;\n");
-        output.append("    window.open(path, 'window' + value[0]);\n");
-        output.append("  }\n");
-        output.append("}\n");
-        output.append("</script>\n");
-        //armar despliegue del select a mostrar
-        output.append("<form name=\"openScorecard\" id=\"openScorecard\" action=\"\" target=\"_blank\">\n");
-        output.append("  <select name=\"bscList\" id=\"bscList\" onChange=\"showScorecard(this);\">\n");
         if (todosBsc != null && todosBsc.hasNext()) {
             //Recorrer el listado de Scorecards
             while (todosBsc.hasNext()) {
                 BSC nextBsc = (BSC) todosBsc.next();
                 if (nextBsc.isActive() && user.haveAccess(nextBsc)) {
-                    bscCount++;
-                    String optionValue = nextBsc != currentBsc 
+                    boolean nextIsNotCurrent = !(nextBsc == currentBsc);
+                    String optionValue = nextIsNotCurrent
                             ? nextBsc.getId() + "," + nextBsc.getHomePage().getId()
                             : "";
-                    output.append("    <option value=\"" + optionValue + "\"" +
-                            (nextBsc != currentBsc ? "" : " selected") +
-                            ">" + nextBsc.getTitle() + "</option>\n");
+                    if (nextIsNotCurrent) {
+                        bscCount++;
+                        aux.append("      <li><a href=\"#\" class=\"dropdown-menu-item\"");
+                        aux.append(" onclick=\"showScorecard('");
+                        aux.append(optionValue);
+                        aux.append("');\">");
+                        aux.append(nextBsc.getTitle());
+                        aux.append("</a></li>\n");
+                    }
 		}
             }
-            if (bscCount == 0) {
-		output.append("    <option value=\"\">No hay otros Scorecards</option>\n");
-            }
         }
-        output.append("  </select>\n");
-        output.append("</form>\n");
+        
+        
+        //funcion de javascript para mandar la forma que contiene el select
+        output.append("<script type=\"text/javascript\">\n");
+        output.append("function showScorecard(value) {\n");
+        output.append("  var thisBsc = \"");
+        output.append(currentBsc.getTitle());
+        output.append("\";\n");
+	output.append("  var path = location.pathname.substring(0, location.pathname.indexOf(thisBsc));\n");
+        output.append("  if (value && value != \"\") {\n");
+        output.append("    var elements = value.split(',');\n");
+        output.append("    path += elements[0] + \"/\" + elements[1];\n");
+        output.append("    window.open(path, 'window' + elements[0]);\n");
+        output.append("  }\n");
+        output.append("}\n");
+        output.append("</script>\n");
+        //armar despliegue de opciones a mostrar de acuerdo a diseño grafico
+        output.append("<ul id=\"");
+        output.append(paramRequest.getLocaleString("elementId"));
+        output.append("\" class=\"nav nav-pills navbar-right\">\n");
+        output.append("  <li class=\"dropdown\">\n");
+        output.append("    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">");
+        output.append(paramRequest.getLocaleString("lbl_title"));
+        output.append(": ");
+        output.append(currentBsc.getTitle());
+        if (bscCount > 0) {
+            output.append("<span class=\"caret\"></span>");
+        }
+        output.append("</a>\n");
+        output.append("    <ul class=\"dropdown-menu\" role=\"menu\">\n");
+        //se incluye la lista de scorecards encontrados
+        output.append(aux);
+        output.append("    </ul>\n");
+        output.append("  </li>\n");
+        output.append("</ul>\n");
         
         out.println(output.toString());
     }
