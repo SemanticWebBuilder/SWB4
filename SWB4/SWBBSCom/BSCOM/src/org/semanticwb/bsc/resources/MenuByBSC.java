bac11b1aa6b33298544506006b8228803ddb9312
redentor <JRedentor@users.noreply.github.com>
2014-08-04 17:16:20 +0000

    Se agregó el ordenamiento de los elementos del menú

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/resources/MenuByBSC.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/resources/MenuByBSC.java
@@ -3,11 +3,12 @@ package org.semanticwb.bsc.resources;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.semanticwb.Logger;
-import org.semanticwb.SWBUtils;
 import org.semanticwb.bsc.BSC;
 import org.semanticwb.model.WebPage;
 import org.semanticwb.portal.api.GenericResource;
@@ -16,33 +17,38 @@ import org.semanticwb.portal.api.SWBResourceException;
 
 
 /**
- * Genera el codigo HTML que representa el menu de la aplicacion en la interface del usuario final
+ * Genera el c&oacute;digo HTML que representa el men&uacute; de la aplicaci&oacute;n en la interface del usuario final
  * @author jose.jimenez
  */
 public class MenuByBSC extends GenericResource {
     
     
-    /**
-     * Realiza operaciones en la bitacora de eventos.
-     */
-    private static Logger log = SWBUtils.getLogger(MenuByBSC.class);
-    
     @Override
     public void doView(HttpServletRequest request, HttpServletResponse response,
             SWBParamRequest paramsRequest) throws SWBResourceException, IOException {
         
+        response.setContentType("text/html; charset=ISO-8859-1");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Pragma", "no-cache");
         PrintWriter out = response.getWriter();
-        //Resource base = this.getResourceBase();
         StringBuilder output = new StringBuilder(512);
         BSC currentBsc = (BSC) paramsRequest.getWebPage().getWebSite();
         WebPage home = currentBsc.getHomePage();
         WebPage currentPage = paramsRequest.getWebPage();
         String lang = paramsRequest.getUser().getLanguage();
         Iterator<WebPage> children = home.listVisibleChilds(lang);
+        MenuItemComparator comp = new MenuItemComparator();
+        
+        ArrayList<WebPage> childrenList = new ArrayList<>(16);
+        while (children.hasNext()) {
+            childrenList.add(children.next());
+        }
+        Collections.sort(childrenList, comp);
+        children = childrenList.iterator();
+        childrenList = null;
         
         if (children != null && children.hasNext()) {
-            output.append("<ul class=\"nav navbar-nav\" role=\"menu\">");
-            //TODO: ordenar children por nombre de ordenamiento
+            output.append("<ul class=\"nav navbar-nav\" role=\"menu\">\n");
             while (children.hasNext()) {
                 WebPage option = children.next();
                 Iterator<WebPage> suboptions = option.listVisibleChilds(lang);
@@ -55,15 +61,24 @@ public class MenuByBSC extends GenericResource {
                 if (hasSuboptions) {
                     output.append(" class=\"dropdown\">\n");
                     output.append("    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">");
-                    output.append(option.getTitle(lang));
+                    output.append(option.getDisplayTitle(lang));
                     output.append("<span class=\"caret\"></span></a>\n");
-                    output.append("    <ul class=\"dropdown-menu\" role=\"menu\">");
+                    output.append("    <ul class=\"dropdown-menu\" role=\"menu\">\n");
+                    
+                    ArrayList<WebPage> suboptionsList = new ArrayList<>(16);
+                    while (suboptions.hasNext()) {
+                        suboptionsList.add(suboptions.next());
+                    }
+                    Collections.sort(suboptionsList, comp);
+                    suboptions = suboptionsList.iterator();
+                    suboptionsList = null;
+                    
                     while (suboptions.hasNext()) {
                         WebPage suboption = suboptions.next();
                         output.append("      <li><a href=\"");
                         output.append(suboption.getUrl(lang));
                         output.append("\">");
-                        output.append(suboption.getTitle(lang));
+                        output.append(suboption.getDisplayTitle(lang));
                         output.append("</a></li>\n");
                     }
                     output.append("    </ul>\n");
@@ -76,20 +91,44 @@ public class MenuByBSC extends GenericResource {
                     output.append(option.getUrl(lang));
                     output.append("\">");
                     output.append(option.getTitle(lang));
-                    output.append("</a></li>");
+                    output.append("</a></li>\n");
                 }
-//                output.append("");
-//                output.append("");
-//                output.append("");
-//                output.append("");
-//                output.append("");
-//                output.append("");
-//                output.append("");
-//                output.append("");
-//                output.append("");
             }
-            output.append("</ul>");
+            output.append("</ul>\n");
         }
         out.println(output.toString());
+    }
+}
+
+class MenuItemComparator implements Comparator {
+
+    /**
+     * Compara las nombres de ordenamiento de dos p&aacute;ginas web
+     * @param prop1 una p&aacute;gina web a ordenar de acuerdo a su nombre de ordenamiento
+     * @param prop2 una p&aacute;gina web a ordenar de acuerdo a su nombre de ordenamiento
+     * @return un entero negativo, cero o un entero positivo de acuerdo a si el nombre de ordenamiento
+     *         de la segunda p&aacute;gina web especificada se puede ordenar antes, en la 
+     *         misma ubicacion o despues de aquel de la primera p&aacute;gina web.
+     */
+    @Override
+    public int compare(Object obj1, Object obj2) {
+        
+        int comparacion;
+        String s1 = new String();
+        String s2 = new String();
+        if (obj1 != null && obj1 instanceof WebPage) {
+            WebPage page1 = (WebPage) obj1;
+            if (page1.getSortName() != null) {
+                s1 = page1.getSortName();
+            }
+        }
+        if (obj2 != null && obj2 instanceof WebPage) {
+            WebPage page2 = (WebPage) obj2;
+            if (page2.getSortName() != null) {
+                s2 = page2.getSortName();
+            }
+        }
+        comparacion = s1.compareTo(s2);
+        return comparacion;
     }
 }
