9c745da98c1156eedd78675ff9f329c4d3c9f61e
vlorenzana <vlorenzana73@users.noreply.github.com>
2014-09-05 23:28:28 +0000

    Mejora a cambio de idioma, mantiene las querystring y solo cambia el lenguaje como uri

--- SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/Language.java
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/Language.java
@@ -43,59 +43,71 @@ import org.w3c.dom.Document;
 
 // TODO: Auto-generated Javadoc
 /**
- * Language se encarga de desplegar los lenguajes disponibles en WebBuilder bajo ciertos
- * criterios (configuraci�n de recurso) para actulizar el lenguaje del usuario final. Puede ser un
- * recurso de estrategia o de contenido.
+ * Language se encarga de desplegar los lenguajes disponibles en WebBuilder bajo
+ * ciertos criterios (configuraci�n de recurso) para actulizar el lenguaje del
+ * usuario final. Puede ser un recurso de estrategia o de contenido.
  *
  * Language displays all available languages on WebBuilder under criteria like
- * resource configuration. In addition can update language for user. this resource
- * could be installed as a content resource or a strategy resource.
+ * resource configuration. In addition can update language for user. this
+ * resource could be installed as a content resource or a strategy resource.
  *
  * @author : Vanessa Arredondo Nunez
  */
 public class Language extends GenericAdmResource
 {
-    
-    /** The log. */
+
+    /**
+     * The log.
+     */
     private static Logger log = SWBUtils.getLogger(Language.class);
-    
-    /** The tpl. */
-    javax.xml.transform.Templates tpl; 
-    
-    /** The path. */
-    String path = SWBPlatform.getContextPath()+"swbadmin/xsl/Language/";
-    
+
+    /**
+     * The tpl.
+     */
+    javax.xml.transform.Templates tpl;
+
+    /**
+     * The path.
+     */
+    String path = SWBPlatform.getContextPath() + "swbadmin/xsl/Language/";
+
     /* (non-Javadoc)
      * @see org.semanticwb.portal.api.GenericAdmResource#setResourceBase(org.semanticwb.model.Resource)
      */
     @Override
-    public void setResourceBase(Resource base) throws SWBResourceException {
+    public void setResourceBase(Resource base) throws SWBResourceException
+    {
         super.setResourceBase(base);
-        
-        if(!"".equals(base.getAttribute("template","").trim()))
+
+        if (!"".equals(base.getAttribute("template", "").trim()))
         {
-            try 
+            try
             {
-                tpl = SWBUtils.XML.loadTemplateXSLT(SWBPortal.getFileFromWorkPath(base.getWorkPath() +"/"+ base.getAttribute("template").trim()));
+                tpl = SWBUtils.XML.loadTemplateXSLT(SWBPortal.getFileFromWorkPath(base.getWorkPath() + "/" + base.getAttribute("template").trim()));
                 //path=SWBPlatform.getWebWorkPath() +  base.getWorkPath() + "/";
-                path = SWBPlatform.getContextPath()+"swbadmin/xsl/Language/";
-            }catch(Exception e) {
-                log.error("Error while loading resource template: "+base.getId(), e);
+                path = SWBPlatform.getContextPath() + "swbadmin/xsl/Language/";
+            }
+            catch (Exception e)
+            {
+                log.error("Error while loading resource template: " + base.getId(), e);
             }
         }
-        if(tpl==null)
+        if (tpl == null)
         {
-            try {
+            try
+            {
                 tpl = SWBUtils.XML.loadTemplateXSLT(SWBPortal.getAdminFileStream("/swbadmin/xsl/Language/Language.xslt"));
-            }catch(Exception e) {
-                log.error("Error while loading default resource template: "+base.getId(), e);
+            }
+            catch (Exception e)
+            {
+                log.error("Error while loading default resource template: " + base.getId(), e);
             }
         }
     }
 
     /**
      * Gets the dom.
-     * 
+     *
      * @param request the request
      * @param response the response
      * @param paramRequest the param request
@@ -105,130 +117,168 @@ public class Language extends GenericAdmResource
      */
     public Document getDom(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
     {
-        Document  dom = null;
+        Document dom = null;
         try
         {
-            SWBResourceURL url = paramRequest.getActionUrl().setMode(paramRequest.Mode_VIEW);                        
+            //SWBResourceURL url = paramRequest.getActionUrl().setMode(paramRequest.Mode_VIEW);                        
             dom = SWBUtils.XML.getNewDocument();
             Element root = dom.createElement("resource");
             root.setAttribute("currentlang", paramRequest.getUser().getLanguage());
 
             //System.out.println("\n\npath="+path);
-
             root.setAttribute("path", path);
             dom.appendChild(root);
 
             Iterator<org.semanticwb.model.Language> itLang = paramRequest.getWebPage().getWebSite().listLanguages();
-            while(itLang.hasNext()) {
+            while (itLang.hasNext())
+            {
                 org.semanticwb.model.Language lang = itLang.next();
                 Element elang = dom.createElement("language");
                 elang.setAttribute("id", String.valueOf(lang.getId()));
                 elang.setAttribute("idtm", lang.getWebSite().getId());
                 elang.setAttribute("lang", lang.getDisplayTitle(paramRequest.getUser().getLanguage()));//
                 elang.setAttribute("title", lang.getDisplayTitle(paramRequest.getUser().getLanguage()));
-                url.setParameter("language", lang.getId());//
-                elang.setAttribute("ref", url.toString());
+                String url = request.getRequestURI();
+                String[] values = url.split("/");
+                StringBuilder urlToLanguage = new StringBuilder();
+                if (url.equals("/"))
+                {
+                    url += lang.getId() + "/" + paramRequest.getWebPage().getWebSiteId() + "/" + paramRequest.getWebPage().getId();
+                    urlToLanguage.append(url);
+                }
+                else
+                {
+                    int i = 0;
+                    for (String value : values)
+                    {
+                        if (!value.isEmpty())
+                        {
+                            i++;
+                            if (i != 1)
+                            {
+                                urlToLanguage.append("/");
+                                urlToLanguage.append(value);
+                            }
+                            else
+                            {
+                                urlToLanguage.append("/");
+                                urlToLanguage.append(lang.getId());
+                            }
+                        }
+                    }
+                }
+                //url.setParameter("language", lang.getId());//
+                String urlToDisplay = urlToLanguage.toString();
+                if (request.getQueryString() != null)
+                {
+                    urlToDisplay += "?" + request.getQueryString();
+                }
+                elang.setAttribute("ref", urlToDisplay);
                 root.appendChild(elang);
-            }            
-        }catch (Exception e) {
-            log.error("Error while generating DOM in resource "+ getResourceBase().getResourceType().getResourceClassName() +" with identifier " + getResourceBase().getId() + " - " + getResourceBase().getTitle(), e);
+            }
+        }
+        catch (Exception e)
+        {
+            log.error("Error while generating DOM in resource " + getResourceBase().getResourceType().getResourceClassName() + " with identifier " + getResourceBase().getId() + " - " + getResourceBase().getTitle(), e);
         }
         return dom;
     }
-    
-    
+
     /*public static ArrayList getAppLanguages() {
-        ArrayList languages = new ArrayList();
-        Iterator<WebSite> it = SWBContext.listWebSites();
-        while (it.hasNext()) {
-            WebSite site = it.next();
-            Iterator<org.semanticwb.model.Language > itLang = site.listLanguages();
-            while (itLang.hasNext()) {
-                org.semanticwb.model.Language lang = itLang.next();
-                if (!languages.contains(lang.getId())) {
-                    languages.add(lang.getId());
-                }
-            }
-        }
-        return languages;
-    }*/
-    
-    
-    
+     ArrayList languages = new ArrayList();
+     Iterator<WebSite> it = SWBContext.listWebSites();
+     while (it.hasNext()) {
+     WebSite site = it.next();
+     Iterator<org.semanticwb.model.Language > itLang = site.listLanguages();
+     while (itLang.hasNext()) {
+     org.semanticwb.model.Language lang = itLang.next();
+     if (!languages.contains(lang.getId())) {
+     languages.add(lang.getId());
+     }
+     }
+     }
+     return languages;
+     }*/
     /**
      * Do xml.
-     * 
+     *
      * @param request the request
      * @param response the response
      * @param paramRequest the param request
      * @throws IOException Signals that an I/O exception has occurred.
      * @throws SWBResourceException the sWB resource exception
-     */    
+     */
     public void doXML(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
     {
         try
         {
-            org.w3c.dom.Document dom=getDom(request, response, paramRequest);
-            if(dom!=null) {
+            org.w3c.dom.Document dom = getDom(request, response, paramRequest);
+            if (dom != null)
+            {
                 response.getWriter().println(SWBUtils.XML.domToXml(dom));
             }
         }
-        catch(Exception e){ log.error(e); }        
-    }    
-    
+        catch (Exception e)
+        {
+            log.error(e);
+        }
+    }
+
     /**
      * Do view.
-     * 
+     *
      * @param request the request
      * @param response the response
      * @param paramRequest the param request
      * @throws IOException Signals that an I/O exception has occurred.
      * @throws SWBResourceException the sWB resource exception
-     */    
+     */
     @Override
     public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
     {
         String action = null != request.getParameter("lng_act") && !"".equals(request.getParameter("lng_act").trim()) ? request.getParameter("lng_act").trim() : "lng_step1";
-        
+
         /*if("lng_step2".equals(action)) {
-            paramRequest.getUser().icooklanguage(request,response,request.getParameter("language"));
-        }*/
+         paramRequest.getUser().icooklanguage(request,response,request.getParameter("language"));
+         }*/
         try
         {
-            Document dom =getDom(request, response, paramRequest);
-            if(dom != null)  {
+            Document dom = getDom(request, response, paramRequest);
+            if (dom != null)
+            {
                 response.getWriter().print(SWBUtils.XML.transformDom(tpl, dom));
             }
         }
-        catch(Exception e) { log.error(e); }
+        catch (Exception e)
+        {
+            log.error(e);
+        }
     }
-    
+
     /**
      * Process action.
-     * 
+     *
      * @param request the request
      * @param response the response
      * @throws IOException Signals that an I/O exception has occurred.
      * @throws SWBResourceException the sWB resource exception
-     */      
+     */
     @Override
     public void processAction(HttpServletRequest request, SWBActionResponse response) throws SWBResourceException, IOException
     {
         String language = null != request.getParameter("language") && !"".equals(request.getParameter("language").trim()) ? request.getParameter("language").trim() : "";
-        if(!"".equals(language))
+        if (!"".equals(language))
         {
-            User user=response.getUser();
+            User user = response.getUser();
             user.setLanguage(language);
             request.getSession().removeAttribute("WBCookieMgrUI"); //remove User Intert flag (remoteWebApps)
-            /** TODO: VER 4.0
-            if(user.isRegistered()) user.getRecUser().update(); 
-            else 
-            {
-                response.setRenderParameter("lng_act","lng_step2");
-                response.setRenderParameter("language",language);
-            }**/
-            response.setRenderParameter("lng_act","lng_step2");
-            response.setRenderParameter("language",language);
+            /**
+             * TODO: VER 4.0 if(user.isRegistered()) user.getRecUser().update();
+             * else { response.setRenderParameter("lng_act","lng_step2");
+             * response.setRenderParameter("language",language); }*
+             */
+            response.setRenderParameter("lng_act", "lng_step2");
+            response.setRenderParameter("language", language);
         }
-    }    
+    }
 }
