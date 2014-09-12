34bf7c767495a4d2e1f26a52b89bc94a06481edc
vlorenzana <vlorenzana@60c509d2-c84e-0410-9847-e8532c4ce32d>
2014-09-12 18:06:31 +0000

    Se agrego validación de calendarización y validación de dispositivo

--- SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/sem/videolibrary/SWBVideoLibrary.java
+++ SWB4/swb/SWBPortal/src/org/semanticwb/portal/resources/sem/videolibrary/SWBVideoLibrary.java
@@ -22,12 +22,12 @@
  */
 package org.semanticwb.portal.resources.sem.videolibrary;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -35,6 +35,8 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.*;
 import org.semanticwb.Logger;
 import org.semanticwb.SWBUtils;
+import org.semanticwb.model.CalendarRef;
+import org.semanticwb.model.Device;
 import org.semanticwb.model.GenericIterator;
 import org.semanticwb.model.Resource;
 import org.semanticwb.model.ResourceCollectionCategory;
@@ -45,38 +47,40 @@ import org.semanticwb.portal.api.*;
 /**
  * The Class SWBVideoLibrary.
  */
-public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videolibrary.base.SWBVideoLibraryBase 
+public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videolibrary.base.SWBVideoLibraryBase
 {
-    
-    /** The Constant log. */
+
+    /**
+     * The Constant log.
+     */
     private static final Logger log = SWBUtils.getLogger(SWBVideoLibrary.class);
-    
+
     /**
      * Instantiates a new sWB video library.
      */
     public SWBVideoLibrary()
     {
     }
-    
+
     /**
      * Checks for video.
-     * 
+     *
      * @param contents the contents
      * @param month the month
      * @param year the year
      * @return true, if successful
      */
-    public static boolean hasVideo(List<VideoContent> contents, int month,int year)
+    public static boolean hasVideo(List<VideoContent> contents, int month, int year)
     {
         Calendar calendar = Calendar.getInstance();
         for (VideoContent content : contents)
         {
-            if(content.getPublishDate()!=null)
+            if (content.getPublishDate() != null)
             {
                 calendar.setTime(content.getPublishDate());
                 int newmonth = calendar.get(Calendar.MONTH);
-                int new_year=calendar.get(Calendar.YEAR);
-                if (month == newmonth && year==new_year)
+                int new_year = calendar.get(Calendar.YEAR);
+                if (month == newmonth && year == new_year)
                 {
                     return true;
                 }
@@ -85,10 +89,10 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
         }
         return false;
     }
-    
+
     /**
      * Gets the video by month.
-     * 
+     *
      * @param contents the contents
      * @return the video by month
      */
@@ -98,17 +102,17 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
         TreeMap<String, List<VideoContent>> getVideoByMonth = new TreeMap<String, List<VideoContent>>();
         for (VideoContent content : contents)
         {
-            if(content.getPublishDate()!=null)
+            if (content.getPublishDate() != null)
             {
                 calendar.setTime(content.getPublishDate());
                 int newmonth = calendar.get(Calendar.MONTH);
                 int year = calendar.get(Calendar.YEAR);
-                String key=newmonth+"_"+year;
+                String key = newmonth + "_" + year;
 
-                List<VideoContent> contentsMonth=getVideoByMonth.get(key);
-                if(contentsMonth==null)
+                List<VideoContent> contentsMonth = getVideoByMonth.get(key);
+                if (contentsMonth == null)
                 {
-                    contentsMonth=new ArrayList<VideoContent>();
+                    contentsMonth = new ArrayList<VideoContent>();
                     getVideoByMonth.put(key, contentsMonth);
                 }
                 contentsMonth.add(content);
@@ -116,20 +120,20 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
         }
         return getVideoByMonth;
     }
-    
+
     /**
      * Gets the years.
-     * 
+     *
      * @param contents the contents
      * @return the years
      */
     public static String[] getYears(List<VideoContent> contents)
     {
-        HashSet<String> getyears=new HashSet<String>();
+        HashSet<String> getyears = new HashSet<String>();
         Calendar calendar = Calendar.getInstance();
         for (VideoContent content : contents)
         {
-            if(content.getPublishDate()!=null)
+            if (content.getPublishDate() != null)
             {
                 calendar.setTime(content.getPublishDate());
                 int new_year = calendar.get(Calendar.YEAR);
@@ -141,25 +145,25 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
 
     /**
      * Instantiates a new sWB video library.
-     * 
+     *
      * @param base the base
      */
     public SWBVideoLibrary(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
     }
-    
+
     /**
      * Gets the videos.
-     * 
+     *
      * @param uri the uri
      * @param user the user
      * @return the videos
      */
-    private List<VideoContent> getVideos(String uri,User user)
+    private List<VideoContent> getVideos(String uri, User user)
     {
         List<VideoContent> list = new ArrayList<VideoContent>();
-        GenericIterator<Resource> resources = null;        
+        GenericIterator<Resource> resources = null;
         if (this.getCollection() != null && this.getCollection().getResourceCollectionType().getResourceClassName().equals(VideoContent.class.getCanonicalName()))
         {
             if (this.getCategory() == null)
@@ -178,7 +182,7 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
                         isCategoryOfCallection = true;
                         break;
                     }
-                }                
+                }
                 if (isCategoryOfCallection)
                 {
                     resources = this.getCategory().listResources();
@@ -195,9 +199,32 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
             while (resources.hasNext())
             {
                 Resource resource = resources.next();
-                if (resource.isActive() && !resource.isDeleted() && user.haveAccess(resource))
+                Iterator<CalendarRef> refs = resource.listCalendarRefs();
+                boolean isOnSchedule = true;
+                while (refs.hasNext())
                 {
-                    VideoContent object = (VideoContent)resource.getResourceData().createGenericInstance();
+                    CalendarRef ref = refs.next();
+                    if (ref.isValid() && ref.isActive() && ref.getCalendar().isActive() && ref.getCalendar().isValid())
+                    {
+                        if (!ref.getCalendar().isOnSchedule())
+                        {
+                            isOnSchedule = false;
+                        }
+                    }
+                }
+                Device d = resource.getDevice();
+                boolean device = true;
+                if (d != null)
+                {
+                    if (!user.hasDevice(d))
+                    {
+                        device = false;
+
+                    }
+                }
+                if (device && isOnSchedule && resource.isActive() && !resource.isDeleted() && user.haveAccess(resource) && resource.isValid())
+                {
+                    VideoContent object = (VideoContent) resource.getResourceData().createGenericInstance();
                     if (uri == null)
                     {
                         try
@@ -205,7 +232,7 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
                             object.setResourceBase(resource);
                             list.add(object);
                         }
-                        catch(Exception e)
+                        catch (Exception e)
                         {
                             log.error(e);
 
@@ -221,7 +248,7 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
                                 object.setResourceBase(resource);
                                 list.add(object);
                             }
-                            catch(Exception e)
+                            catch (Exception e)
                             {
                                 log.error(e);
 
@@ -234,7 +261,7 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
         }
         return list;
     }
-    
+
     /* (non-Javadoc)
      * @see org.semanticwb.portal.api.GenericResource#processRequest(HttpServletRequest, HttpServletResponse, SWBParamRequest)
      */
@@ -250,10 +277,10 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
             super.processRequest(request, response, paramRequest);
         }
     }
-    
+
     /**
      * Do show videos by month.
-     * 
+     *
      * @param request the request
      * @param response the response
      * @param paramRequest the param request
@@ -262,24 +289,24 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
      */
     public void doShowVideosByMonth(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
     {
-        String basePath="/work/models/"+paramRequest.getWebPage().getWebSite().getId()+"/jsp/"+ this.getClass().getSimpleName() +"/";
-        if(request.getParameter("month")!=null)
+        String basePath = "/work/models/" + paramRequest.getWebPage().getWebSite().getId() + "/jsp/" + this.getClass().getSimpleName() + "/";
+        if (request.getParameter("month") != null)
         {
-            int year=Calendar.getInstance().get(Calendar.YEAR);
-            if(request.getParameter("year")!=null)
+            int year = Calendar.getInstance().get(Calendar.YEAR);
+            if (request.getParameter("year") != null)
             {
-                year=Integer.parseInt(request.getParameter("year"));
+                year = Integer.parseInt(request.getParameter("year"));
             }
-            int month=-1;
+            int month = -1;
             try
             {
-                month=Integer.parseInt(request.getParameter("month"));
-                String key=month+"_"+year;
-                if(month>=0 && month<=12)
+                month = Integer.parseInt(request.getParameter("month"));
+                String key = month + "_" + year;
+                if (month >= 0 && month <= 12)
                 {
-                    List<VideoContent> list = getVideos(null,paramRequest.getUser());                    
-                    List<VideoContent> listMonth=getVideoByMonth(list).get(key);
-                    String path = basePath+"videoByMonth.jsp";
+                    List<VideoContent> list = getVideos(null, paramRequest.getUser());
+                    List<VideoContent> listMonth = getVideoByMonth(list).get(key);
+                    String path = basePath + "videoByMonth.jsp";
                     RequestDispatcher dis = request.getRequestDispatcher(path);
                     try
                     {
@@ -294,22 +321,22 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
                     return;
                 }
             }
-            catch(NumberFormatException nfe)
+            catch (NumberFormatException nfe)
             {
                 log.error(nfe);
             }
         }
         doView(request, response, paramRequest);
     }
-    
+
     /* (non-Javadoc)
      * @see org.semanticwb.portal.api.GenericResource#doView(HttpServletRequest, HttpServletResponse, SWBParamRequest)
      */
     @Override
     public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException
     {
-        String basePath="/work/models/"+paramRequest.getWebPage().getWebSite().getId()+"/jsp/"+ this.getClass().getSimpleName() +"/";
-        String path = basePath+"content.jsp";
+        String basePath = "/work/models/" + paramRequest.getWebPage().getWebSite().getId() + "/jsp/" + this.getClass().getSimpleName() + "/";
+        String path = basePath + "content.jsp";
 
         String uri = request.getParameter("uri");
         if (uri != null)
@@ -322,10 +349,10 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
         }
         if (paramRequest.getCallMethod() == paramRequest.Call_STRATEGY)
         {
-            path = basePath+"strategy.jsp";
+            path = basePath + "strategy.jsp";
             uri = null;
         }
-        List<VideoContent> list = getVideos(null,paramRequest.getUser());        
+        List<VideoContent> list = getVideos(null, paramRequest.getUser());
         if (uri != null && paramRequest.getCallMethod() == paramRequest.Call_CONTENT)
         {
             // busca el objeto
@@ -333,9 +360,35 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
             {
                 if (content.getResourceBase().getURI().equals(uri))
                 {
-                    if (content.getResourceBase().isValid() && paramRequest.getUser().haveAccess(content.getResourceBase()))
+                    Resource resource = content.getResourceBase();
+                    User user = paramRequest.getUser();
+                    Iterator<CalendarRef> refs = resource.listCalendarRefs();
+                    boolean isOnSchedule = true;
+                    while (refs.hasNext())
                     {
-                        path = basePath+ "showvideo.jsp";
+                        CalendarRef ref = refs.next();
+                        if (ref.isValid() && ref.isActive() && ref.getCalendar().isActive() && ref.getCalendar().isValid())
+                        {
+                            if (!ref.getCalendar().isOnSchedule())
+                            {
+                                isOnSchedule = false;
+                            }
+                        }
+                    }
+                    Device d = resource.getDevice();
+                    boolean device = true;
+                    if (d != null)
+                    {
+                        if (!user.hasDevice(d))
+                        {
+                            device = false;
+
+                        }
+                    }
+                    //if (content.getResourceBase().isValid() && paramRequest.getUser().haveAccess(content.getResourceBase()))
+                    if (device && isOnSchedule && resource.isActive() && !resource.isDeleted() && user.haveAccess(resource) && resource.isValid())
+                    {
+                        path = basePath + "showvideo.jsp";
                         RequestDispatcher dis = request.getRequestDispatcher(path);
                         try
                         {
@@ -354,7 +407,7 @@ public class SWBVideoLibrary extends org.semanticwb.portal.resources.sem.videoli
         }
         SortVideos sort = new SortVideos();
         Collections.sort(list, sort);
-        
+
         RequestDispatcher dis = request.getRequestDispatcher(path);
         try
         {
@@ -377,7 +430,7 @@ class SortVideos implements java.util.Comparator<VideoContent>
 
     public int compare(VideoContent o1, VideoContent o2)
     {
-        if(o1.getPublishDate()!=null && o2.getPublishDate()!=null)
+        if (o1.getPublishDate() != null && o2.getPublishDate() != null)
         {
             return o2.getPublishDate().compareTo(o1.getPublishDate());
         }
