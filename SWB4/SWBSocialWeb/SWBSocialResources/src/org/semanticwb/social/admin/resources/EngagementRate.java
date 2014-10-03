c75b5409283c227a1a8eac54c57fde7aa425c3a3
francisco <dendral@users.noreply.github.com>
2014-10-03 00:00:33 +0000

    Engagement Rate for Facebook

---
+++ SWB4/SWBSocialWeb/SWBSocialResources/src/org/semanticwb/social/admin/resources/EngagementRate.java
@@ -0,0 +1,108 @@
+/**  
+* SWB Social es una plataforma que descentraliza la publicación, seguimiento y monitoreo hacia las principales redes sociales. 
+* SWB Social escucha y entiende opiniones acerca de una organización, sus productos, sus servicios e inclusive de su competencia, 
+* detectando en la información sentimientos, influencia, geolocalización e idioma, entre mucha más información relevante que puede ser 
+* útil para la toma de decisiones. 
+* 
+* SWB Social, es una herramienta basada en la plataforma SemanticWebBuilder. SWB Social, como SemanticWebBuilder, es una creación original 
+* del Fondo de Información y Documentación para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite. 
+* 
+* INFOTEC pone a su disposición la herramienta SWB Social a través de su licenciamiento abierto al público (‘open source’), 
+* en virtud del cual, usted podrá usarla en las mismas condiciones con que INFOTEC la ha diseñado y puesto a su disposición; 
+* aprender de élla; distribuirla a terceros; acceder a su código fuente y modificarla, y combinarla o enlazarla con otro software, 
+* todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización 
+* del SemanticWebBuilder 4.0. y SWB Social 1.0
+* 
+* INFOTEC no otorga garantía sobre SWB Social, de ninguna especie y naturaleza, ni implícita ni explícita, 
+* siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los riesgos que puedan derivar 
+* de la misma. 
+* 
+* Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder o SWB Social, INFOTEC pone a su disposición la siguiente 
+* dirección electrónica: 
+*  http://www.semanticwebbuilder.org
+**/ 
+ 
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.semanticwb.social.admin.resources;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBPlatform;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.portal.api.SWBResourceURL;
+
+/**
+ *
+ * @author francisco.jimenez
+ */
+public class EngagementRate extends GenericResource{
+    public static Logger log = SWBUtils.getLogger(EngagementRate.class);
+    
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        if(request.getParameter("suri")!=null)
+        {
+            String jspResponse = SWBPlatform.getContextPath() +"/work/models/" + paramRequest.getWebPage().getWebSiteId() +"/jsp/socialNetworks/engagementRate.jsp";
+            if (request.getParameter("jspResponse") != null) {
+                jspResponse = request.getParameter("jspResponse");
+            }
+            RequestDispatcher dis = request.getRequestDispatcher(jspResponse);
+            try {
+                request.setAttribute("paramRequest", paramRequest);
+                dis.include(request, response);
+            } catch (Exception e) {
+                log.error(e);
+            }
+        }
+        
+        /*if (request.getParameter("doView") == null) {
+            doEdit(request, response, paramRequest);
+            return;
+        }
+        final String myPath = SWBPlatform.getContextPath() + "/work/models/" + paramRequest.getWebPage().getWebSiteId() + "/jsp/socialNetworks/engagementRate.jsp";
+        RequestDispatcher dis = request.getRequestDispatcher(myPath);
+        if (dis != null) {
+            try {
+                request.setAttribute("paramRequest", paramRequest);
+                request.setAttribute("suri", request.getParameter("suri"));
+                dis.include(request, response);
+            } catch (Exception ex) {
+                log.error(ex);
+            }
+        }*/
+
+    }
+
+    
+    @Override
+    public void processRequest(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        if(paramRequest.getMode().equals("showChart")){
+            doShowChart(request, response, paramRequest);
+        }
+        else {
+            super.processRequest(request, response, paramRequest);
+        }
+    }
+    
+
+    public void doShowChart(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {        
+        RequestDispatcher rd = request.getRequestDispatcher(SWBPlatform.getContextPath() +"/work/models/" + paramRequest.getWebPage().getWebSiteId() +"/jsp/socialNetworks/engagementRateChart.jsp");
+        request.setAttribute("paramRequest", paramRequest);
+        try {
+            rd.include(request, response);
+        } catch (ServletException ex) {
+            log.error("Error al enviar los datos a engagementRateChart.jsp " + ex.getMessage());
+        }
+    }
+}
