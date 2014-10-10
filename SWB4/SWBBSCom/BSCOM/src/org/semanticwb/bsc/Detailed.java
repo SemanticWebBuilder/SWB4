5004853ad17a0e9c45ebe1b68284b941145d2b89
cramos <ciramosi@users.noreply.github.com>
2014-10-10 20:47:40 +0000

    Permisos

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/Detailed.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/Detailed.java
@@ -4,6 +4,7 @@ import org.semanticwb.bsc.accessory.Period;
 
 public interface Detailed extends org.semanticwb.bsc.base.DetailedBase
 {
+    public boolean canView();
     public String getStatusIconClass();
     public String getStatusIconClass(Period period);
 }
